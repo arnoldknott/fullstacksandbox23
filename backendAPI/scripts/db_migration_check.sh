3dbcffafb347265ac58754266e65a5b438863111
arnoldknott <knott@dtu.dk>
2023-12-10 00:51:38 +0100

    chore: mounts backendAPI root directory into container to reach the scripts, renames scripts and first run of scripts in docker container to check for missing migrations

--- backendAPI/scripts/db_migration_check.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd ../src
-alembic check
-# docker compose run -T --rm backend_api sh -c "alembic check"
--- backendAPI/scripts/db_migration_create.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd ../src
-alembic revision --autogenerate -m \"$COMMIT_SHA\"
-# docker compose run -T --rm backend_api sh -c "alembic revision --autogenerate -m \"$1\"
