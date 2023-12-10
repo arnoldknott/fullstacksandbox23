fd1e0432ad98b699f6ffa1c5571e0d64d84b067d
arnoldknott <knott@dtu.dk>
2023-12-10 21:01:25 +0100

    ci: moves migration file to end of output of and adds the missing COMMIT_SHA to the container

--- backendAPI/scripts/postgres_migration_create.sh
+++ backendAPI/scripts/postgres_migration_create.sh
@@ -4,8 +4,9 @@
 cd /app/src
 alembic -c alembic_stage_prod.ini  revision --autogenerate -m \"$COMMIT_SHA\" > alembic_revision.log
 MIGRATION_SCRIPT_FILENAME=$(cat alembic_revision.log | awk '/Generating /{print $2}')
-echo $MIGRATION_SCRIPT_FILENAME
+# Leave those outputs as they are, as the runer picks up the echo and recreates the file for pull request:
 cat $MIGRATION_SCRIPT_FILENAME
+echo $MIGRATION_SCRIPT_FILENAME
 
 # apk del git
 # docker compose run -T --rm backend_api sh -c "alembic revision --autogenerate -m \"$1\"
