ea4e9dd3ddfecea9ee927004e7493c5a55507315
arnoldknott <knott@dtu.dk>
2023-12-10 14:39:50 +0100

    ci: ggettin gthe exit code from alembic back into the logs

--- backendAPI/scripts/postgres_migration_check.sh
+++ backendAPI/scripts/postgres_migration_check.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 cd /app/src
-alembic check > alembic_check.log || true
-ALEMBIC_EXIT_CODE=$?
-echo $ALEMBIC_EXIT_CODE > alembic_check_exit_code.log
-cat alembic_check.log
-echo "Alembic exit code: $ALEMBIC_EXIT_CODE"
-exit $ALEMBIC_EXIT_CODE
+alembic check > alembic_check.log
+# ALEMBIC_EXIT_CODE=$?
+# echo $ALEMBIC_EXIT_CODE > alembic_check_exit_code.log
+# cat alembic_check.log
+# echo "Alembic exit code: $ALEMBIC_EXIT_CODE"
+# exit $ALEMBIC_EXIT_CODE
 
 # cd /app/src
 # alembic check > alembic_check.log
