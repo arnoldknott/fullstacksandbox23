2f3126f1946c8e2f2fa3cf623c830dba821bc334
arnoldknott <knott@dtu.dk>
2023-12-10 21:51:49 +0100

    ci: corrects line numbers and triling line breaks for MIGRATION-FILE_NAME

--- backendAPI/scripts/postgres_migration_run.sh
+++ backendAPI/scripts/postgres_migration_run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd /app/src
-alembic -c alembic_stage_prod.ini upgrade head
+alembic -c alembic_stage_prod.ini upgrade head > alembic_upgrade.log
 # docker compose run -T --rm backend_api sh -c "alembic upgrade head"
