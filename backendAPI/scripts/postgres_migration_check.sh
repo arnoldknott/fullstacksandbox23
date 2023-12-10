0495da9f9c368ab1301ece599ddb961b397da4d3
arnoldknott <knott@dtu.dk>
2023-12-10 13:14:43 +0100

    ci: fixes typo in original script command

--- backendAPI/scripts/postgres_migration_check.sh
+++ backendAPI/scripts/postgres_migration_check.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 cd /app/src
-alembic check
+alembic check > alembic_check.log
+ALEMBIC_EXIT_CODE=$?
+cat alembic_check.log
+exit $ALEMBIC_EXIT_CODE
 # docker compose run -T --rm backend_api sh -c "alembic check"
