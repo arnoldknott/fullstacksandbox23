89878bda9f8441e80dddebfb07665089565c5178
arnoldknott <knott@dtu.dk>
2023-12-10 15:31:25 +0100

    ci: adds creating database migration files to workflow

--- backendAPI/scripts/postgres_migration_create.sh
+++ backendAPI/scripts/postgres_migration_create.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-apk add --no-cache git
+# apk add --no-cache git
 cd /app/src
-alembic revision --autogenerate -m \"$COMMIT_SHA\"
+alembic revision --autogenerate -m \"$COMMIT_SHA\" > alembic_revision.log
+MIGRATION_SCRIPT_FILENAME=$(cat alembic_revision.log | grep -oP 'Generating \K[^ ]+')
+echo $MIGRATION_SCRIPT_FILENAME
+cat $MIGRATION_SCRIPT_FILENAME
 
+# apk del git
 # docker compose run -T --rm backend_api sh -c "alembic revision --autogenerate -m \"$1\"
