1b58d8e4208d5c5dcf3ba904c25ff9006f34c19a
arnoldknott <knott@dtu.dk>
2023-12-11 18:03:42 +0100

    ci: fixes typo and adds path as env variables in migration scripts

--- backendAPI/scripts/postgres_migration_check.sh
+++ backendAPI/scripts/postgres_migration_check.sh
@@ -2,13 +2,15 @@
 
 set +e
 
+$LOGFILE_PATH="/data/migrations/stage_prod/logs"
+
 # TBD: add $COMMIT_SHA to file names
-mkdir -p /data/migrations/stage_prod/logs
+mkdir -p $LOGFILE_PATH
 cd /app/src
-alembic -c alembic_stage_prod.ini check > /data/migrations/stage_prod/logs/check_$COMMIT_SHA.log
+alembic -c alembic_stage_prod.ini check > $LOGFILE_PATH/check_$COMMIT_SHA.log
 ALEMBIC_EXIT_CODE=$?
-echo $ALEMBIC_EXIT_CODE > /data/migrations/stage_prod/logs/check_exit_code_$COMMIT_SHA.log
-cat check_$COMMIT_SHA.log
+echo $ALEMBIC_EXIT_CODE > $LOGFILE_PATH/check_exit_code_$COMMIT_SHA.log
+cat $LOGFILE_PATH/check_$COMMIT_SHA.log
 echo "Alembic exit code: $ALEMBIC_EXIT_CODE"
 exit $ALEMBIC_EXIT_CODE
 
--- backendAPI/scripts/postgres_migration_create.sh
+++ backendAPI/scripts/postgres_migration_create.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+$LOGFILE_PATH="/data/migrations/stage_prod/logs"
+$VERSIONSFILE_PATH="/data/migrations/stage_prod/versions"
+
+
 # apk add --no-cache git
 mkdir -p /data/migrations/stage_prod/versions
 cd /app/src
-alembic -c alembic_stage_prod.ini  revision --autogenerate -m \"$COMMIT_SHA\" > alembic_revision_$COMMIT_SHA.log
-MIGRATION_SCRIPT_FILENAME=$(cat alembic_revision.log | awk '/Generating /{print $2}')
-mv $MIGRATION_SCRIPT_FILENAME /data/migrations/stage_prod/versions
-mv alembic_revision_$COMMIT_SHA.log /data/migrations/stage_prod/logs
-# Leave those outputs as they are, as the runer picks up the echo and recreates the file for pull request:
+alembic -c alembic_stage_prod.ini  revision --autogenerate -m \"$COMMIT_SHA\" > $LOGFILE_PATH/revision_$COMMIT_SHA.log
+MIGRATION_SCRIPT_FILENAME=$(cat $LOGFILE_PATH/revision_$COMMIST_SHA.log | awk '/Generating /{print $2}')
+mv $MIGRATION_SCRIPT_FILENAME $VERSIONSFILE_PATH
+# mv $LOGFILE_PATH/revision_$COMMIT_SHA.log /data/migrations/stage_prod/logs
+# Leave those outputs as they are, as the runner picks up the echo and recreates the file for pull request:
 cat $MIGRATION_SCRIPT_FILENAME
 echo $MIGRATION_SCRIPT_FILENAME
 
