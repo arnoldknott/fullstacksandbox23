d3633455ce6bfb33894c0a4e05d5705b1814afd1
arnoldknott <knott@dtu.dk>
2023-12-11 15:41:55 +0100

    ci: adds file transfer through volume mount

--- backendAPI/scripts/postgres_migration_check.sh
+++ backendAPI/scripts/postgres_migration_check.sh
@@ -2,10 +2,14 @@
 
 set +e
 
+# TBD: add $COMMIT_SHA to file names
+mkdir -P /data/migrations/stage_prod/logs
 cd /app/src
 alembic -c alembic_stage_prod.ini check > alembic_check.log
 ALEMBIC_EXIT_CODE=$?
 echo $ALEMBIC_EXIT_CODE > alembic_check_exit_code.log
+mv alembic_check.log /data/migrations/stage_prod/logs
+mv alembic_check_exit_code.log /data/migrations/stage_prod/logs
 cat alembic_check.log
 echo "Alembic exit code: $ALEMBIC_EXIT_CODE"
 exit $ALEMBIC_EXIT_CODE
