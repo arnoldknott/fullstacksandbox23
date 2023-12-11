8dbc44b3a3fb817b797619881a787c31fe10b685
arnoldknott <knott@dtu.dk>
2023-12-11 20:54:07 +0100

    ci: fixes remote path for downloading migration script

--- backendAPI/scripts/postgres_migration_upgrade.sh
+++ backendAPI/scripts/postgres_migration_upgrade.sh
@@ -4,5 +4,6 @@ LOGFILE_PATH="/data/migrations/stage_prod/logs"
 
 cd /app/src
 alembic -c alembic_stage_prod.ini upgrade head > $LOGFILE_PATH/upgrade_$COMMIT_SHA.log
+cat $LOGFILE_PATH/upgrade_$COMMIT_SHA.log
 # mv alembic_upgrade.log /data/migrations/stage_prod/logs
 # docker compose run -T --rm backend_api sh -c "alembic upgrade head"
