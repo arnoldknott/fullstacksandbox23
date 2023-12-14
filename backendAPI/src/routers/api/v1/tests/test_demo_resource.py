41a9ade1588c93e897e9b877efc0da88e291a703
arnoldknott <knott@dtu.dk>
2023-12-14 08:36:59 +0100

    ci: fixes pretests, adda another migration file and files for testing demo_resource

---
+++ backendAPI/src/routers/api/v1/tests/test_demo_resource.py
@@ -0,0 +1,59 @@
+import pytest
+from httpx import AsyncClient
+from utils import demo_resource_test_input
+
+
+# , get_async_test_session: AsyncSession
+@pytest.mark.anyio
+# @pytest.mark.usefixtures("get_async_test_session")
+async def test_post_demo_resource(async_client: AsyncClient):
+    """Tests POST of a demo_resource."""
+    resource = demo_resource_test_input
+    response = await async_client.post("/api/v1/demo_resource/", json=resource)
+    assert response.status_code == 201
+    content = response.json()
+    assert content["name"] == demo_resource_test_input["name"]
+    assert content["description"] == demo_resource_test_input["description"]
+    assert "id" in content
+
+
+# TBD: check for correct data in response
+# @pytest.mark.anyio
+# async def test_get_demo_resource(async_client: AsyncClient):
+#     """Tests GET all demo resources."""
+#     response = await async_client.get("/api/v1/demo_resource/")
+
+#     assert response.status_code == 200
+#     assert len(response.json()) == 1
+#     assert response.json()[0]["description"] == "ok"
+#     assert "id" in response.json()[0]
+#     # assert DemoResource == [
+#     #     DemoResource(**item).model_dump() for item in response.json()
+#     # ]
+
+
+# @pytest.mark.anyio
+# async def test_get_demo_resource_by_id(async_client: AsyncClient):
+#     """Tests GET of a demo resources."""
+#     response = await async_client.get("/api/v1/demo_resource/1")
+
+#     assert response.status_code == 200
+#     assert {"status": "ok"} == response.json()
+
+
+# @pytest.mark.anyio
+# async def test_put_demo_resource(async_client: AsyncClient):
+#     """Tests PUT of a demo resource."""
+#     response = await async_client.get("/demo_resource/1")
+
+#     assert response.status_code == 200
+#     assert {"status": "ok"} == response.json()
+
+
+# @pytest.mark.anyio
+# async def test_delete_demo_resource(async_client: AsyncClient):
+#     """Tests DELETE of a demo resource."""
+#     response = await async_client.get("/demo_resource/1")
+
+#     assert response.status_code == 200
+#     assert {"status": "ok"} == response.json()
---
+++ backendAPI/src/routers/api/v1/tests/utils.py
@@ -0,0 +1,8 @@
+"""Defines utility functions for testing, for example test datasets and dependencies (for example a logged in user if required)."""
+
+demo_resource_test_input = {
+    "name": "Name of Test Resource",
+    "description": "Some fancy description of my test resource.",
+    "language": "en-US",
+    "timezone": "UTC+1",
+}
