bcd89e4fae15e21651c264f5912a871886b28d46
arnoldknott <knott@dtu.dk>
2023-12-27 20:27:44 +0100

    core: adds live connection to keyvault and some fixes

--- frontend_svelte/src/lib/keyvault.ts
+++
@@ -1,5 +0,0 @@
-// import { ManagedIdentityCredential } from "@azure/identity";
-// import { KeyClient } from "@azure/keyvault";
-
-// const credential = new ManagedIdentityCredential();
-// const client = new KeyClient(vaultUrl, credential);
--- frontend_svelte/src/routes/playground/core/+page.server.ts
+++ frontend_svelte/src/routes/playground/core/+page.server.ts
@@ -1,8 +1,10 @@
-import { app_config } from '$lib/core';
+import { app_config } from '$lib/config';
 import { error } from '@sveltejs/kit';
 
 // TBD: add type PageServerLoad here?
-export const load = async () => {
+// export const load = async ( {fetch, session, context } ) => {
+	// const host = context.host;
+export const load = async (  ) => {
 	const configuration = await app_config();
 
 	if (configuration === null) {
