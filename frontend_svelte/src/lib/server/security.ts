cfe4fdf81a60a4572645eaca8486f3a3d43d6fad
arnoldknott <knott@dtu.dk>
2023-12-30 18:21:25 +0100

    chore: comment unused code preparation for security

--- frontend_svelte/src/lib/server/security.ts
+++ frontend_svelte/src/lib/server/security.ts
@@ -1,34 +1,34 @@
-import { PublicClientApplication } from '@azure/msal-browser';
-import { app_config } from './config';
+// import { PublicClientApplication } from '@azure/msal-browser';
+// import { app_config } from './config';
 
 
-const configuration = await app_config();
+// const configuration = await app_config();
 
 
-const msalConfig = {
-  auth: {
-    clientId: configuration.app_reg_client_id,
-    authority: configuration.authority,
-  },
-};
+// const msalConfig = {
+//   auth: {
+//     clientId: configuration.app_reg_client_id,
+//     authority: configuration.authority,
+//   },
+// };
 
-// export const msalInstance = new PublicClientApplication(msalConfig);
+// // export const msalInstance = new PublicClientApplication(msalConfig);
 
-export const loginRequest = async (host: string) => {
+// export const loginRequest = async (host: string) => {
 
-  const redirectUri = `${host}/oauth/callback`;
-  try {
-    const redirectUri = `${host}/oauth/callback`;
-    const response = await msalInstance.loginPopup({ 
-      scopes: ["read.all"],// TBD: fix scopes to the ones registered in app registration
-      redirectUri: redirectUri
-    }); // add scopes here?
-    console.log(response);
-    return response;
-  } catch (err) {
-    console.error(err);
-  }
-};
+//   const redirectUri = `${host}/oauth/callback`;
+//   try {
+//     const redirectUri = `${host}/oauth/callback`;
+//     const response = await msalInstance.loginPopup({ 
+//       scopes: ["read.all"],// TBD: fix scopes to the ones registered in app registration
+//       redirectUri: redirectUri
+//     }); // add scopes here?
+//     console.log(response);
+//     return response;
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
 
 // export const loginRequest = async ( host: string ) => {
 //   const msalConfig = {
