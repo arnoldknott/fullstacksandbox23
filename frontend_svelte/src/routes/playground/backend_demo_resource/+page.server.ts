c0170cf9a55c86522cc283f4559fc49bd391ece1
arnoldknott <knott@dtu.dk>
2024-01-03 20:14:09 +0100

    chore: removes formating error

--- frontend_svelte/src/routes/playground/backend_demo_resource/+page.server.ts
+++ frontend_svelte/src/routes/playground/backend_demo_resource/+page.server.ts
@@ -1,12 +1,12 @@
 import { getBackend } from '$lib/backend';
-import { error } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+// import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from '../backend_authorization/$types';
 
 // TBD: add type PageServerLoad here?
 export const load: PageServerLoad = async () => {
 	const health = await getBackend('/api/v1/demo_resource');
-  console.log("health");
-  console.log(health);
+  // console.log("demo resource - server - health");
+  // console.log(health);
 	// if (health === null) {
 	// 	return error(404, 'Unavailable');
 	// }
