f2864c99b5c2c45cdfc52cb7d85d55f53b4ffb16
arnoldknott <knott@dtu.dk>
2024-01-08 15:48:33 +0100

    chore: removes backend calls from frontend

--- frontend_svelte/src/routes/playground/ms_graph_me/+page.server.ts
+++ frontend_svelte/src/routes/playground/ms_graph_me/+page.server.ts
@@ -1,15 +1,16 @@
 
 // import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import { get_ms_graph } from '$lib/server/microsoft_graph';
+// import { get_ms_graph } from '$lib/server/microsoft_graph';
 
 // TBD: add type PageServerLoad here?
 export const load: PageServerLoad = async () => {
-	const demo_resource = await get_ms_graph('/me');
+	// const demo_resource = await get_ms_graph('/me');
   // console.log("demo resource - server - health");
   // console.log(health);
 	// if (health === null) {
 	// 	return error(404, 'Unavailable');
 	// }
-	return { body: demo_resource };
+	console.log("Hello from routes/playground/ms_graph_me/+page.server.ts");
+	// return { body: demo_resource };
 };
