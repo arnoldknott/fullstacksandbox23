778db4023e7180399632f7dd1edcbbdd463e61a3
arnoldknott <knott@dtu.dk>
2024-01-09 08:41:29 +0100

    refactor: eliminates need of msalCLient and redisClient at build stage, cleans up code

--- frontend_svelte/src/lib/server/oauth.ts
+++ frontend_svelte/src/lib/server/oauth.ts
@@ -1,36 +1,49 @@
 import { app_config } from './config';
 import { ConfidentialClientApplication, type AuthenticationResult, type AccountInfo } from '@azure/msal-node';
 
-const configuration = await app_config();
 const scopes = ["User.Read"];
 
-const msalConfig = {
-  auth: {
-    clientId: configuration.app_reg_client_id,
-    authority: configuration.azure_authority, 
-    clientSecret: configuration.app_client_secret,
-    scopes: scopes
-    /***********************************************************************************************************************************
-    Configure persisting the cache to Redis - as soon as Redis is encrypted!
-    https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-node/docs/caching.md#persistent-cache
-    implementation of on-behalf-of-with-distributed-cache:
-    https://github.com/AzureAD/microsoft-authentication-library-for-js/tree/dev/samples/msal-node-samples/on-behalf-of-distributed-cache
-    implementation of on-behalf-of:
-    https://github.com/AzureAD/microsoft-authentication-library-for-js/tree/dev/samples/msal-node-samples/on-behalf-of
-    But not strictly necessary: right now the cache is in a protected network.
-    ***********************************************************************************************************************************/
-  },
+let msalConfClient: ConfidentialClientApplication | null = null;
+
+const createMsalConfClient = async () => {
+  if (!msalConfClient){
+    const configuration = await app_config();
+
+    const msalConfig = {
+      auth: {
+        clientId: configuration.app_reg_client_id,
+        authority: configuration.azure_authority, 
+        clientSecret: configuration.app_client_secret,
+        scopes: scopes
+        /***********************************************************************************************************************************
+        Configure persisting the cache to Redis - as soon as Redis is encrypted!
+        https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-node/docs/caching.md#persistent-cache
+        implementation of on-behalf-of-with-distributed-cache:
+        https://github.com/AzureAD/microsoft-authentication-library-for-js/tree/dev/samples/msal-node-samples/on-behalf-of-distributed-cache
+        implementation of on-behalf-of:
+        https://github.com/AzureAD/microsoft-authentication-library-for-js/tree/dev/samples/msal-node-samples/on-behalf-of
+        But not strictly necessary: right now the cache is in a protected network.
+        ***********************************************************************************************************************************/
+      },
+    }
+  
+    msalConfClient = new ConfidentialClientApplication(msalConfig);
+  }
+  return msalConfClient;
 }
 
-const msalConfClient = new ConfidentialClientApplication(msalConfig);
-
 export const signIn = async ( origin: string): Promise<string> => {
+  // Check if msalClient exists on very first login, if not create it.
+  msalConfClient ? null : await createMsalConfClient()
   const authCodeUrlParameters = {
     scopes: ["User.Read"],
     redirectUri: `${origin}/oauth/callback`
   };
   let authCodeUrl: string
   try {
+    if (!msalConfClient){
+      throw new Error("oauth - Authentication - signIn failed - msalConfClient not initialized");
+    }
     authCodeUrl = await msalConfClient.getAuthCodeUrl(authCodeUrlParameters);
   } catch (err) {
     console.error("oauth - Authentication - signIn failed");
@@ -45,6 +58,9 @@ export const getTokens = async(code: string | null, origin: string): Promise<Aut
     throw new Error("oauth - GetAccessToken failed - no code");
   }
   try {
+    if (!msalConfClient){
+      throw new Error("oauth - Authentication - signIn failed - msalConfClient not initialized");
+    }
     const response = await msalConfClient.acquireTokenByCode({
       code: code,
       scopes: scopes,
@@ -61,6 +77,9 @@ export const getTokens = async(code: string | null, origin: string): Promise<Aut
 }
 
 export const getAccessToken = async ( account: AccountInfo): Promise<string> => {
+  if (!msalConfClient){
+    throw new Error("oauth - Authentication - signIn failed - msalConfClient not initialized");
+  }
   const response = await msalConfClient.acquireTokenSilent({
     scopes: scopes,
     account: account,
@@ -72,6 +91,9 @@ export const getAccessToken = async ( account: AccountInfo): Promise<string> => 
 export const signOut = async ( ): Promise<void> => {
   // TBD: implement logout
   try {
+    if (!msalConfClient){
+      throw new Error("oauth - Authentication - signIn failed - msalConfClient not initialized");
+    }
     // implement logout
   } catch (err) {
     console.error("oauth - Logout failed: ", err);
