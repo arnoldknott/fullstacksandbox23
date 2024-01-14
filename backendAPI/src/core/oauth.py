8d1a028a2c03cb5adc57b6d6cbeb53f2b005c852
arnoldknott <knott@dtu.dk>
2024-01-14 17:02:33 +0100

    chore: cleans up the oauth in the backend and tries to implment OAuth in Swagger UI - bot not a priority right now

--- backendAPI/src/core/oauth.py
+++ backendAPI/src/core/oauth.py
@@ -8,9 +8,11 @@ from core.config import config
 from fastapi import HTTPException, Request
 from jwt.algorithms import RSAAlgorithm
 
-## Use pyjwt to decode the tokens!
-
 logger = logging.getLogger(__name__)
+
+# To get the swagger UI to work, add the OAuth2AuthorizationCodeBearer to the securitySchemes section of the openapi.json file
+# https://github.com/tiangolo/fastapi/pull/797
+# make the relevant routers dependent on it!
 
 
 def get_jwks(no_cache: bool = False):
@@ -37,42 +39,13 @@ def get_jwks(no_cache: bool = False):
     except Exception as err:
         logger.error("🔥 Failed to get JWKS.")
         raise err
-    # try:
-    #     jwks_url = f"https://{AUTH0_DOMAIN}/.well-known/jwks.json"
-    #     jwks = httpx.get(jwks_url)
-    #     jwks.raise_for_status()
-    #     return jwks.json()
-    # except Exception as e:
-    #     logger.error(f"Failed to fetch JWKS: ${e}")
-    #     raise HTTPException(status_code=500, detail="Failed to fetch JWKS")
-    #
-    # Get the OpenID Connect metadata for the tenant:
-    # oidc_config = requests.get(
-    #     f"https://login.microsoftonline.com/{os.environ['AZ_TENANT_ID']}/v2.0/.well-known/openid-configuration"
-    # ).json()
-
-    # # Get the JSON Web Key Set (JWKS) from the OpenID Connect discovery endpoint:
-    # jwks = requests.get(oidc_config["jwks_uri"]).json()
-    # kid = jwt.get_unverified_header(token)["kid"]
-
-    # # Get the key that matches the kid:
-    # rsa_key = {}
-    # for key in jwks["keys"]:
-    #     if key["kid"] == kid:
-    #         rsa_key = RSAAlgorithm.from_jwk(key)
 
 
 def validate_token(request: Request, retries: int = 0):
     """Validates the access token sent in the request header"""
-    # get the token from the header:
-    # print("=== request.headers ===")
-    # print(request.headers)
     logger.info("🔑 Validating token")
     try:
-        # request.headers.get("Authorization").split("Bearer ")[1]
         authHeader = request.headers.get("Authorization")
-        # print("=== authHeader ===")
-        # print(authHeader)
         token = authHeader.split("Bearer ")[1]
         if token:
             jwks = get_jwks()
@@ -83,16 +56,9 @@ def validate_token(request: Request, retries: int = 0):
             for key in jwks["keys"]:
                 if key["kid"] == kid:
                     rsa_key = RSAAlgorithm.from_jwk(key)
-            # print("=== rsa_key ===")
-            # print(rsa_key)
-            # print("=== token ===")
-            # print(token)
-            # # print("=== config.APP_REG_CLIENT_ID ===")
-            # print(config.APP_REG_CLIENT_ID)
-            # print("=== config.AZURE_ISSUER_URL ===")
-            # print(config.AZURE_ISSUER_URL)
             logger.info("Decoding token")
-            payload = jwt.decode(
+            # validate the token
+            jwt.decode(
                 token,
                 rsa_key,
                 algorithms=["RS256"],
@@ -107,26 +73,10 @@ def validate_token(request: Request, retries: int = 0):
                 },
             )
             logger.info("Token decoded successfully")
-            print("=== payload ===")
-            print(payload)
+            # print("=== payload ===")
+            # print(payload)
 
             return True
-            # Try validating token first with cached keys - if no success, fetch new keys, put them in the cache and try again!
-            # print(token)
-            # print("=== get_jwks() ===")
-            # print(get_jwks())
-        # jwt.decode(
-        #         token,
-        #         rsa_key,
-        #         algorithms=["RS256"],
-        #         audience=os.environ["AZ_CLIENT_ID"],
-        #         issuer=f"https://login.microsoftonline.com/{os.environ['AZ_TENANT_ID']}/v2.0",
-        #         options={
-        #             "validate_iss": True,
-        #             "validate_aud": True,
-        #             "validate_exp": True,
-        #         },
-        #     )
     except Exception as e:
         # only one retry allowed: by now the tokens should be cached!
         if retries < 1:
--- backendAPI/src/routers/api/v1/protected_resource.py
+++ backendAPI/src/routers/api/v1/protected_resource.py
@@ -7,9 +7,32 @@ from fastapi import APIRouter, Depends
 logger = logging.getLogger(__name__)
 router = APIRouter()
 
+# TBD: add OAuth2AuthorizationCodeBearer, asks for client_id and client_secret
+# needs scopes
+# primarily this is relevant for Swagger UI, API can be accessed by other tools right now, as long as
+# their callback URL is registered in the Azure AD app registration!
+# oauth2_scheme = OAuth2AuthorizationCodeBearer(
+#     authorizationUrl="https://login.microsoftonline.com/f251f123-c9ce-448e-9277-34bb285911d9/oauth2/v2.0/authorize",
+#     tokenUrl="https://login.microsoftonline.com/f251f123-c9ce-448e-9277-34bb285911d9/oauth2/token",
+#     scopes={
+#         "api://{-- put API scope here -- }/api.read": "Read API",
+#         "api://{-- put API scope here -- }/api.write": "Write API",
+#     },
+# )
 
+
+# This is secure and works!
 @router.get("/")
 def get_protected_resource(token: Annotated[str, Depends(validate_token)]):
     """Returns a protected resource."""
     logger.info("GET protected resource")
     return {"message": "Hello from a protected resource!"}
+
+
+# @router.get("/oauth")
+# def get_protected_resource_oauth(token: Annotated[str, Depends(oauth2_scheme)]):
+#     """Returns a protected resource."""
+#     logger.info("GET protected resource")
+#     return {
+#         "message": "Hello from another protected resource - enabling oAuth2 on Swagger UI!"
+#     }
