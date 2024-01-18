2ea0727889a15f3db11c40e61a839689d749c860
arnoldknott <knott@dtu.dk>
2024-01-18 22:52:34 +0100

    test: monitoring config variables for cache

--- backendAPI/src/core/cache.py
+++ backendAPI/src/core/cache.py
@@ -1,9 +1,19 @@
 import redis
 from core.config import config
 
+print("=== cache.py started ===")
+print("=== config.REDIS_HOST ===")
+print(config.REDIS_HOST)
+print("=== config.REDIS_PORT ===")
+print(config.REDIS_PORT)
+print("=== config.REDIS_JWKS_DB ===")
+print(config.REDIS_JWKS_DB)
+
 redis_jwks_client = redis.Redis(
     host=config.REDIS_HOST,
     port=config.REDIS_PORT,
     password=config.REDIS_PASSWORD,
     db=config.REDIS_JWKS_DB,
 )
+
+print("=== cache.py finished ===")
