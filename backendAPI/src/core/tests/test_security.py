c62cefd8c4d089ed6f322c11bb3177ad7f61fc24
arnoldknott <knott@dtu.dk>
2024-02-20 21:29:02 +0100

    fix: fixes formating

--- backendAPI/src/core/tests/test_security.py
+++ backendAPI/src/core/tests/test_security.py
@@ -7,14 +7,14 @@ from crud.user import UserCRUD
 from fastapi.encoders import jsonable_encoder
 from core.security import Guards
 from httpx import AsyncClient
-from models.user import User, UserRead
+# from models.user import User, UserRead
 from tests.utils import (
     # token_payload_roles_user,
     # token_payload_scope_api_write,
     token_payload_user_id,
     token_payload_tenant_id,
     token_payload_one_group,
-    token_payload_many_groups,
+    # token_payload_many_groups,
     one_test_user,
 )
 
