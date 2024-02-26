3b63a54fe9ae0ff342f126dc5ad74a46b45ed4f2
arnoldknott <knott@dtu.dk>
2024-02-26 16:23:45 +0100

    refactor: prepares a class BasicView which specific resource views can inheret from

---
+++ backendAPI/src/routers/api/v1/base.py
@@ -0,0 +1,41 @@
+import logging
+
+from models.user import UserRead
+
+logger = logging.getLogger(__name__)
+
+
+class BaseView:
+    """Base class for all views"""
+
+    def __init__(self, crud):
+        self.crud = crud
+        # using private attribute to avoid direct access
+        # note Python is not strict on that with _BaseView__update_last_access
+        # this variable can still be accessed from the outside
+        self.__update_last_access = True
+
+    # TBD: Replace the owner_id with the model from access control table
+    def updates_last_access(
+        self, admin: bool, current_user: UserRead, owner_id: str
+    ) -> None:
+        logger.info("POST updated_last_access")
+        if (admin is True) and (str(current_user.user_id) != str(owner_id)):
+            self.__update_last_access = False
+        return self.__update_last_access
+
+
+# consider implementing state and/or strategy pattern with extends and implements (if available in Python)?
+class BasePOST(BaseView):
+    """Base class for POST views"""
+
+    def __init__(self, crud):
+        # makes the attributes of the BaseView class available:
+        super().__init__(crud)
+
+    # depends on the __updates_last_access attribute of the BaseView class
+    async def create(self, object):
+        logger.info("POST calls create")
+        async with self.crud() as crud:
+            created_object = await crud.create(object, self.__update_last_access)
+        return created_object
--- backendAPI/src/routers/api/v1/user.py
+++ backendAPI/src/routers/api/v1/user.py
@@ -15,13 +15,13 @@ logger = logging.getLogger(__name__)
 router = APIRouter()
 
 
+# note: self-sign-up through security - controlled by token content,
+# that means, controlled by group and user membership from Azure Identity Provider!
+# The roles assigned to users and groups decide about sign-up here.
+# This function allows admins to sign up users through API on top of that.
 @router.post("/", status_code=201)
 async def post_user(
     user: UserCreate,
-    # note: self-sign-up through security - controlled by token content,
-    # that means, controlled by group and user membership in azure entra ad!
-    # The roles assigned to users and groups decide about sign-up here.
-    # This function allows admins to sign up users through API on top of that.
     _1=Depends(CurrentAzureTokenHasScope("api.write")),
     _2=Depends(CurrentAzureTokenHasRole("Admin")),
 ) -> User:
@@ -30,6 +30,34 @@ async def post_user(
     async with UserCRUD() as crud:
         created_user = await crud.create(user)
     return created_user
+
+
+# First experiment to work with BasePOST class inherited from BaseView
+# # TBD: using the BasePOST view class should replace the above implementation!
+# @router.post("/", status_code=201)
+# async def post_user(
+#     user: UserCreate,
+#     _1=Depends(CurrentAzureTokenHasScope("api.write")),
+#     _2=Depends(CurrentAzureTokenHasRole("Admin")),
+# ) -> User:
+#     """Creates a new user."""
+#     # uses the default for updated_last_access:
+#     return BasePOST(UserCRUD).create(user)
+
+
+# # this is an example on how to utilize the methods from the BaseView class through BasePOST instance
+# # note: that's not relevant for the POST method above, but for GET and PUT below
+# @router.post("/admin_prevents_updated_last_access", status_code=201)
+# async def post_user(
+#     user: UserCreate,
+#     _1=Depends(CurrentAzureTokenHasScope("api.write")),
+#     check_admin_role=Depends(CurrentAzureTokenHasRole("Admin")),
+# ) -> User:
+#     """Creates a new user."""
+#     post_view = BasePOST(UserCRUD)
+#     # executes the logic for determining the updated_last_access:
+#     # post_view.updates_last_access(check_admin_role, current_user, user.user_id)
+#     return post_view.create(user)
 
 
 @router.get("/")
@@ -101,7 +129,6 @@ async def get_user_by_id(
     return response
 
 
-# TBD: write tests for this:
 @router.put("/{user_id}")
 async def update_user(
     user_id: str,
@@ -111,8 +138,6 @@ async def update_user(
     check_admin_role=Depends(CurrentAzureTokenHasRole("Admin", require=False)),
 ) -> User:
     """Updates a user."""
-    print("=== check_admin_role ===")
-    print(check_admin_role)
     if (str(current_user.user_id) != str(user_id)) and (check_admin_role is False):
         raise HTTPException(status_code=403, detail="Access denied")
 
@@ -133,7 +158,6 @@ async def update_user(
     return updated_user
 
 
-# TBD: write tests for this:
 @router.delete("/{user_id}")
 async def delete_user(
     user_id: str,
