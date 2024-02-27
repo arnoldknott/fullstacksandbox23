01ac50e5af87c85551bb2802107dc4668c663c91
arnoldknott <knott@dtu.dk>
2024-02-27 10:48:11 +0100

    chore: adds field last_accessed_at to protected resource and adds migration scripts from dev

--- backendAPI/src/models/protected_resource.py
+++ backendAPI/src/models/protected_resource.py
@@ -7,17 +7,21 @@ from sqlmodel import Field, SQLModel
 class ProtectedResourceCreate(SQLModel):
     name: str
     description: Optional[str] = None
+    last_accessed_at: Optional[datetime] = datetime.now()
 
 
 class ProtectedResource(ProtectedResourceCreate, table=True):
     protected_resource_id: Optional[int] = Field(default=None, primary_key=True)
     created_at: datetime = Field(default=datetime.now())
     last_updated_at: datetime = Field(default=datetime.now())
+    last_accessed_at: datetime = Field(default=datetime.now())
+
     # Note: so far all times are UTC!
 
 
 class ProtectedResourceRead(ProtectedResourceCreate):
     demo_resource_id: int
+    last_accessed_at: datetime
 
 
 class ProtectedResourceUpdate(ProtectedResourceCreate):
