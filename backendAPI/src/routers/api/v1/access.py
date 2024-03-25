a48549d627d9368a6ab4c4eed795721fb3adf3d8
arnoldknott <knott@dtu.dk>
2024-03-25 22:30:51 +0100

    doc: adds comments for implementing access endpoints

--- backendAPI/src/routers/api/v1/access.py
+++ backendAPI/src/routers/api/v1/access.py
@@ -12,10 +12,15 @@
 # - create (public share)
 # - make a generic for the above two!
 # - read (check who has access)
+# - read by resource - use filter
+# - read by identity - use filter
+# - change access Action (own, write, read) -> use delete and create
 # - delete (unshare)
 # - hierarchy (inheritance)?
 # AccessLogs:
 # - read (check who accessed what)
 # - read first "own": corresponds to create
-# - read last access
-# - read access count
+# - read last access - use order_by
+# - read access count - use func.count
+# - read access by identity - use filter
+# - read access by resource - use filter
