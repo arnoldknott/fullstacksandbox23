8b7832a24ff70fbf6ab54b0d7b772743c1d0f250
arnoldknott <knott@dtu.dk>
2024-04-16 23:38:57 +0200

    chore: cleans up the user and demo-resource endpoints

--- backendAPI/src/routers/api/v1/demo_resource.py
+++ backendAPI/src/routers/api/v1/demo_resource.py
@@ -24,37 +24,7 @@ logger = logging.getLogger(__name__)
 router = APIRouter()
 
 
-# class DemoResourceView(BaseView):
-#     """Extends base view with demo resource specific methods."""
-
-#     def __init__(self):
-#         super().__init__(DemoResourceCRUD, DemoResource)
-
-#     async def add_tag(self, resource_id: UUID, tag_ids: List[UUID]):
-#         """Adds a tag to a demo resource."""
-#         async with self.crud() as crud:
-#             return await crud.add_tag(resource_id, tag_ids)
-
-
 demo_resource_view = BaseView(DemoResourceCRUD, DemoResource)
-# demo_resource_view = DemoResourceView()
-
-# TBD: remove old version from before refactoring:
-# @router.post("/", status_code=201)
-# async def post_demo_resource(
-#     demo_resource: DemoResourceCreate,
-# ) -> DemoResource:
-#     """Creates a new demo resource."""
-#     logger.info("POST demo resource")
-#     # print("=== demo_resource.category_id ===")
-#     # print(demo_resource.category_id)
-#     if demo_resource.category_id:
-#         async with CategoryCRUD() as crud:
-#             await crud.read_by_id(demo_resource.category_id)
-#     async with DemoResourceCRUD() as crud:
-#         created_demo_resource = await crud.create(demo_resource)
-#     return created_demo_resource
-#     # crud = DemoResourceCRUD()
 
 
 # Post requires a user!
@@ -65,41 +35,7 @@ async def post_category(
     guards: GuardTypes = Depends(Guards(scopes=["api.write"], roles=["User"])),
 ) -> DemoResourceRead:
     """Creates a new demo resource."""
-    demo_resource_db = await demo_resource_view.post(
-        demo_resource, token_payload, guards
-    )
-    return demo_resource_db
-    # TBD: remove this! That should be the return value of the post method!
-    # if demo_resource_db.category_id:
-    #     await get_category_by_id(demo_resource_db.category_id, token_payload)
-    # return demo_resource_db
-
-
-# TBD: remove old version from before refactoring:
-# @router.get("/")
-# async def get_all_demo_resources() -> List[DemoResource]:
-#     """Returns all demo resource."""
-#     logger.info("GET all demo resource")
-#     async with DemoResourceCRUD() as crud:
-#         response = await crud.read_all()
-#     # crud = DemoResourceCRUD()
-#     # response = await crud.read_all()
-#     return response
-
-
-# @router.get("/{resource_id}")
-# async def get_demo_resource_by_id(resource_id: UUID) -> DemoResourceRead:
-#     """Returns a demo resource."""
-#     logger.info("GET demo resource")
-#     # crud = DemoResourceCRUD()
-#     try:
-#         resource_id = UUID(resource_id)
-#     except ValueError:
-#         logger.error("Resource ID is not a universal unique identifier (uuid).")
-#         raise HTTPException(status_code=400, detail="Invalid resource id")
-#     async with DemoResourceCRUD() as crud:
-#         response = await crud.read_by_id_with_childs(resource_id)
-#     return response
+    return await demo_resource_view.post(demo_resource, token_payload, guards)
 
 
 # The get functions are totally public
@@ -126,28 +62,6 @@ async def get_demo_resource_by_id(
     return await demo_resource_view.get_by_id(demo_resource_id, token_payload)
 
 
-# TBD: remove the old version after refactoring:
-# @router.put("/{resource_id}")
-# async def update_demo_resource(
-#     resource_id: UUID,
-#     demo_resource: DemoResourceUpdate,
-# ) -> DemoResource:
-#     """Updates a demo resource."""
-#     logger.info("PUT demo resource")
-#     # print("=== demo_resource ===")
-#     # print(demo_resource)
-#     # crud = DemoResourceCRUD()
-#     try:
-#         resource_id = UUID(resource_id)
-#     except ValueError:
-#         logger.error("Resource ID is not a universal unique identifier (uuid).")
-#         raise HTTPException(status_code=400, detail="Invalid resource id")
-#     async with DemoResourceCRUD() as crud:
-#         old_resource = await crud.read_by_id(resource_id)
-#         updated_resource = await crud.update(old_resource, demo_resource)
-#     return updated_resource
-
-
 @router.put("/{demo_resource_id}", status_code=200)
 async def put_category(
     demo_resource_id: UUID,
@@ -159,24 +73,6 @@ async def put_category(
     return await demo_resource_view.put(
         demo_resource_id, demo_resource, token_payload, guards
     )
-
-
-# # TBD: remove the old version after refactoring:
-# @router.delete("/{resource_id}")
-# async def delete_demo_resource(resource_id: UUID) -> DemoResource:
-#     """Deletes a demo resource."""
-#     logger.info("DELETE demo resource")
-#     # crud = DemoResourceCRUD()
-#     try:
-#         resource_id = UUID(resource_id)
-#     except ValueError:
-#         logger.error("Resource ID is not a universal unique identifier (uuid).")
-#         raise HTTPException(status_code=400, detail="Invalid resource id")
-#     async with DemoResourceCRUD() as crud:
-#         result = await crud.delete(resource_id)
-#     # print("=== result ===")
-#     # print(result)
-#     return result
 
 
 @router.delete("/{demo_resource_id}", status_code=200)
@@ -195,23 +91,9 @@ async def delete_demo_resource(
 # like sharing, tagging, creating hierarchies etc.
 @router.post("/{resource_id}/tag/")
 async def add_tag_to_demo_resource(
-    resource_id: UUID,  # TBD consider using Annotated for the id checks!
+    resource_id: UUID,
     tag_ids: Annotated[List[UUID], Query()],
-    # TBD: move the arguments from Query to json!
-    # as the tags are queries, no body is required. This could be a GET request!!
 ) -> DemoResourceRead:
     """Adds a tag to a demo resource."""
     async with demo_resource_view.crud() as crud:
         return await crud.add_tag(resource_id, tag_ids)
-    # # logger.info("POST demo resource")
-    # # try:
-    # #     resource_id = uuid.UUID(resource_id)
-    # #     # for tag_id in tag_ids:
-    # #     #     uuid.UUID(tag_id)
-    # # except ValueError:
-    # #     logger.error("ID's must by a universal unique identifier (uuid).")
-    # #     raise HTTPException(status_code=400, detail="Invalid id")
-    # return await demo_resource_view.add_tag(resource_id, tag_ids)
-    # # async with DemoResourceCRUD() as crud:
-    # #     result = await crud.add_tag(resource_id, tag_ids)
-    # # return result
--- backendAPI/src/routers/api/v1/user.py
+++ backendAPI/src/routers/api/v1/user.py
@@ -14,31 +14,6 @@ router = APIRouter()
 user_view = BaseView(UserCRUD, User)
 
 
-# class UserView(BaseView):
-#     def __init__(self):
-#         super().__init__(UserCRUD, User)
-
-#     async def get_by_azure_user_id(
-#         self,
-#         azure_user_id: UUID,
-#         token_payload=None,
-#         guards=None,  #: GuardTypes = Depends(Guards(roles=["User"])),
-#     ) -> UserRead:
-#         """GET view to retrieve user by azure_user_id."""
-#         logger.info("GET user by azure_user_id")
-#         current_user = await self._check_token_against_guards(token_payload, guards)
-#         async with self.crud() as crud:
-#             user = await crud.read_by_azure_user_id(azure_user_id, current_user)
-#         return user
-
-
-# user_view = UserView()
-
-# this filter definition needs to move to the specific CRUD:
-# filters = [User.azure_user_id == azure_user_id]
-# return await self.(filters=filters, token_payload=token_payload, roles=roles)
-
-
 # note: self-sign-up through security - controlled by token content,
 # that means, controlled by group and user membership from Azure Identity Provider!
 # The roles assigned to users and groups decide about sign-up here.
@@ -58,7 +33,6 @@ user_view = BaseView(UserCRUD, User)
 #     return created_user
 
 
-# TBD: Refactor with AccessControl:
 @router.post("/", status_code=201)
 async def post_user(
     user: UserCreate,
@@ -74,96 +48,15 @@ async def post_user(
     )
 
 
-# First experiment to work with BasePOST class inherited from BaseView
-# # TBD: using the BasePOST view class should replace the above implementation!
-# @router.post("/", status_code=201)
-# async def post_user(
-#     user: UserCreate,
-#     _1=Depends(CurrentAccessTokenHasScope("api.write")),
-#     _2=Depends(CurrentAccessTokenHasRole("Admin")),
-# ) -> User:
-#     """Creates a new user."""
-#     # uses the default for updated_last_access:
-#     return BasePOST(UserCRUD).create(user)
-
-
-# # this is an example on how to utilize the methods from the BaseView class through BasePOST instance
-# # note: that's not relevant for the POST method above, but for GET and PUT below
-# @router.post("/admin_prevents_updated_last_access", status_code=201)
-# async def post_user(
-#     user: UserCreate,
-#     _1=Depends(CurrentAccessTokenHasScope("api.write")),
-#     check_admin_role=Depends(CurrentAccessTokenHasRole("Admin")),
-# ) -> User:
-#     """Creates a new user."""
-#     pass an instance of guards and the CRUD to the BasePOST instance:
-#     post_view = BasePOST(guards, UserCRUD)
-#     # executes the logic for determining the updated_last_access:
-#     # post_view.updates_last_access(check_admin_role, current_user, user.user_id)
-#     return post_view.create(user)
-
-
-# # TBD: delete old version after refactoring with BaseView:
-# @router.get("/")
-# async def get_all_users(
-#     _=Depends(CurrentAccessTokenHasRole("Admin")),
-# ) -> List[User]:
-#     """Returns all user."""
-#     logger.info("GET all user")
-#     async with UserCRUD() as crud:
-#         response = await crud.read_all()
-#     return response
-
-
 @router.get("/", status_code=200)
 async def get_all_users(
     token_payload=Depends(get_access_token_payload),
     guards: GuardTypes = Depends(Guards(roles=["Admin"])),
 ) -> list[UserRead]:
     """Returns all users."""
-    return await user_view.get(
-        token_payload,
-        guards,
-    )
+    return await user_view.get(token_payload, guards)
 
 
-# # TBD: Delete old version after refactoring with BaseView:
-# @router.get("/azure/{azure_user_id}")
-# async def get_user_by_azure_user_id(
-#     azure_user_id: UUID,
-#     current_user: UserRead = Depends(CurrentAzureUserInDatabase()),
-#     check_admin_role=Depends(CurrentAccessTokenHasRole("Admin", require=False)),
-# ) -> UserRead:
-#     """Returns a user based on its azure user id."""
-#     if (str(current_user.azure_user_id) != str(azure_user_id)) and (
-#         check_admin_role is False
-#     ):
-#         raise HTTPException(status_code=403, detail="Access denied")
-
-#     logger.info("GET user")
-
-#     # TBD: Refactor into access control
-#     # update_last_access = True
-#     # if (check_admin_role is True) and (
-#     #     str(current_user.azure_user_id) != str(azure_user_id)
-#     # ):
-#     #     update_last_access = False
-
-#     try:
-#         azure_user_id = UUID(azure_user_id)
-#     except ValueError:
-#         logger.error("User ID is not an UUID")
-#         raise HTTPException(status_code=400, detail="Invalid user id")
-#     async with UserCRUD() as crud:
-#         response = await crud.read_by_azure_user_id_with_childs(
-#             azure_user_id
-#             # TBD; Refactor into access control
-#             # , update_last_access
-#         )
-#     return response
-
-
-### TBD: Should be ready to go:
 @router.get("/azure/{azure_user_id}", status_code=200)
 async def get_user_by_azure_user_id(
     azure_user_id: UUID,
@@ -176,35 +69,6 @@ async def get_user_by_azure_user_id(
     async with user_view.crud() as crud:
         user = await crud.read_by_azure_user_id(azure_user_id, current_user)
     return user
-    # return await user_view.get_by_azure_user_id(azure_user_id, token_payload, guards)
-
-
-# # TBD: Delete old version after refactoring with BaseView:
-# @router.get("/{user_id}")
-# async def get_user_by_id(
-#     user_id: UUID,
-#     current_user: UserRead = Depends(CurrentAzureUserInDatabase()),
-#     check_admin_role=Depends(CurrentAccessTokenHasRole("Admin", require=False)),
-# ) -> UserRead:
-#     """Returns a user with a specific user_id."""
-#     if (str(current_user.id) != str(user_id)) and (check_admin_role is False):
-#         raise HTTPException(status_code=403, detail="Access denied")
-
-#     logger.info("GET user")
-
-#     # TBD: Refactor into access control
-#     # update_last_access = True
-#     # if (check_admin_role is True) and (str(current_user.id) != str(user_id)):
-#     #     update_last_access = False
-
-#     try:
-#         user_id = UUID(user_id)
-#     except ValueError:
-#         logger.error("User ID is not an UUID")
-#         raise HTTPException(status_code=400, detail="Invalid user id")
-#     async with UserCRUD() as crud:
-#         response = await crud.read_by_id_with_childs(user_id)  # , update_last_access)
-#     return response
 
 
 @router.get("/{user_id}", status_code=200)
@@ -221,39 +85,6 @@ async def get_user_by_id(
     )
 
 
-# # TBD: Delete old version after refactoring with BaseView:
-# @router.put("/{user_id}")
-# async def update_user(
-#     user_id: UUID,
-#     user_data_update: UserUpdate,
-#     current_user: UserRead = Depends(CurrentAzureUserInDatabase()),
-#     _=Depends(CurrentAccessTokenHasScope("api.write")),
-#     check_admin_role=Depends(CurrentAccessTokenHasRole("Admin", require=False)),
-# ) -> User:
-#     """Updates a user."""
-#     if (str(current_user.id) != str(user_id)) and (check_admin_role is False):
-#         raise HTTPException(status_code=403, detail="Access denied")
-
-#     logger.info("PUT user")
-
-#     # TBD: Refactor into access control
-#     # update_last_access = True
-#     # if check_admin_role is True:
-#     #     update_last_access = False
-
-#     try:
-#         user_id = UUID(user_id)
-#     except ValueError:
-#         logger.error("User ID is not an UUID")
-#         raise HTTPException(status_code=400, detail="Invalid user id")
-#     async with UserCRUD() as crud:
-#         old_user = await crud.read_by_id(user_id)
-#         updated_user = await crud.update(
-#             old_user, user_data_update
-#         )  # , update_last_access)
-#     return updated_user
-
-
 @router.put("/{user_id}", status_code=200)
 async def put_user(
     user_id: UUID,
@@ -268,30 +99,6 @@ async def put_user(
         token_payload,
         guards,
     )
-
-
-# # TBD: Delete old version after refactoring with BaseView:
-# @router.delete("/{user_id}")
-# async def delete_user(
-#     user_id: UUID,
-#     current_user: UserRead = Depends(CurrentAzureUserInDatabase()),
-#     _=Depends(CurrentAccessTokenHasScope("api.write")),
-#     check_admin_role=Depends(CurrentAccessTokenHasRole("Admin", require=False)),
-# ) -> User:
-#     """Deletes a user."""
-#     if (str(current_user.id) != str(user_id)) and (check_admin_role is False):
-#         raise HTTPException(status_code=403, detail="Access denied")
-
-#     logger.info("DELETE user")
-
-#     try:
-#         user_id = UUID(user_id)
-#     except ValueError:
-#         logger.error("User ID is not an UUID")
-#         raise HTTPException(status_code=400, detail="Invalid user id")
-#     async with UserCRUD() as crud:
-#         result = await crud.delete(user_id)
-#     return result
 
 
 @router.delete("/{user_id}", status_code=200)
