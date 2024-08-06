9657d8c6618132584911ce50e7cb46bcd7f22419
arnoldknott <knott@dtu.dk>
2024-08-06 18:31:18 +0200

    ci: update docker compose commands

--- scripts/enter_backend_test.sh
+++ scripts/enter_backend_test.sh
@@ -2,5 +2,5 @@
 # after a commit the pre-commit hook will run and stops the test containers
 # while developing tests, the backend container is usefull to have running
 
-COMPOSE_PROJECT_NAME=test-fssb23 docker-compose -f compose.yml -f compose.override.test.yml --env-file backendAPI/src/tests/.env up -d  
-COMPOSE_PROJECT_NAME=test-fssb23 docker-compose -f compose.yml -f compose.override.test.yml --env-file backendAPI/src/tests/.env exec backend_api sh -c "/bin/sh"
+COMPOSE_PROJECT_NAME=test-fssb23 docker compose -f compose.yml -f compose.override.test.yml --env-file backendAPI/src/tests/.env up -d  
+COMPOSE_PROJECT_NAME=test-fssb23 docker compose -f compose.yml -f compose.override.test.yml --env-file backendAPI/src/tests/.env exec backend_api sh -c "/bin/sh"
--- scripts/start_backendAPI_test.sh
+++ scripts/start_backendAPI_test.sh
@@ -5,6 +5,6 @@
 # to start all over pre-commit hookl might be handy!
 REPO_ROOT_DIR=$(git rev-parse --show-toplevel)
 cd $REPO_ROOT_DIR
-COMPOSE_PROJECT_NAME=test-fssb23 docker-compose -f compose.yml -f compose.override.test.yml --env-file backendAPI/src/tests/.env up -d
-COMPOSE_PROJECT_NAME=test-fssb23 docker-compose -f compose.yml -f compose.override.test.yml --env-file backendAPI/src/tests/.env exec backend_api sh -c "/bin/sh"
+COMPOSE_PROJECT_NAME=test-fssb23 docker compose -f compose.yml -f compose.override.test.yml --env-file backendAPI/src/tests/.env up -d
+COMPOSE_PROJECT_NAME=test-fssb23 docker compose -f compose.yml -f compose.override.test.yml --env-file backendAPI/src/tests/.env exec backend_api sh -c "/bin/sh"
 # just run `pytest -v` in the container
