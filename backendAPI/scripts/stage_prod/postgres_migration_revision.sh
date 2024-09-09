80519f8eac48ba4c1bad730ffe391b0c4e6216dc
arnoldknott <knott@dtu.dk>
2024-09-09 12:37:34 +0200

    ci: changes capturing of migration filename from redirect into file to saving alembic output in environmental variable

--- backendAPI/scripts/stage_prod/postgres_migration_revision.sh
+++ backendAPI/scripts/stage_prod/postgres_migration_revision.sh
@@ -10,8 +10,12 @@ mkdir -p $VERSIONSFILE_PATH
 cd /app/src
 echo "=== COMMMIT_SHA: \"$COMMIT_SHA\" ==="
 echo "=== LOGFILE_PATH: \"$LOGFILE_PATH\" ==="
-alembic -c alembic_stage_prod.ini revision --autogenerate -m \"$COMMIT_SHA\" > $LOGFILE_PATH/revision_$COMMIT_SHA.log
-MIGRATION_SCRIPT_FILENAME=$(cat $LOGFILE_PATH/revision_$COMMIT_SHA.log | awk '/Generating /{print $2}')
+# alembic -c alembic_stage_prod.ini revision --autogenerate -m \"$COMMIT_SHA\" > $LOGFILE_PATH/revision_$COMMIT_SHA.log
+MIGRATION_GENERATION_LOG=$(alembic -c alembic_stage_prod.ini revision --autogenerate -m \"$COMMIT_SHA\")
+echo $MIGRATION_GENERATION_LOG > $LOGFILE_PATH/revision_$COMMIT_SHA.log
+echo "=== MIGRATION_GENERATION_LOG: \"$MIGRATION_GENERATION_LOG\" ==="
+# MIGRATION_SCRIPT_FILENAME=$(cat $LOGFILE_PATH/revision_$COMMIT_SHA.log | awk '/Generating /{print $2}')
+MIGRATION_SCRIPT_FILENAME=$(echo $MIGRATION_GENERATION_LOG | awk '/Generating /{print $2}')
 echo "=== MIGRATION_SCRIPT_FILENAME: \"$MIGRATION_SCRIPT_FILENAME\" ==="
 echo "=== VERSIONSFILE_PATH: \"$VERSIONSFILE_PATH\" ==="
 cp $MIGRATION_SCRIPT_FILENAME $VERSIONSFILE_PATH
