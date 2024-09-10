5e07468b22bc3381d221d4a0f93b77faa4750478
arnoldknott <knott@dtu.dk>
2024-09-10 16:18:09 +0200

    ci: comment debugging information in migration script

--- backendAPI/scripts/stage_prod/postgres_migration_revision.sh
+++ backendAPI/scripts/stage_prod/postgres_migration_revision.sh
@@ -8,17 +8,16 @@ VERSIONSFILE_PATH="/data/migrations/stage_prod/versions"
 
 mkdir -p $VERSIONSFILE_PATH
 cd /app/src
-echo "=== COMMIT_SHA: \"$COMMIT_SHA\" ==="
-echo "=== LOGFILE_PATH: \"$LOGFILE_PATH\" ==="
+# echo "=== COMMIT_SHA: \"$COMMIT_SHA\" ==="
+# echo "=== LOGFILE_PATH: \"$LOGFILE_PATH\" ==="
 # alembic -c alembic_stage_prod.ini revision --autogenerate -m \"$COMMIT_SHA\" > $LOGFILE_PATH/revision_$COMMIT_SHA.log
 MIGRATION_GENERATION_LOG=$(alembic -c alembic_stage_prod.ini revision --autogenerate -m \"$COMMIT_SHA\")
 echo $MIGRATION_GENERATION_LOG > $LOGFILE_PATH/revision_$COMMIT_SHA.log
-echo "=== MIGRATION_GENERATION_LOG: \"$MIGRATION_GENERATION_LOG\" ==="
-# MIGRATION_SCRIPT_FILENAME=$(cat $LOGFILE_PATH/revision_$COMMIT_SHA.log | awk '/Generating /{print $2}')
+# echo "=== MIGRATION_GENERATION_LOG: \"$MIGRATION_GENERATION_LOG\" ==="
 MIGRATION_SCRIPT_FILENAME=$(cat $LOGFILE_PATH/revision_$COMMIT_SHA.log | sed -n 's/.*Generating \(.*\) ... done/\1/p' | tr -d ' ')
 # MIGRATION_SCRIPT_FILENAME=$(echo $MIGRATION_GENERATION_LOG | awk '/Generating /{print $2}')
-echo "=== MIGRATION_SCRIPT_FILENAME: \"$MIGRATION_SCRIPT_FILENAME\" ==="
-echo "=== VERSIONSFILE_PATH: \"$VERSIONSFILE_PATH\" ==="
+# echo "=== MIGRATION_SCRIPT_FILENAME: \"$MIGRATION_SCRIPT_FILENAME\" ==="
+# echo "=== VERSIONSFILE_PATH: \"$VERSIONSFILE_PATH\" ==="
 cp $MIGRATION_SCRIPT_FILENAME $VERSIONSFILE_PATH
 echo $MIGRATION_SCRIPT_FILENAME > $LOGFILE_PATH/revision_filename_$COMMIT_SHA.log
 # mv $LOGFILE_PATH/revision_$COMMIT_SHA.log /data/migrations/stage_prod/logs
