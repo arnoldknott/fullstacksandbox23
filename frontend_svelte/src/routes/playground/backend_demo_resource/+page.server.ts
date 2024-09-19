e2dd8f44a31ce287cd548b0224e7a7e4c43d354c
arnoldknott <knott@dtu.dk>
2024-09-19 12:47:02 +0200

    chore: avoids redirect when calling backendAPi from frontend to retrieve demo resources

--- frontend_svelte/src/routes/playground/backend_demo_resource/+page.server.ts
+++ frontend_svelte/src/routes/playground/backend_demo_resource/+page.server.ts
@@ -7,7 +7,7 @@ const appConfig = await AppConfig.getInstance();
 export const load: PageServerLoad = async ({fetch, locals}) => {
 	// either send a token or make the demo resource publically accessable by adding an access policy with flag public=True
 	const accessToken = await getAccessToken(locals.sessionData, [`${appConfig.api_scope}/api.read`]);
-	const response = await fetch(`${appConfig.backend_origin}/api/v1/demoresource`, {
+	const response = await fetch(`${appConfig.backend_origin}/api/v1/demoresource/`, {
 		headers: {
 			Authorization: `Bearer ${accessToken}`
 		}
