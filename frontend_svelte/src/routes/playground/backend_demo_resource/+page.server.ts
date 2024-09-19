c8cb888cfd1ec6cb0b936a64ca29d1af3af54380
arnoldknott <knott@dtu.dk>
2024-09-19 11:04:57 +0200

    chore: accessses list of demo resources from frontend with token instead of counting on public access policies for the demo resources

--- frontend_svelte/src/routes/playground/backend_demo_resource/+page.server.ts
+++ frontend_svelte/src/routes/playground/backend_demo_resource/+page.server.ts
@@ -1,10 +1,17 @@
 import type { PageServerLoad } from './$types';
+import { getAccessToken } from '$lib/server/oauth';
 import AppConfig from '$lib/server/config';
 
 const appConfig = await AppConfig.getInstance();
 
-export const load: PageServerLoad = async () => {
-	const response = await fetch(`${appConfig.backend_origin}/api/v1/demoresource`);
+export const load: PageServerLoad = async ({fetch, locals}) => {
+	// either send a token or make the demo resource publically accessable by adding an access policy with flag public=True
+	const accessToken = await getAccessToken(locals.sessionData, [`${appConfig.api_scope}/api.read`]);
+	const response = await fetch(`${appConfig.backend_origin}/api/v1/demoresource`, {
+		headers: {
+			Authorization: `Bearer ${accessToken}`
+		}
+    });
 	const demoResources = await response.json();
 	return { body: demoResources };
 };
