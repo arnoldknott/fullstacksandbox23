e6026489d0a1504ee4630fbd7ffbe1b710b27492
arnoldknott <knott@dtu.dk>
2024-10-04 13:29:59 +0200

    test: adds a job for formating the infrastructure code in the pipeline and comments the infrastructure fmt in pre-commit

---
+++ infrastructure/scripts/init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "=== Running: ==="
+echo "=== tofu - init ==="
+
+echo "=== tofu - init - first input variable to intit script ==="
+echo $1
+
+echo "=== tofu - init ==="
+echo "=== Done! ==="
--- infrastructure/scripts/version.sh
+++ infrastructure/scripts/version.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+echo "=== Running: ==="
+echo "=== tofu - version ==="
+
 docker compose run --rm tofu --version
+
+echo "=== tofu - version ==="
+echo "=== Done! ==="
