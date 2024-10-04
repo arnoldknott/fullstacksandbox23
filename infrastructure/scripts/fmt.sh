7934321ac80af15f7ea012cbdf729e09d1408847
arnoldknott <knott@dtu.dk>
2024-10-04 13:29:59 +0200

    test: adds a job for formating the infrastructure code in the pipeline and comments the infrastructure fmt in pre-commit

---
+++ infrastructure/scripts/fmt.sh
@@ -0,0 +1,10 @@
+#!/bash/bin
+
+echo "=== Running: ==="
+echo "=== tofu - fmt ==="
+
+# TBD: implement the fmt command
+# docker compose run --rm tofu fmt
+
+echo "=== tofu - fmt ==="
+echo "=== Done! ==="
--- scripts/run_infrastructure.sh
+++ scripts/run_infrastructure.sh
@@ -1,26 +1,44 @@
 #!/bin/bash
 
 REPO_ROOT_DIR=$(git rev-parse --show-toplevel)
+BRANCH_NAME=$(git branch --show-current)
+
 cd $REPO_ROOT_DIR/infrastructure
+
 
 docker compose build
 # docker compose up
 
 docker compose down --remove-orphans
 
+cd scripts
+
 # can be used to copy files from inside the container to the host:
 # docker compose run --name testcontainername tofu
 # docker cp testcontainername:/code/hello.txt hello_local.txt
 
 # Run each of those as a single step in the CI/CD job  and use variables to pass the secrets to the script:
-echo ""
+echo "=== Tofu Version ==="
 # docker compose run --rm tofu --version
-./scripts/version.sh
+./version.sh
 
-echo ""
+echo "=== Tofu Help ==="
 # docker compose run --rm tofu --help
-./scripts/help.sh
+./help.sh
 
-echo ""
+echo "=== Tofu Select Workspace ==="
+if [ "$BRANCH_NAME" == "dev" ]; then
+    docker compose run --rm tofu workspace select dev
+elif [ "$BRANCH_NAME" == "stage" ]; then
+    docker compose run --rm tofu workspace select stage
+elif [ "$BRANCH_NAME" == "prod" ]; then
+    docker compose run --rm tofu workspace select prod
+else
+    echo "Branch name not recognized"
+    exit 1
+fi
+
+echo "=== Tofu Init ==="
+./init.sh --my-variable=123
 
 # docker compose run --rm tofu --version > tofu_version.txt
