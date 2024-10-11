6b2c50eb855d75fdd4cb0265ba5b3f4a64d73f00
arnoldknott <knott@dtu.dk>
2024-10-11 13:53:30 +0200

    ci: testing tofu fmt in pre-commit

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,17 +11,17 @@ terraform {
   # az storage account create --name <storage_account_name> --resource-group <resource_group_name> --location <location> --sku Standard_LRS --public-network-access enabled
   # az storage container create --name <container_name> --account-name <storage_account_name># consider adding --default-encryption-scope <scope> --prevent-encryption-scope-override true
   backend "azurerm" {
-  #   resource_group_name  = "StorageAccount-ResourceGroup"          # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-  #   storage_account_name = "abcd1234"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-  #   container_name       = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-  #   key                  = "dev.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-  # pass via environment variables for running local: ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_SUBSCRIPTION_ID, ARM_TENANT_ID
-  #   use_oidc             = true                                    # Can also be set via `ARM_USE_OIDC` environment variable.
-  #   client_id            = "00000000-0000-0000-0000-000000000000"  # Can also be set via `ARM_CLIENT_ID` environment variable.
-  #   client_secret        = "************************************"  # Can also be set via `ARM_CLIENT_SECRET` environment variable.
-  #   subscription_id      = "00000000-0000-0000-0000-000000000000"  # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable.
-  #   tenant_id            = "00000000-0000-0000-0000-000000000000"  # Can also be set via `ARM_TENANT_ID` environment variable.
-  #   use_azuread_auth     = true                                    # Can also be set via `ARM_USE_AZUREAD` environment variable.
+    #   resource_group_name  = "StorageAccount-ResourceGroup"          # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    #   storage_account_name = "abcd1234"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    #   container_name       = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    #   key                  = "dev.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    # pass via environment variables for running local: ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_SUBSCRIPTION_ID, ARM_TENANT_ID
+    #   use_oidc             = true                                    # Can also be set via `ARM_USE_OIDC` environment variable.
+    #   client_id            = "00000000-0000-0000-0000-000000000000"  # Can also be set via `ARM_CLIENT_ID` environment variable.
+    #   client_secret        = "************************************"  # Can also be set via `ARM_CLIENT_SECRET` environment variable.
+    #   subscription_id      = "00000000-0000-0000-0000-000000000000"  # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable.
+    #   tenant_id            = "00000000-0000-0000-0000-000000000000"  # Can also be set via `ARM_TENANT_ID` environment variable.
+    #   use_azuread_auth     = true                                    # Can also be set via `ARM_USE_AZUREAD` environment variable.
   }
 }
 
