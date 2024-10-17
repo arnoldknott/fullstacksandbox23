3375c261cbdd696df491b27b358a5f7244348447
arnoldknott <knott@dtu.dk>
2024-10-17 15:00:18 +0200

    ci: remove the ARM_variables in github actions pipeline to ensure tofu is running as the logged in user

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,9 +1,9 @@
 # Provider configuration - no longer necessary, passed via environment variables ARM_*
-# variable "azure_client_id" {
-#   description = "Service principle client ID"
-#   type        = string
-#   sensitive   = true
-# }
+variable "azure_client_id" {
+  description = "Service principle client ID"
+  type        = string
+  sensitive   = true
+}
 
 # variable "azure_client_secret" {
 #   description = "Service principle password"
@@ -11,10 +11,10 @@
 #   sensitive   = true
 # }
 
-# variable "azure_subscription_id" {
-#   description = "Azure subscription ID"
-#   type        = string
-# }
+variable "azure_subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+}
 
 variable "azure_tenant_id" {
   description = "Azure tenant ID"
