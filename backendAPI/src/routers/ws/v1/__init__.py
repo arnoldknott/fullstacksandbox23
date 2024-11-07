7c677a2d2389eae7c48f964086802c1b99aae0de
arnoldknott <knott@dtu.dk>
2024-11-07 13:28:22 +0100

    feat: adds the first and public websocket

---
+++ backendAPI/src/routers/ws/v1/__init__.py
@@ -0,0 +1,3 @@
+"""Web sockets version 1 hosts the web sockets endpoints for the first version of web sockets.
+This module includes the tests for the endpoints:
+in general each endpoint shall get its own tests."""
