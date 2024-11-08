fe53d4da3b2e6774479d7086bf2cd6281b6ea35f
arnoldknott <knott@dtu.dk>
2024-11-08 10:34:17 +0100

    doc: adds todos to client and server side of websockets to run pre-commit hooks and trigger ci again

--- frontend_svelte/src/routes/playground/websockets/+page.svelte
+++ frontend_svelte/src/routes/playground/websockets/+page.svelte
@@ -1,46 +1,60 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import '@material/web/textfield/filled-text-field.js';
-    import '@material/web/button/filled-button.js';
-    import Title from '$components/Title.svelte';
+	import { onMount } from 'svelte';
+	import '@material/web/textfield/filled-text-field.js';
+	import '@material/web/button/filled-button.js';
+	import Title from '$components/Title.svelte';
 
-    let new_message =$state('');
-    let socket: WebSocket | undefined = $state(undefined);
-    let old_messages: string[] = $state([]);
+	let new_message = $state('');
+	let socket: WebSocket | undefined = $state(undefined);
+	let old_messages: string[] = $state([]);
 
-    onMount(async () => {
-        socket = new WebSocket('ws://localhost:8660/ws/v1/public_web_socket');
+	onMount(async () => {
+		socket = new WebSocket('ws://localhost:8660/ws/v1/public_web_socket');
 
-        socket.onopen = event => {
-            console.log('== socket opened ===');
-            socket.send('Hello from the client!');
-        };
+		socket.onopen = (event) => {
+			console.log('== socket opened ===');
+			socket.send('Hello from the client!');
+		};
 
-        // const sendMessage = (event) => {
-        //     event.preventDefault();
-        //     socket.send(message);
+		// const sendMessage = (event) => {
+		//     event.preventDefault();
+		//     socket.send(message);
 
-        // };
-        socket.onmessage = event => {
-            console.log('Message from server:', event.data);
-            old_messages.push(event.data);
-        };
+		// };
+		socket.onmessage = (event) => {
+			console.log('Message from server:', event.data);
+			old_messages.push(event.data);
+		};
 
-        socket.onclose = event => {
-            console.log('=== socket closed ===');
-        };
-    });
+		socket.onclose = (event) => {
+			console.log('=== socket closed ===');
+		};
+	});
 </script>
 
+<!-- TBD: clear text box -->
 <div class="w-50">
-    <form id="post-message" class="flex flex-col">
-        <md-filled-text-field label="Message" type="input" name="message" oninput={e => new_message = e.target.value} class="w-50"> </md-filled-text-field>
-        <div class="ml-auto py-4">
-            <md-filled-button type="submit" role="button" onclick={socket?.send(new_message)} onkeydown={(e) => e.key === 'Enter' && socket?.send(new_message)} tabindex="0">OK</md-filled-button>
-        </div>
-    </form>
+	<form id="post-message" class="flex flex-col">
+		<md-filled-text-field
+			label="Message"
+			type="input"
+			name="message"
+			oninput={(e) => (new_message = e.target.value)}
+			class="w-50"
+		>
+		</md-filled-text-field>
+		<div class="ml-auto py-4">
+			<md-filled-button
+				type="submit"
+				role="button"
+				onclick={socket?.send(new_message)}
+				onkeydown={(e) => e.key === 'Enter' && socket?.send(new_message)}
+				tabindex="0">OK</md-filled-button
+			>
+		</div>
+	</form>
 </div>
 
-{#each old_messages as old_message }
-    <Title>{old_message}</Title>
+{#each old_messages as old_message}
+	<Title>{old_message}</Title>
 {/each}
