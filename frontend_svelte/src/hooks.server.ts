5e0acf44962b439016d88d1d278cee7f66d58b95
arnoldknott <knott@dtu.dk>
2024-11-14 09:05:25 +0100

    chore: changes the base files to how they look in a ground up curretn sveltekit installation, especially app.html

--- frontend_svelte/src/hooks.server.ts
+++ frontend_svelte/src/hooks.server.ts
@@ -1,6 +1,6 @@
 /** @type {import('@sveltejs/kit').Handle} */
 import { getSession, updateSessionExpiry } from '$lib/server/cache';
-import type { Session } from '$lib/types';
+import type { Session } from '$lib/types';// TBD: move to app.d.ts (look at new template from ground up installation)
 import { redirect } from '@sveltejs/kit';
 
 const retrieveSession = async (sessionId: string | null): Promise<Session | void> => {
@@ -44,3 +44,23 @@ export const handle = async ({ event, resolve }) => {
 
 	return await resolve(event);
 };
+
+
+/* app.html before update:
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<link rel="icon" href="%sveltekit.assets%/favicon.png" />
+		<link
+			href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap"
+			rel="stylesheet"
+		/>
+		<meta name="viewport" content="width=device-width" />
+		%sveltekit.head%
+	</head>
+	<body data-sveltekit-preload-data="hover">
+		<div style="display: contents">%sveltekit.body%</div>
+	</body>
+</html>
+*/
