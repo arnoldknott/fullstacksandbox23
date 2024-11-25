ccf351b46b2433cf9d300e7730c65d638c64bdff
arnoldknott <knott@dtu.dk>
2024-11-25 11:47:43 +0100

    chore: login working again in Safari and two user handling is up and running - sporadic issues with backend calls to ms graph with 5xx responses

--- frontend_svelte/src/routes/logout/+page.server.ts
+++ frontend_svelte/src/routes/logout/+page.server.ts
@@ -1,5 +1,6 @@
 import type { PageServerLoad } from './$types';
 // import { signOut } from '$lib/server/oauth';
+import { redisCache } from '$lib/server/cache';
 import { redirect } from '@sveltejs/kit';
 // import {redirect} from '@sveltejs/kit';
 import AppConfig from '$lib/server/config';
@@ -8,12 +9,19 @@ const appConfig = await AppConfig.getInstance();
 
 export const load: PageServerLoad = async ({ url, cookies }) => {
 	// console.log("IMPLEMENT logout!");
+	const sessionId = cookies.get('session_id');
 	cookies.delete('session_id', {
 		path: '/',
 		expires: new Date(0)
 	});
 	user_store.set(undefined);
-	// TBD: delete accessTokens in session from redis?
+	// TBD: delete set loggedIn to false in redis?
+	if (!sessionId) {
+		console.error('🔥 logout - server - missing session_id');
+		redirect(307, '/');
+	}
+	const redisClient = await redisCache.provideClient();
+	await redisClient.json.set(sessionId, '$.loggedIn', false);
 	// signOut();
 	redirect(307, `${appConfig.az_logout_uri}?post_logout_redirect_uri=${url.origin}/`);
 	// 	// redirect(302, "/");
--- frontend_svelte/src/routes/playground/ms_graph_me/+page.server.ts
+++ frontend_svelte/src/routes/playground/ms_graph_me/+page.server.ts
@@ -29,7 +29,7 @@ export const load: PageServerLoad = async ({ locals, cookies }) => {
 	const userProfile = await response.json();
 
 	return {
-		account: locals.sessionData.account,
+		account: locals.sessionData.microsoftAccount,
 		userProfile: userProfile
 	};
 };
