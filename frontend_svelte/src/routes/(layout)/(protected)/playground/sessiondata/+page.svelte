8901b8cab149f88217fd4cdbd08c4dca33062a54
arnoldknott <knott@dtu.dk>
2024-12-05 16:33:41 +0100

    chore: adds missing files for sessiondata in playground, fixes typescript error on developer host for missing rollup-darwin error in svelte.config.ts

---
+++ frontend_svelte/src/routes/(layout)/(protected)/playground/sessiondata/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Title from '$components/Title.svelte';
+	import JsonData from '$components/JsonData.svelte';
+	import { page } from '$app/stores';
+	const logSessionData = () => {
+		console.log('=== routes - playground - userstore - $page.data.sessionData ===');
+		console.log($page.data.session);
+	};
+</script>
+
+<button class="m-4 rounded bg-blue-400 p-2" onclick={logSessionData}
+	>Current $page.data.sessionData -> console</button
+>
+
+<Title>$page data session data</Title>
+<JsonData data={$page.data.session} />
+
+<Title>Page</Title>
+<JsonData data={$page} />
