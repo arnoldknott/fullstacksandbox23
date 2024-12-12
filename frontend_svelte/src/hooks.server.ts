7407db5b90d4e6362b76ff5439fe1e4f9cfa4dca
arnoldknott <knott@dtu.dk>
2024-12-12 19:31:36 +0100

    chore: secures admin routes by requiring Admin role in azure token, as decoded in backend

--- frontend_svelte/src/hooks.server.ts
+++ frontend_svelte/src/hooks.server.ts
@@ -2,6 +2,7 @@
 import { redisCache } from '$lib/server/cache';
 import type { Session } from '$lib/types'; // or types.d.ts?
 import { redirect } from '@sveltejs/kit';
+import { backendAPI } from '$lib/server/apis';
 
 const getSession = async (sessionId: string): Promise<Session | void> => {
 	try {
@@ -12,9 +13,10 @@ const getSession = async (sessionId: string): Promise<Session | void> => {
 			}
 		}
 	} catch {
-		console.error('🎣 hooks - server - getSession - public access, no user logged in');
+		console.error('🔥 🎣 hooks - server - getSession - public access, no user logged in');
 	}
 };
+
 
 export const handle = async ({ event, resolve }) => {
 	const sessionId = event.cookies.get('session_id');
@@ -23,27 +25,40 @@ export const handle = async ({ event, resolve }) => {
 		event.locals.sessionData = session;
 	}
 
+	let redirectTarget = `/login?targetURL=${event.url.href}`
 	try {
 		if (event.route.id?.includes('(protected)')) {
 			if (event.locals.sessionData.loggedIn !== true) {
 				console.error(
 					'🔥 🎣 hooks - server - access attempt to protected route with invalid session'
 				);
-				redirect(307, `/login?targetURL=${event.url.href}`);
+				throw new Error('Invalid session');
+				// redirect(307, `/login?targetURL=${event.url.href}`);
 			} else {
 				if (event.route.id?.includes('(admin)')) {
 					console.log('🎣 hooks - server - access to admin route');
+					const userResponse = await backendAPI.get(event.locals.sessionData.sessionId, '/user/me');
+					const user = await userResponse.json();
+					if (!user.azureTokenRoles.includes('Admin')) {
+						console.error(
+							'🔥 🎣 hooks - server - access to admin route failed (user is not admin)'
+						);
+						redirectTarget = `/`;
+						throw new Error('User is not admin');
+						// redirect(307, `/`);
+					}
 					// Add check if user is admin in backend here
 					// thrown another redirect here, if user is not admin!
 				}
 			}
 		}
-	} catch {
+	} catch (error) {
 		console.error(
 			'🔥 🎣 hooks - server - access to this protected route failed (potentially session expired):'
 		);
 		console.log(event.url.href);
-		redirect(307, `/login?targetURL=${event.url.href}`);
+		// redirect(307, `/login?targetURL=${event.url.href}`);
+		redirect(307, redirectTarget);
 	}
 	return await resolve(event);
 };
--- frontend_svelte/src/routes/(layout)/(protected)/(admin)/admin/dashboard/+page.svelte
+++ frontend_svelte/src/routes/(layout)/(protected)/(admin)/admin/dashboard/+page.svelte
@@ -2,15 +2,12 @@
 	import Title from '$components/Title.svelte';
 	import type { PageData } from './$types';
 
-	let data: PageData = $props();
+	let {data }: {data: PageData} = $props();
 	const apiLink = data.body;
 </script>
 
 <Title>Admin dashboard</Title>
 
-<h3 class="text-danger mb-2 mt-0 text-3xl font-medium leading-tight">
-	Not yet protected! Anybody can get here!
-</h3>
 
 <h3 class="mb-2 mt-0 text-3xl font-medium leading-tight text-primary">
 	Update environmental variables
@@ -20,7 +17,7 @@
 	<button data-sveltekit-preload-data="false">Now</button>
 </form>
 
-<h3 class="mb-2 mt-0 text-3xl font-medium leading-tight text-primary">API Documentation</h3>
+<h3 class="mb-2 mt-0 text-3xl font-medium leading-tight text-primary">API Documentation Backend</h3>
 <a
 	href={apiLink}
 	class="hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600 text-primary transition duration-150 ease-in-out"
