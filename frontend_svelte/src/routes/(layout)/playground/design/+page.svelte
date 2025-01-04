585d4335acea7939ebbe033ae558d979f3b47940
arnoldknott <knott@dtu.dk>
2025-01-04 19:38:44 +0100

    chore: makes buttons reactive

--- frontend_svelte/src/routes/(layout)/playground/design/+page.svelte
+++ frontend_svelte/src/routes/(layout)/playground/design/+page.svelte
@@ -36,111 +36,153 @@
 
 <div class="accordion accordion-bordered divide-y" data-accordion-always-open="">
 	<AccordionItem title="Backgrounds">
-		<div class="grid h-screen w-full grid-cols-5 bg-background text-base-content">
+		<div class="grid h-screen w-full grid-cols-6 bg-background text-base-content">
 			<div class="bg-background p-4 text-center">Background</div>
-			<div class="h-2/5 min-h-fit bg-base-100 p-4 text-center">base-100</div>
-			<div class="h-3/5 min-h-fit bg-base-150 p-4 text-center">base-150</div>
-			<div class="h-4/5 min-h-fit bg-base-200 p-4 text-center">base-200</div>
-			<div class="h-5/5 min-h-fit bg-base-300 p-4 text-center">base-300</div>
+			<div class="h-2/6 min-h-fit bg-base-100 p-4 text-center">base-100</div>
+			<div class="h-3/6 min-h-fit bg-base-150 p-4 text-center">base-150</div>
+			<div class="h-4/6 min-h-fit bg-base-200 p-4 text-center">base-200</div>
+			<div class="h-5/6 min-h-fit bg-base-200 p-4 text-center">base-250</div>
+			<div class="h-6/6 min-h-fit bg-base-300 p-4 text-center">base-300</div>
 		</div>
 	</AccordionItem>
 	<AccordionItem title="Text and colors for content">
-		<div class="grid h-screen w-full grid-cols-5 bg-background text-base-content">
+		<div class="grid h-screen min-h-fit w-full grid-cols-6 bg-background text-base-content">
 			<div class="p-4">
 				Background
 				<ContentText />
 			</div>
-			<div class="h-2/5 min-h-fit bg-base-100 p-4">
+			<div class="h-2/6 min-h-fit bg-base-100 p-4">
 				base-100
 				<ContentText />
 			</div>
-			<div class="h-3/5 min-h-fit bg-base-150 p-4">
+			<div class="h-3/6 min-h-fit bg-base-150 p-4">
 				base-150
 				<ContentText />
 			</div>
-			<div class="h-4/5 min-h-fit bg-base-200 p-4">
+			<div class="h-4/6 min-h-fit bg-base-200 p-4">
 				base-200
 				<ContentText />
 			</div>
-			<div class="h-5/5 min-h-fit bg-base-300 p-4">
+			<div class="h-5/6 min-h-fit bg-base-250 p-4">
+				base-250
+				<ContentText />
+			</div>
+			<div class="h-6/6 min-h-fit bg-base-300 p-4">
 				base-300
 				<ContentText />
 			</div>
 		</div>
 	</AccordionItem>
 	<AccordionItem title="Text and colors for app functionality">
-		<div class="grid h-screen w-full grid-cols-5 bg-background text-base-content">
+		<div class="grid h-screen min-h-fit w-full grid-cols-6 bg-background text-base-content">
 			<div class="p-4">
 				Background
 				<AppText />
 			</div>
-			<div class="h-2/5 min-h-fit bg-base-100 p-4">
+			<div class="h-2/6 min-h-fit bg-base-100 p-4">
 				base-100
 				<AppText />
 			</div>
-			<div class="h-3/5 min-h-fit bg-base-150 p-4">
+			<div class="h-3/6 min-h-fit bg-base-150 p-4">
 				base-150
 				<AppText />
 			</div>
-			<div class="h-4/5 min-h-fit bg-base-200 p-4">
+			<div class="h-4/6 min-h-fit bg-base-200 p-4">
 				base-200
 				<AppText />
 			</div>
-			<div class="h-5/5 min-h-fit bg-base-300 p-4">
+			<div class="h-5/6 min-h-fit bg-base-250 p-4">
+				base-250
+				<AppText />
+			</div>
+			<div class="h-6/6 min-h-fit bg-base-300 p-4">
 				base-300
 				<AppText />
 			</div>
 		</div>
 	</AccordionItem>
-	<AccordionItem title="Buttons">
-		<div class="grid h-screen w-full grid-cols-5 bg-background text-base-content">
+	<AccordionItem title="Buttons for content control">
+		<div class="grid h-screen min-h-fit w-full grid-cols-3 md:grid-cols-6 bg-background text-base-content">
 			<div class="p-4">
 				Background
 				<ContentButtons />
 			</div>
-			<div class="h-2/5 min-h-fit bg-base-100 p-4">
+			<div class="h-2/6 min-h-fit bg-base-100 p-4">
 				base-100
 				<ContentButtons />
 			</div>
-			<div class="h-3/5 min-h-fit bg-base-150 p-4">
+			<div class="h-3/6 min-h-fit bg-base-150 p-4">
 				base-150
 				<ContentButtons />
 			</div>
-			<div class="h-4/5 min-h-fit bg-base-200 p-4">
+			<div class="h-4/6 min-h-fit bg-base-200 p-4">
 				base-200
 				<ContentButtons />
 			</div>
-			<div class="h-5/5 min-h-fit bg-base-300 p-4">
+			<div class="h-5/6 min-h-fit bg-base-250 p-4">
+				base-250
+				<ContentButtons />
+			</div>
+			<div class="h-6/6 min-h-fit bg-base-300 p-4">
 				base-300
 				<ContentButtons />
 			</div>
 		</div>
-		<div class="skeleton h-screen w-full bg-base-100 text-base-content">
-			<div class="flex flex-col gap-2 p-4">
-				<p class="body-small">Base-100 background with base-content text</p>
-				<div class="flex flex-row flex-wrap gap-4 pl-6">
-					
-				</div>
-				<div class="flex flex-row flex-wrap gap-4 pl-6">
-					<div class="flex flex-col gap-2">
-						<button class="btn btn-info rounded-full bg-info text-info-content">Info</button>
-						<button class="btn-info-container btn rounded-full">Info Container</button>
+		
+	</AccordionItem>
+	<AccordionItem title="Buttons for app functionality">
+		<div class="grid h-screen min-h-fit w-full grid-cols-3 sm:grid-cols-6 bg-background text-base-content">
+			<div class="p-4">
+				Background
+				
+			</div>
+			<div class="h-2/6 min-h-fit bg-base-100 p-4">
+				base-100
+				
+			</div>
+			<div class="h-3/6 min-h-fit bg-base-150 p-4">
+				base-150
+				
+			</div>
+			<div class="h-4/6 min-h-fit bg-base-200 p-4">
+				base-200
+				
+			</div>
+			<div class="h-5/6 min-h-fit bg-base-250 p-4">
+				base-250
+				
+			</div>
+			<div class="h-6/6 min-h-fit bg-base-300 p-4">
+				base-300
+				
+			</div>
+			<div class="skeleton h-screen w-full bg-base-100 text-base-content">
+				<div class="flex flex-col gap-2 p-4">
+					<p class="body-small">Base-100 background with base-content text</p>
+					<div class="flex flex-row flex-wrap gap-4 pl-6">
+						
 					</div>
-					<div class="flex flex-col gap-2">
-						<button class="btn btn-success rounded-full bg-success text-success-content"
-							>Success</button
-						>
-						<button class="btn-success-container btn rounded-full">Success Container</button>
-					</div>
-					<div class="flex flex-col gap-2">
-						<button class="btn btn-warning rounded-full bg-warning text-warning-content"
-							>Warning</button
-						>
-						<button class="btn-warning-container btn rounded-full">Warning Container</button>
-					</div>
-					<div class="flex flex-col gap-2">
-						<button class="btn btn-error rounded-full bg-error text-error-content">Error</button>
-						<button class="btn-error-container btn rounded-full">Error Container</button>
+					<div class="flex flex-row flex-wrap gap-4 pl-6">
+						<div class="flex flex-col gap-2">
+							<button class="btn btn-info rounded-full bg-info text-info-content">Info</button>
+							<button class="btn-info-container btn rounded-full">Info Container</button>
+						</div>
+						<div class="flex flex-col gap-2">
+							<button class="btn btn-success rounded-full bg-success text-success-content"
+								>Success</button
+							>
+							<button class="btn-success-container btn rounded-full">Success Container</button>
+						</div>
+						<div class="flex flex-col gap-2">
+							<button class="btn btn-warning rounded-full bg-warning text-warning-content"
+								>Warning</button
+							>
+							<button class="btn-warning-container btn rounded-full">Warning Container</button>
+						</div>
+						<div class="flex flex-col gap-2">
+							<button class="btn btn-error rounded-full bg-error text-error-content">Error</button>
+							<button class="btn-error-container btn rounded-full">Error Container</button>
+						</div>
 					</div>
 				</div>
 			</div>
--- frontend_svelte/src/routes/(layout)/playground/design/ContentButtons.svelte
+++ frontend_svelte/src/routes/(layout)/playground/design/ContentButtons.svelte
@@ -1,27 +1,29 @@
 <div class="flex flex-col gap-4">
     <!-- <p class="text-primary text-body-small">Primary text on base-100 background</p> -->
-    <button class="btn btn-primary rounded-full bg-primary lab text-primary-content"
+    <button class="btn btn-primary rounded-full bg-primary text-label-small md:text-label text-primary-content"
         >Primary</button
     >
-    <button class="btn-primary-container btn rounded-full">Primary Container</button>
+    <button class="btn-primary-container btn max-md:h-16 text-label-small lg:text-label rounded-3xl lg:rounded-full">Primary Container</button>
 </div>
 <div class="divider divider-outline-variant"></div>
 <div class="flex flex-col gap-4">
-    <button class="btn btn-secondary rounded-full bg-secondary text-secondary-content"
+    <button class="btn btn-secondary rounded-full bg-secondary text-label-small md:text-label text-secondary-content"
         >Secondary</button
     >
-    <button class="btn-secondary-container btn rounded-full">Secondary Container</button>
+    <button class="btn-secondary-container btn max-lg:h-16 text-label-small lg:text-label rounded-3xl lg:rounded-full">Secondary Container</button>
 </div>
 <div class="divider divider-outline-variant"></div>
 <div class="flex flex-col gap-4">
-    <button class="btn btn-accent rounded-full bg-accent text-accent-content">Accent</button
+    <button class="btn btn-accent rounded-full bg-accent text-label-small md:text-label text-accent-content">Accent</button
     >
-    <button class="btn-accent-container btn rounded-full">Accent Container</button>
+    <button class="btn-accent-container btn max-lg:h-16 text-label-small lg:text-label rounded-3xl lg:rounded-full">Accent Container</button>
 </div>
 <div class="divider divider-outline-variant"></div>
 <div class="flex flex-col gap-4">
-    <button class="btn-neutral btn rounded-full bg-neutral text-neutral-content"
+    <button class="btn-neutral btn rounded-full bg-neutral text-label-small md:text-label text-neutral-content"
         >Neutral</button
     >
-    <button class="btn-neutral-container btn rounded-full">Neutral Container</button>
+    <button class="btn-neutral-container btn max-lg:h-16 text-label-small lg:text-label rounded-3xl lg:rounded-full">Neutral Container</button>
 </div>
+<div class="divider divider-outline-variant"></div>
+<p class="text-body-small text-outline-variant">The dividers are outline-variant</p>
