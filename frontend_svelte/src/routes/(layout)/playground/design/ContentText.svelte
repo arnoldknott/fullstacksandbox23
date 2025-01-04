e5fdef5bbebc3aeb719915af7fd530f25069248a
arnoldknott <knott@dtu.dk>
2025-01-04 21:02:47 +0100

    chore: adds headings to text demonastration

--- frontend_svelte/src/routes/(layout)/playground/design/ContentText.svelte
+++ frontend_svelte/src/routes/(layout)/playground/design/ContentText.svelte
@@ -1,21 +1,25 @@
 <div class="flex flex-wrap">
 	<div class="grow">
+        <h2 class="text-heading lg:text-heading-large mt-4 text-primary">Primary Heading</h2>
 		<h3 class="text-title md:text-title-large mt-4 text-primary">Primary Title</h3>
-		<p class="text-body-small md:text-body text-primary">Primary text-body text</p>
+		<p class="text-body-small max-w-64 md:text-body text-primary">Primary body text for longer sentences and paragraphs.</p>
 	</div>
 	<div class="divider-outline-variant divider divider-horizontal"></div>
 	<div class="grow">
+        <h2 class="text-heading lg:text-heading-large mt-4 text-secondary">Secondary Heading</h2>
 		<h3 class="text-title md:text-title-large mt-4 text-secondary">Secondary Title</h3>
-		<p class="text-body-small md:text-body text-secondary">Secondary text-body text</p>
+		<p class="text-body-small max-w-64  md:text-body text-secondary">Secondary body text for longer sentences and paragraphs.</p>
 	</div>
 	<div class="divider-outline-variant divider divider-horizontal"></div>
 	<div class="grow">
+        <h2 class="text-heading lg:text-heading-large mt-4 text-accent">Accent Heading</h2>
 		<h3 class="text-title md:text-title-large mt-4 text-accent">Accent Title</h3>
-		<p class="text-body-small md:text-body text-accent">Accent text-body text</p>
+		<p class="text-body-small max-w-64 md:text-body text-accent">Accent body text for longer sentences and paragraphs.</p>
 	</div>
 	<div class="divider-outline-variant divider divider-horizontal"></div>
 	<div class="grow">
+        <h2 class="text-heading lg:text-heading-large mt-4 text-neutral">Neutral Heading</h2>
 		<h3 class="text-title md:text-title-large mt-4 text-neutral">Neutral Title</h3>
-		<p class="text-body-small md:text-body text-neutral">Neutral text-body text</p>
+		<p class="text-body-small max-w-64 md:text-body text-neutral">Neutral body text for longer sentences and paragraphs.</p>
 	</div>
 </div>
