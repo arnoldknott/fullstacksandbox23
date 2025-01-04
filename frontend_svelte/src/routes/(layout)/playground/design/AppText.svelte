46514bcd1e8d7255d67efa485304eb172e57d5e3
arnoldknott <knott@dtu.dk>
2025-01-04 21:05:44 +0100

    chore: adds headings to text demonastration also for app text

--- frontend_svelte/src/routes/(layout)/playground/design/AppText.svelte
+++ frontend_svelte/src/routes/(layout)/playground/design/AppText.svelte
@@ -1,21 +1,25 @@
 <div class="flex flex-wrap">
-	<div class="grow">
-		<h3 class="text-title md:text-title-large mt-4 text-info">Info Title</h3>
-		<p class="text-body-small md:text-body text-info">Info text-body text</p>
-	</div>
-	<div class="divider-outline-variant divider divider-horizontal"></div>
-	<div class="grow">
-		<h3 class="text-title md:text-title-large mt-4 text-success">Success Title</h3>
-		<p class="text-body-small md:text-body text-success">Success text-body text</p>
-	</div>
-	<div class="divider-outline-variant divider divider-horizontal"></div>
-	<div class="grow">
-		<h3 class="text-title md:text-title-large mt-4 text-warning">Warning Title</h3>
-		<p class="text-body-small md:text-body text-warning">Warning text-body text</p>
-	</div>
-	<div class="divider-outline-variant divider divider-horizontal"></div>
-	<div class="grow">
-		<h3 class="text-title md:text-title-large mt-4 text-error">Error Title</h3>
-		<p class="text-body-small md:text-body text-error">Error text-body text</p>
-	</div>
+    <div class="grow">
+        <h2 class="text-heading lg:text-heading-large mt-4 text-info">Info Heading</h2>
+        <h3 class="text-title md:text-title-large mt-4 text-info">Info Title</h3>
+        <p class="text-body-small max-w-64 md:text-body text-info">Info body text for longer sentences and paragraphs.</p>
+    </div>
+    <div class="divider-outline-variant divider divider-horizontal"></div>
+    <div class="grow">
+        <h2 class="text-heading lg:text-heading-large mt-4 text-success">Success Heading</h2>
+        <h3 class="text-title md:text-title-large mt-4 text-success">Success Title</h3>
+        <p class="text-body-small max-w-64 md:text-body text-success">Success body text for longer sentences and paragraphs.</p>
+    </div>
+    <div class="divider-outline-variant divider divider-horizontal"></div>
+    <div class="grow">
+        <h2 class="text-heading lg:text-heading-large mt-4 text-warning">Warning Heading</h2>
+        <h3 class="text-title md:text-title-large mt-4 text-warning">Warning Title</h3>
+        <p class="text-body-small max-w-64 md:text-body text-warning">Warning body text for longer sentences and paragraphs.</p>
+    </div>
+    <div class="divider-outline-variant divider divider-horizontal"></div>
+    <div class="grow">
+        <h2 class="text-heading lg:text-heading-large mt-4 text-error">Error Heading</h2>
+        <h3 class="text-title md:text-title-large mt-4 text-error">Error Title</h3>
+        <p class="text-body-small max-w-64 md:text-body text-error">Error body text for longer sentences and paragraphs.</p>
+    </div>
 </div>
