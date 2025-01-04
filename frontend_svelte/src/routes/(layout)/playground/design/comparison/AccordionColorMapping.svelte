21dbf30de03d1c705d1a3ed91a876bb0e4bb2f34
arnoldknott <knott@dtu.dk>
2025-01-04 13:12:47 +0100

    chore: moves comparison of flyonui and material design colors into accordion

---
+++ frontend_svelte/src/routes/(layout)/playground/design/comparison/AccordionColorMapping.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import type { Snippet } from 'svelte';
+    let { title, children }: { title: string, children: Snippet} = $props();
+    const id = title.toLowerCase().replaceAll(' ', '-');
+</script>
+
+<div class="active accordion-item" id={id}>
+    <button
+        class="accordion-toggle inline-flex items-center gap-x-4 text-start"
+        aria-controls="{id}-collapse"
+        aria-expanded="true"
+    >
+        <span
+            class="icon-[tabler--chevron-right] size-5 shrink-0 transition-transform duration-300 accordion-item-active:rotate-90 rtl:rotate-180"
+        ></span>
+        <p class="ml-10 text-base md:text-xl">{title}</p>
+    </button>
+    <div
+        id="{id}-collapse"
+        class="accordion-content w-full overflow-hidden transition-[height] duration-300"
+        aria-labelledby={id}
+        role="region"
+    >
+        <div class="grid grid-cols-2 gap-2">
+            {@render children?.()}
+        </div>
+    </div>
+</div>
