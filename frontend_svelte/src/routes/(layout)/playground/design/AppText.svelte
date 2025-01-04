a2ea9e54f64168b49b950ce14b645d7c2797420f
arnoldknott <knott@dtu.dk>
2025-01-04 18:36:59 +0100

    chore: adds dividers between texts

--- frontend_svelte/src/routes/(layout)/playground/design/AppText.svelte
+++ frontend_svelte/src/routes/(layout)/playground/design/AppText.svelte
@@ -1,8 +1,13 @@
 <h3 class="title md:title-large mt-4 text-info">Info Title</h3>
 <p class="body-small md:body text-info">Info body text</p>
+<div class="divider divider-outline-variant"></div>
 <h3 class="title md:title-large mt-4 text-success">Success Title</h3>
 <p class="body-small md:body text-success">Success body text</p>
+<div class="divider divider-outline-variant"></div>
 <h3 class="title md:title-large mt-4 text-warning">Warning Title</h3>
 <p class="body-small md:body text-warning">Warning body text</p>
+<div class="divider divider-outline-variant"></div>
 <h3 class="title md:title-large mt-4 text-error">Error Title</h3>
 <p class="body-small md:body text-error">Error body text</p>
+<div class="divider divider-outline-variant"></div>
+<p class="body-small text-outline-variant">The dividers are outline-variant</p>
--- frontend_svelte/src/routes/(layout)/playground/design/ContentText.svelte
+++ frontend_svelte/src/routes/(layout)/playground/design/ContentText.svelte
@@ -1,8 +1,13 @@
 <h3 class="title md:title-large mt-4 text-primary">Primary Title</h3>
 <p class="body-small md:body text-primary">Primary body text</p>
+<div class="divider divider-outline-variant"></div>
 <h3 class="title md:title-large mt-4 text-secondary">Secondary Title</h3>
 <p class="body-small md:body text-secondary">Secondary body text</p>
+<div class="divider divider-outline-variant"></div>
 <h3 class="title md:title-large mt-4 text-accent">Accent Title</h3>
 <p class="body-small md:body text-accent">Accent body text</p>
+<div class="divider divider-outline-variant"></div>
 <h3 class="title md:title-large mt-4 text-neutral">Neutral Title</h3>
 <p class="body-small md:body text-neutral">Neutral body text</p>
+<div class="divider divider-outline-variant"></div>
+<p class="body-small text-outline-variant">The dividers are outline-variant</p>
