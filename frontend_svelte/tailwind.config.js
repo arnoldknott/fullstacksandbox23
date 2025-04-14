41810d41f7878f1c32ec8dac5ad3dde9be2ce23f
arnoldknott <knott@dtu.dk>
2025-04-14 17:58:33 +0200

    chore: removes flyonui and iconify plugin from tailwind config to prepare upgrading tailwindcss to 4.1.3

--- frontend_svelte/tailwind.config.js
+++ frontend_svelte/tailwind.config.js
@@ -121,87 +121,88 @@ module.exports = {
 		// }
 	},
 	darkMode: 'media',
-	plugins: [flyonui, flyonuiPlugin, addDynamicIconSelectors()],
-	flyonui: {
-		// consider adding this to disable all flyonUI formating and
-		// leave it to Material Design assisted by TailwindCSS
-		// styled: false,
-		vendors: true,
-		// themeRoot: "TBD", // consider passing the theme root element here, so not all elements get themed!
-		themes: [
-			// {
-			// 	light: {
-			// 		...light, // TBD: remove the themes, when Material Color synchronization is fully implemented
-			// 		transparent: 'transparent',
-			// 		current: 'currentColor',
-			// 		info: light.info,
-			// 		'info-content': light['info-content'],
-			// 		success: light.success,
-			// 		'success-content': light['success-content']
-			// 		// // 		// lightFlyonUI,
-			// 		// // 		// lightFlyonUI,// the default FlyonUI theme
-			// 		// // 		// set the color object for Tailwind programmatically here - matching output from Material Dynamic Color:
-			// 		// // 		// https://github.com/themeselection/flyonui/blob/bdbdaeec6b575b80283f5fda51abd3981a168fca/src/theming/index.js#L2
-			// 		// 		transparent: 'transparent',
-			// 		// 		current: 'currentColor',
-			// 		// 		primary: '#394DFF',
-			// 		// 		// primary: 'var(--my-color)',
-			// 		// 		'primary-content': '#9AE7FF',
-			// 		// // 		// Match accent into tertiary!
-			// 		// // 		// tertiary: '#1B82F6',
-			// 		// // 		// 'tertiary-content': '#0BEAFE',
-			// 	}
-			// },
-			// {
-			// 	dark: {
-			// 		...dark, // TBD: remove the themes, when Material Color synchronization is fully implemented
-			// 		transparent: 'transparent',
-			// 		current: 'currentColor',
-			// 		info: dark.info,
-			// 		'info-content': dark['info-content'],
-			// 		success: dark.success,
-			// 		'success-content': dark['success-content']
-			// 	}
-			// }
-			// {
-			// 	dark: {
-			// 		darkFlyonUI,
-			// 		// darkFlyonUI,// the default FlyonUI theme
-			// 		// set the color object for Tailwind programmatically here - matching output from Material Dynamic Color:
-			// 		//
-			// 	}
-			// },
-			// 'light',
-			// 'dark',
-			// {
-			// 	light: {
-			// 		transparent: 'transparent',
-			// 		current: 'currentColor',
-			// 		light,
-			// 	}
-			// },
-			// {
-			// 	dark: {
-			// 		transparent: 'transparent',
-			// 		current: 'currentColor',
-			// 		dark,
-			// 	}
-			// },
-			// {
-			// 	darkhc: {
-			// 		primary: '#f44336'
-			// 		// Additional customizations from FlyonUI
-			// 		// "--rounded-box": "1rem", // border-radius for large boxes
-			// 		// "--rounded-btn": "0.5rem", // border-radius for buttons
-			// 		// "--rounded-tooltip": "1.9rem", // border-radius for tooltip
-			// 		// "--animation-btn": "0.25s", // button click animation duration
-			// 		// "--animation-input": "0.2s", // input animation duration (e.g., checkboxes, switch)
-			// 		// "--btn-focus-scale": "0.95", // button scale transform on focus
-			// 		// "--border-btn": "1px", // button border width
-			// 		// "--tab-border": "1px", // tab border width
-			// 		// "--tab-radius": "0.5rem" // tab border-radius
-			// 	}
-			// }
-		]
-	}
+	// commented for updating to TailwindCSS 4.1.3:
+	// plugins: [flyonui, flyonuiPlugin, addDynamicIconSelectors()],
+	// flyonui: {
+	// 	// consider adding this to disable all flyonUI formating and
+	// 	// leave it to Material Design assisted by TailwindCSS
+	// 	// styled: false,
+	// 	vendors: true,
+	// 	// themeRoot: "TBD", // consider passing the theme root element here, so not all elements get themed!
+	// 	themes: [
+	// 		// {
+	// 		// 	light: {
+	// 		// 		...light, // TBD: remove the themes, when Material Color synchronization is fully implemented
+	// 		// 		transparent: 'transparent',
+	// 		// 		current: 'currentColor',
+	// 		// 		info: light.info,
+	// 		// 		'info-content': light['info-content'],
+	// 		// 		success: light.success,
+	// 		// 		'success-content': light['success-content']
+	// 		// 		// // 		// lightFlyonUI,
+	// 		// 		// // 		// lightFlyonUI,// the default FlyonUI theme
+	// 		// 		// // 		// set the color object for Tailwind programmatically here - matching output from Material Dynamic Color:
+	// 		// 		// // 		// https://github.com/themeselection/flyonui/blob/bdbdaeec6b575b80283f5fda51abd3981a168fca/src/theming/index.js#L2
+	// 		// 		// 		transparent: 'transparent',
+	// 		// 		// 		current: 'currentColor',
+	// 		// 		// 		primary: '#394DFF',
+	// 		// 		// 		// primary: 'var(--my-color)',
+	// 		// 		// 		'primary-content': '#9AE7FF',
+	// 		// 		// // 		// Match accent into tertiary!
+	// 		// 		// // 		// tertiary: '#1B82F6',
+	// 		// 		// // 		// 'tertiary-content': '#0BEAFE',
+	// 		// 	}
+	// 		// },
+	// 		// {
+	// 		// 	dark: {
+	// 		// 		...dark, // TBD: remove the themes, when Material Color synchronization is fully implemented
+	// 		// 		transparent: 'transparent',
+	// 		// 		current: 'currentColor',
+	// 		// 		info: dark.info,
+	// 		// 		'info-content': dark['info-content'],
+	// 		// 		success: dark.success,
+	// 		// 		'success-content': dark['success-content']
+	// 		// 	}
+	// 		// }
+	// 		// {
+	// 		// 	dark: {
+	// 		// 		darkFlyonUI,
+	// 		// 		// darkFlyonUI,// the default FlyonUI theme
+	// 		// 		// set the color object for Tailwind programmatically here - matching output from Material Dynamic Color:
+	// 		// 		//
+	// 		// 	}
+	// 		// },
+	// 		// 'light',
+	// 		// 'dark',
+	// 		// {
+	// 		// 	light: {
+	// 		// 		transparent: 'transparent',
+	// 		// 		current: 'currentColor',
+	// 		// 		light,
+	// 		// 	}
+	// 		// },
+	// 		// {
+	// 		// 	dark: {
+	// 		// 		transparent: 'transparent',
+	// 		// 		current: 'currentColor',
+	// 		// 		dark,
+	// 		// 	}
+	// 		// },
+	// 		// {
+	// 		// 	darkhc: {
+	// 		// 		primary: '#f44336'
+	// 		// 		// Additional customizations from FlyonUI
+	// 		// 		// "--rounded-box": "1rem", // border-radius for large boxes
+	// 		// 		// "--rounded-btn": "0.5rem", // border-radius for buttons
+	// 		// 		// "--rounded-tooltip": "1.9rem", // border-radius for tooltip
+	// 		// 		// "--animation-btn": "0.25s", // button click animation duration
+	// 		// 		// "--animation-input": "0.2s", // input animation duration (e.g., checkboxes, switch)
+	// 		// 		// "--btn-focus-scale": "0.95", // button scale transform on focus
+	// 		// 		// "--border-btn": "1px", // button border width
+	// 		// 		// "--tab-border": "1px", // tab border width
+	// 		// 		// "--tab-radius": "0.5rem" // tab border-radius
+	// 		// 	}
+	// 		// }
+	// 	]
+	// }
 };
