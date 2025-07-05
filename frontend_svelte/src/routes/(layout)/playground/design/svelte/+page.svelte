7b91ef17fd6f9b7555e23bdb15e677940ddf04bb
arnoldknott <knott@dtu.dk>
2025-07-05 19:30:55 +0200

    chore: adds parameters to draw

--- frontend_svelte/src/routes/(layout)/playground/design/svelte/+page.svelte
+++ frontend_svelte/src/routes/(layout)/playground/design/svelte/+page.svelte
@@ -1,48 +1,71 @@
 <script lang="ts">
 	import Heading from '$components/Heading.svelte';
-    import Box from './Box.svelte'
-    import Slider from './Slider.svelte';
-	import { blur, crossfade, draw, fade, fly, scale, slide  } from 'svelte/transition';
-    import {flip} from 'svelte/animate';
-    
+	import Box from './Box.svelte';
+	import Slider from './Slider.svelte';
+	import { blur, crossfade, draw, fade, fly, scale, slide } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
 
-
-    let blurParameters = $state({
-        delay: 0,
-        duration: 300,
-        // easing: (t: number) => t, // Easing function not implemented in this demo
-        amount: 5,
-        opacity: 0.5,
-    });
+	let blurParameters = $state({
+		delay: 0,
+		duration: 300,
+		// easing: (t: number) => t, // Easing function not implemented in this demo
+		amount: 5,
+		opacity: 0.5
+	});
 	let newBlur = $state<string>('');
 	let blurItems = $state<string[]>([]);
 
-    let crossfadeParameters = $state({
-        delay: 0,
-        duration: 300,
-        // easing: (t: number) => t, // Easing function not implemented in this demo
-    })
-    const [sendCrossfade, receiveCrossfade] = crossfade({...crossfadeParameters});
-	let left = $state([ {name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}, {name: 'Item 5'} ]);
-    let right = $state([ {name: 'Item A'}, {name: 'Item B'}, {name: 'Item C'}, {name: 'Item D'}, {name: 'Item E'} ]);
+	let crossfadeParameters = $state({
+		delay: 0,
+		duration: 300
+		// easing: (t: number) => t, // Easing function not implemented in this demo
+	});
+	const [sendCrossfade, receiveCrossfade] = crossfade({ ...crossfadeParameters });
+	let left = $state([
+		{ name: 'Item 1' },
+		{ name: 'Item 2' },
+		{ name: 'Item 3' },
+		{ name: 'Item 4' },
+		{ name: 'Item 5' }
+	]);
+	let right = $state([
+		{ name: 'Item A' },
+		{ name: 'Item B' },
+		{ name: 'Item C' },
+		{ name: 'Item D' },
+		{ name: 'Item E' }
+	]);
 
-    let newDraw = $state<string>('');
-    let drawItems = $state<string[]>([]);
+	let drawParameters = $state({
+		delay: 0,
+		speed: 0.1,
+		duration: 300 as number | undefined
+		// easing: (t: number) => t, // Easing function not implemented in this demo
+	});
+	let useDrawDuration = $state(true);
+	$effect(() => {
+		if (!useDrawDuration) {
+			drawParameters.duration = undefined;
+		} else {
+			drawParameters.duration = 300;
+		}
+	});
+	let drawShow = $state(false);
 
-    let newFade = $state<string>('');
-    let fadeItems = $state<string[]>([]);
+	let newFade = $state<string>('');
+	let fadeItems = $state<string[]>([]);
 
-    let newFly = $state<string>('');
-    let flyItems = $state<string[]>([]);
+	let newFly = $state<string>('');
+	let flyItems = $state<string[]>([]);
 
-    let newScale = $state<string>('');
-    let scaleItems = $state<string[]>([]);
+	let newScale = $state<string>('');
+	let scaleItems = $state<string[]>([]);
 
-    let newSlide = $state<string>('');
-    let slideItems = $state<string[]>([]);
+	let newSlide = $state<string>('');
+	let slideItems = $state<string[]>([]);
 
 	const pushItem = (newItem: string, list: string[]) => {
-        console.log('Pushing item:', newItem, 'to list:', list);
+		console.log('Pushing item:', newItem, 'to list:', list);
 		if (newItem.trim() !== '') {
 			list.push(newItem);
 			newBlur = '';
@@ -52,193 +75,256 @@
 
 <Heading>Transitions</Heading>
 
+<div class="sm w-full md:grid md:grid-cols-4 md:gap-4">
+	{#snippet blurHeader()}
+		<div class="flex flex-col">
+			<div class="title-small italic">Parameters:</div>
+			<Slider
+				name="Delay"
+				id="blurDelay"
+				bind:value={blurParameters.delay}
+				min={0}
+				max={3000}
+				step={100}
+			/>
+			<Slider
+				name="Duration"
+				id="blurDuration"
+				bind:value={blurParameters.duration}
+				min={0}
+				max={3000}
+				step={100}
+			/>
+			<Slider
+				name="Amount"
+				id="blurAmount"
+				bind:value={blurParameters.amount}
+				min={0}
+				max={20}
+				step={1}
+			/>
+			<Slider
+				name="Opacity"
+				id="blurOpacity"
+				bind:value={blurParameters.opacity}
+				min={0}
+				max={1}
+				step={0.1}
+			/>
+			<div class="divider divider-outline py-2"></div>
+			<div class="title-small italic">Notes:</div>
+			<div class="body-small">- <code>easing</code> not implemented.</div>
+			<div class="divider divider-outline py-2"></div>
+			<div class="title-small italic">Playground:</div>
+			<div class="input-filled">
+				<input
+					type="text"
+					placeholder="New blur item"
+					bind:value={newBlur}
+					onblur={() => pushItem(newBlur, blurItems)}
+					onkeydown={(e) => {
+						if (e.key === 'Enter') {
+							pushItem(newBlur, blurItems);
+						}
+					}}
+					class="input"
+					id="blurItem"
+				/>
+				<label class="input-filled-label" for="blurItem">Add some text</label>
+			</div>
+		</div>
+	{/snippet}
+	<Box title="Blur" header={blurHeader}>
+		<ul class="h-38 list-inside overflow-y-scroll py-2">
+			{#each blurItems as item, idx (idx)}
+				<li class="title" transition:blur={blurParameters}>
+					<div class="flex flex-row justify-between">
+						{item}
+						<button
+							class="btn btn-error-container btn-circle btn-gradient btn-sm"
+							aria-label="Remove Item"
+							onclick={() => blurItems.splice(idx, 1)}
+						>
+							<span class="icon-[fa6-solid--minus] size-4"></span>
+						</button>
+					</div>
+				</li>
+			{/each}
+		</ul>
+	</Box>
 
-<div class="w-full md:grid sm md:grid-cols-4 md:gap-4">
-    {#snippet blurHeader()}
-<div class="flex flex-col">
-    <div class="title-small italic">Parameters:</div>
-    <Slider
-        name="Delay"
-        id="blurDelay"
-        bind:value={blurParameters.delay}
-        min={0}
-        max={3000}
-        step={100}
+	{#snippet crossfadeHeader()}
+		<div class="flex flex-col">
+			<div class="title-small italic">Parameters:</div>
+			<Slider
+				name="Delay"
+				id="crossfadeDelay"
+				bind:value={crossfadeParameters.delay}
+				min={0}
+				max={3000}
+				step={100}
+			/>
+			<Slider
+				name="Duration"
+				id="crossfadeDuration"
+				bind:value={crossfadeParameters.duration}
+				min={0}
+				max={3000}
+				step={100}
+			/>
+		</div>
+		<div class="divider divider-outline py-2"></div>
+		<div class="title-small italic">Notes:</div>
+		<div class="body-small">- <code>easing</code> not implemented.</div>
+		<div class="body-small">- Also uses <code>animate:flip</code>.</div>
+		<div class="divider divider-outline py-2"></div>
+		<div class="title-small italic">Playground:</div>
+	{/snippet}
 
-    />
-    <Slider
-        name="Duration"
-        id="blurDuration"
-        bind:value={blurParameters.duration}
-        min={0}
-        max={3000}
-        step={100}
+	<Box title="Crossfade" header={crossfadeHeader}>
+		<div class="grid grid-cols-2">
+			<ul class="h-85 w-full list-inside overflow-y-scroll">
+				{#each left as item (item)}
+					<li class="title" animate:flip={crossfadeParameters}>
+						<button
+							onclick={() => {
+								left = left.filter((i) => i !== item);
+								right.push(item);
+							}}
+							in:receiveCrossfade={{ key: item, ...crossfadeParameters }}
+							out:sendCrossfade={{ key: item, ...crossfadeParameters }}
+						>
+							{item.name}
+						</button>
+					</li>
+				{/each}
+			</ul>
+			<ul class="h-85 w-full list-inside overflow-y-scroll">
+				{#each right as item (item)}
+					<li class="title" animate:flip={crossfadeParameters}>
+						<button
+							onclick={() => {
+								right = right.filter((i) => i !== item);
+								left.push(item);
+							}}
+							in:receiveCrossfade={{ key: item, ...crossfadeParameters }}
+							out:sendCrossfade={{ key: item, ...crossfadeParameters }}
+						>
+							{item.name}
+						</button>
+					</li>
+				{/each}
+			</ul>
+		</div>
+	</Box>
 
-    />
-        <Slider
-        name="Amount"
-        id="blurAmount"
-        bind:value={blurParameters.amount}
-        min={0}
-        max={20}
-        step={1}
+	{#snippet durationCheckbox()}
+		<div class="flex items-center pr-2">
+			<input
+				type="checkbox"
+				id="useDrawDuration"
+				bind:checked={useDrawDuration}
+				class="checkbox checkbox-sm"
+			/>
+		</div>
+	{/snippet}
 
-    />
-        <Slider
-        name="Opacity"
-        id="blurOpacity"
-        bind:value={blurParameters.opacity}
-        min={0}
-        max={1}
-        step={0.1}
+	<Box title="Draw">
+		<div class="title-small italic">Parameters:</div>
+		<Slider
+			name="Delay"
+			id="drawDelay"
+			bind:value={drawParameters.delay}
+			min={0}
+			max={3000}
+			step={100}
+		/>
+		<Slider
+			name="Speed"
+			id="drawSpeed"
+			bind:value={drawParameters.speed}
+			min={0.01}
+			max={0.1}
+			step={0.01}
+		/>
 
-    />
-    <div class="divider divider-outline py-2"></div>
-    <div class="title-small italic">Notes:</div>
-    <div class="body-small"><code>easing</code> not implemented.</div>
-    <div class="divider divider-outline py-2"></div>
-    <div class="title-small italic">Playground:</div>
-    <div class="input-filled">
-        <input
-            type="text"
-            placeholder="New blur item"
-            bind:value={newBlur}
-            onblur={() => pushItem(newBlur, blurItems)}
-            onkeydown={(e) => {
-                if (e.key === 'Enter') {
-                    pushItem(newBlur, blurItems);
-                }
-            }}
-            class="input"
-            id="blurItem"
-        />
-        <label class="input-filled-label" for="blurItem">Add some text</label>
-    </div>
-</div>
-{/snippet}
-    <Box title="Blur" header={blurHeader}>
-        <ul class="h-38 py-2 list-inside overflow-y-scroll">
-            {#each blurItems as item, idx (idx)}
-                <li class="title" transition:blur={{...blurParameters}}>
-                    <div class="flex flex-row justify-between">
-                        {item}
-                        <button
-                            class="btn btn-error-container btn-circle btn-gradient btn-sm"
-                            aria-label="Remove Item"
-                            onclick={() => blurItems.splice(idx, 1)}
-                        >
-                            <span class="icon-[fa6-solid--minus] size-4"></span>
-                        </button>
-                    </div>
-                </li>
-            {/each}
-        </ul>
-    </Box>
+		<Slider
+			preName={durationCheckbox}
+			name="Duration"
+			id="drawDuration"
+			bind:value={drawParameters.duration}
+			min={0}
+			max={3000}
+			step={100}
+		/>
+		<div class="divider divider-outline py-2"></div>
+		<div class="title-small italic">Notes:</div>
+		<div class="body-small">
+			- drawing from <a
+				href="https://svelte.dev/playground/149a5c35040343daa9477e0d54412398?version=3.31.0">here</a
+			>.
+		</div>
+		<div class="body-small">- <code>duration</code> takes precedence over <code>speed</code>.</div>
+		<div class="body-small">- <code>speed</code> uses stroke length to calculate duration.</div>
+		<div class="body-small">- <code>easing</code> not implemented.</div>
+		<div class="divider divider-outline py-2"></div>
+		<div class="title-small italic">Playground:</div>
+		<button
+			class="btn w-full rounded-full px-2"
+			onclick={() => {
+				drawShow = !drawShow;
+			}}
+		>
+			{drawShow ? 'Hide' : 'Show'} drawing
+		</button>
+		{#if drawShow}
+			<div class="flex justify-center">
+				<svg width={110} height={110} viewBox="0 0 12 12" class="stroke-primary">
+					<g transform="translate(0 12) scale(1 -1)">
+						<path
+							transition:draw={drawParameters}
+							d="M 2 5 v-4 h3 v3 h2 v-3 h3 v4 h-9 l 5 4 l 5 -4 h-1"
+							fill="none"
+							stroke-width="0.3px"
+						/>
+					</g>
+				</svg>
+			</div>
+		{/if}
+	</Box>
 
-    {#snippet crossfadeHeader()}
-    <div class="flex flex-col">
-        <div class="title-small italic">Parameters:</div>
-        <Slider
-            name="Delay"
-            id="crossfadeDelay"
-            bind:value={crossfadeParameters.delay}
-            min={0}
-            max={3000}
-            step={100}
-
-        />
-        <Slider
-            name="Duration"
-            id="crossfadeDuration"
-            bind:value={crossfadeParameters.duration}
-            min={0}
-            max={3000}
-            step={100}
-
-        />
-    </div>
-    <div class="divider divider-outline py-2"></div>
-    <div class="title-small italic">Notes:</div>
-    <div class="body-small"><code>easing</code> not implemented.</div>
-    <div class="body-small">also uses <code>animate:flip</code>.</div>
-    <div class="divider divider-outline py-2"></div>
-    <div class="title-small italic">Playground:</div>
-    {/snippet}
-
-    <Box title="Crossfade" header={crossfadeHeader}>
-        <div class="grid grid-cols-2">
-            <ul class="h-85 w-full list-inside overflow-y-scroll">
-                {#each left as item (item)}
-                <li class="title" animate:flip={{ ...crossfadeParameters }}>
-                    <button
-                        onclick={() => {
-                        left = left.filter((i) => i !== item);
-                        right.push(item)
-                        }}
-                        in:receiveCrossfade={{ key: item, ...crossfadeParameters }}
-                        out:sendCrossfade={{ key: item, ...crossfadeParameters }}
-                        
-                    >
-                        {item.name}
-                    </button>
-                </li>
-                {/each}
-            </ul>
-            <ul class="h-85 w-full list-inside overflow-y-scroll">
-                {#each right as item (item)}
-                <li class="title" animate:flip={{ ...crossfadeParameters }}>
-                    <button
-                        onclick={() => {
-                        right = right.filter((i) => i !== item);
-                        left.push(item)
-                        }}
-                        in:receiveCrossfade={{ key: item, ...crossfadeParameters }}
-                        out:sendCrossfade={{ key: item, ...crossfadeParameters }}
-                    >
-                        {item.name}
-                    </button>
-                </li>
-                {/each}
-            </ul>
-        </div>
-    </Box>
-
-    <Box title="Draw">
-        TBD
-    </Box>
-
-    <Box title="Fade">
-        <div class="input-filled">
-            <input
-                type="text"
-                placeholder="New fade item"
-                bind:value={newFade}
-                onblur={() => pushItem(newFade, fadeItems)}
-                onkeydown={(e) => {
-                    if (e.key === 'Enter') {
-                        pushItem(newFade, fadeItems);
-                    }
-                }}
-                class="input"
-                id="fadeItem"
-            />
-            <label class="input-filled-label" for="fadeItem">Add some text</label>
-        </div>
-        <ul class="h-85 list-inside overflow-y-scroll">
-            {#each fadeItems as item, idx (idx)}
-                <li class="title" transition:fade>
-                    <div class="flex flex-row justify-between">
-                        {item}
-                        <button
-                            class="btn btn-error-container btn-circle btn-gradient btn-sm"
-                            aria-label="Remove Item"
-                            onclick={() => fadeItems.splice(idx, 1)}
-                        >
-                            <span class="icon-[fa6-solid--minus] size-4"></span>
-                        </button>
-                    </div>
-                </li>
-            {/each}
-        </ul>
-    </Box>
+	<Box title="Fade">
+		<div class="input-filled">
+			<input
+				type="text"
+				placeholder="New fade item"
+				bind:value={newFade}
+				onblur={() => pushItem(newFade, fadeItems)}
+				onkeydown={(e) => {
+					if (e.key === 'Enter') {
+						pushItem(newFade, fadeItems);
+					}
+				}}
+				class="input"
+				id="fadeItem"
+			/>
+			<label class="input-filled-label" for="fadeItem">Add some text</label>
+		</div>
+		<ul class="h-85 list-inside overflow-y-scroll">
+			{#each fadeItems as item, idx (idx)}
+				<li class="title" transition:fade>
+					<div class="flex flex-row justify-between">
+						{item}
+						<button
+							class="btn btn-error-container btn-circle btn-gradient btn-sm"
+							aria-label="Remove Item"
+							onclick={() => fadeItems.splice(idx, 1)}
+						>
+							<span class="icon-[fa6-solid--minus] size-4"></span>
+						</button>
+					</div>
+				</li>
+			{/each}
+		</ul>
+	</Box>
 </div>
--- frontend_svelte/src/routes/(layout)/playground/design/svelte/Slider.svelte
+++ frontend_svelte/src/routes/(layout)/playground/design/svelte/Slider.svelte
@@ -1,27 +1,42 @@
 <script lang="ts">
+	import type { Snippet } from 'svelte';
+
 	let {
 		name,
 		id,
-		value = $bindable(0),
+		value = $bindable(),
 		min = 0,
 		max = 1000,
-		step = 100
+		step = 100,
+		preName
 	}: {
 		name: string;
 		id: string;
-		value?: number;
+		value?: number | undefined;
 		min?: number;
 		max?: number;
 		step?: number;
+		preName?: Snippet;
 	} = $props();
 </script>
 
 <div>
 	<label class="label label-text flex justify-between" for={id}
-		>{name}:
+		><div class="flex flex-row">{@render preName?.()}{name}:</div>
 		<span class="label">
 			<code class="label-text-alt">{value}</code>
 		</span></label
 	>
-	<input type="range" {min} {max} {step} class="range w-full" aria-label={name} {id} bind:value />
+
+	<input
+		type="range"
+		{min}
+		{max}
+		{step}
+		class="range w-full"
+		aria-label={name}
+		{id}
+		bind:value
+		disabled={value === undefined}
+	/>
 </div>
