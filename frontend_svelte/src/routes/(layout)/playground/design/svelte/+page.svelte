fe1a0e1e8b65e957e7a2061a09deb77df28b8a17
arnoldknott <knott@dtu.dk>
2025-07-05 10:58:17 +0200

    chore: adds crossfade transition

--- frontend_svelte/src/routes/(layout)/playground/design/svelte/+page.svelte
+++ frontend_svelte/src/routes/(layout)/playground/design/svelte/+page.svelte
@@ -1,64 +1,29 @@
 <script lang="ts">
 	import Heading from '$components/Heading.svelte';
-	import { blur, crossfade, draw, fade, fly, scale, slide, type BlurParams,  } from 'svelte/transition';
-    // import { blur } from 'svelte/transition';
+	import { blur, crossfade, draw, fade, fly, scale, slide  } from 'svelte/transition';
+    const [send, receive] = crossfade({});
 
-    const data = $state([
-        {
-            name: 'Blur',
-            animation: blur,
-            newItem: '',
-            itemList: [] as string[],
-        },
-        {
-            name: 'Crossfade',
-            animation: crossfade,
-            newItem: '',
-            itemList: [] as string[],
-        },
-        {
-            name: 'Draw',
-            animation: draw,
-            newItem: '',
-            itemList: [] as string[],
-        },
-        {
-            name: 'Fade',
-            animation: fade,
-            newItem: '',
-            itemList: [] as string[],
-        },
-        {
-            name: 'Fly',
-            animation: fly,
-            newItem: '',
-            itemList: [] as string[],
-        },
-        {
-            name: 'Scale',
-            animation: scale,
-            newItem: '',
-            itemList: [] as string[],
-        },
-        {
-            name: 'Slide',
-            animation: slide,
-            newItem: '',
-            itemList: [] as string[],
-        }
-    ])
-
-    const blurData = $state(
-        {
-            name: 'Blur',
-            animation: blur,
-            newItem: '',
-            itemList: [] as string[],
-        }
-    )
 
 	let newBlur = $state<string>('');
 	let blurItems = $state<string[]>([]);
+
+	let left = $state([ 'Item 1', 'Item 2', 'Item 3' ]);
+    let right = $state([ 'Item A', 'Item B', 'Item C' ]);
+
+    let newDraw = $state<string>('');
+    let drawItems = $state<string[]>([]);
+
+    let newFade = $state<string>('');
+    let fadeItems = $state<string[]>([]);
+
+    let newFly = $state<string>('');
+    let flyItems = $state<string[]>([]);
+
+    let newScale = $state<string>('');
+    let scaleItems = $state<string[]>([]);
+
+    let newSlide = $state<string>('');
+    let slideItems = $state<string[]>([]);
 
 	const pushItem = (newItem: string, list: string[]) => {
         console.log('Pushing item:', newItem, 'to list:', list);
@@ -69,61 +34,12 @@
 	};
 </script>
 
-{#snippet box(name: string, animation: (node: Element, { delay, duration, easing, amount, opacity }?: BlurParams | undefined) => TransitionConfig, newItem: string, itemList: string[])}
-    <div>
-		<Heading>{name}</Heading>
-		<div class="bg-base-150 shadow-outline h-100 w-90% rounded-lg p-2 shadow-inner">
-			<div class="flex flex-row">
-				<div class="title-small italic">{name} list</div>
-				<div class="input-filled">
-					<input
-						type="text"
-						placeholder="New blur item"
-						value={newItem}
-						onblur={() => pushItem(newItem, itemList)}
-						onkeydown={(e) => {
-							if (e.key === 'Enter') {
-								pushItem(newItem, itemList);
-							}
-						}}
-						class="input"
-						id="blurItem"
-					/>
-					<label class="input-filled-label" for="blurItem">Add some text</label>
-				</div>
-				<!-- <button
-                class="btn-neutral-container btn btn-circle btn-gradient"
-                onclick={() => {pushItem(newBlur, blurItems)}}
-                aria-label="Add"
-            >
-                <span class="icon-[fa6-solid--plus]"></span>
-            </button> -->
-			</div>
-			<div class="divider divider-outline"></div>
-			<ul class="h-85 list-inside overflow-y-scroll">
-				{#each itemList as item, idx (idx)}
-					<li class="title" transition:animation>
-						<div class="flex flex-row justify-between">
-							{item}
-							<button
-								class="btn btn-error-container btn-circle btn-gradient btn-sm"
-								aria-label="Remove Item"
-								onclick={() => blurItems.splice(idx, 1)}
-							>
-								<span class="icon-[fa6-solid--minus] size-4"></span>
-							</button>
-						</div>
-					</li>
-				{/each}
-			</ul>
-		</div>
-	</div>
-{/snippet}
+<Heading>Transitions</Heading>
 
-<div class="w-full md:grid md:grid-cols-4 xl:gap-4">
-	<div>
-		<Heading>Blur</Heading>
-		<div class="bg-base-150 shadow-outline h-100 w-90% rounded-lg p-2 shadow-inner">
+<div class="w-full md:grid sm md:grid-cols-4 md:gap-4">
+	<div class="w-full">
+		<div class="title-large text-center">Blur</div>
+		<div class="bg-base-150 shadow-outline h-100 w-full rounded-lg p-2 shadow-inner">
 			<div class="flex flex-row">
 				<div class="title-small italic">Blur list</div>
 				<div class="input-filled">
@@ -170,11 +86,44 @@
 		</div>
 	</div>
 
-    {@render box(blurData.name, blurData.animation, blurData.newItem, blurData.itemList)}
-    {#each data as animation, idx   (idx)}
-        {@render box(animation.name, animation.animation, animation.newItem, animation.itemList)}
-    {/each}
-	<div>
-		<Heading>Crossfade</Heading>
+	<div class="w-full">
+		<div class="title-large text-center">Crossfade</div>
+        <div class="bg-base-150 shadow-outline h-100 w-full rounded-lg p-2 shadow-inner">
+            <div class="title-small italic">Crossfade Lists</div>
+			<div class="divider divider-outline"></div>
+            <div class="grid grid-cols-2 gap-8">
+                <ul class="h-85 list-inside overflow-y-scroll">
+                    {#each left as item, idx (idx)}
+                        <li class="title">
+                        <button
+                            onclick={() => {
+                            left = left.filter((i) => i !== item);
+                            right.push(item)
+                            }}
+							in:receive={{ key: item, duration: 300 }}
+							out:send={{ key: item, duration: 300 }}>
+							{item}
+						</button>
+					</li>
+                    {/each}
+                </ul>
+                <ul class="h-85 list-inside overflow-y-scroll">
+                    {#each right as item, idx (idx)}
+                    <li class="title">
+                        <button
+                            onclick={() => {
+                            right = right.filter((i) => i !== item);
+                            left.push(item)
+                            }}
+							in:receive={{ key: item, duration: 300 }}
+							out:send={{ key: item, duration: 300 }}>
+							{item}
+						</button>
+					</li>
+                    {/each}
+                </ul>
+            </div>
+			
+		</div>
 	</div>
 </div>
