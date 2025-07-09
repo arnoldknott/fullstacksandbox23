57ca88224599c01c884c390763d91982e2c90609
arnoldknott <knott@dtu.dk>
2025-07-09 17:45:44 +0200

    chore: adds a modal for creating a new ueber group to identities and adds a shadow to the buttons in identities

--- frontend_svelte/src/routes/(layout)/(protected)/dashboard/identities/+page.svelte
+++ frontend_svelte/src/routes/(layout)/(protected)/dashboard/identities/+page.svelte
@@ -59,12 +59,12 @@
 		>
 			<p class="body">{microsoftTeam.description}</p>
 			<a href={microsoftTeam.webUrl} target="_blank" rel="noopener"
-				><button class="btn btn-primary-container"
+				><button class="btn btn-primary-container shadow-outline shadow-md"
 					><span class="icon-[bi--microsoft-teams]"></span>Open in Microsoft Teams</button
 				></a
 			>
 			<a href="./identities/msteams/{microsoftTeam.id}"
-				><button class="btn btn-info-container"
+				><button class="btn btn-info-container shadow-outline shadow-md"
 					><span class="icon-[tabler--info-triangle]"></span>More information</button
 				></a
 			>
@@ -84,9 +84,73 @@
 			</div>
 		</div>
 		{#if data.session?.currentUser?.azure_token_roles?.find((roles) => roles === 'Admin')}
-			<button class="btn-warning-container btn mr-2 self-center" aria-label="Create Ueber Group">
+			<button
+				class="btn-warning-container btn shadow-outline mr-2 self-center shadow-md"
+				aria-haspopup="dialog"
+				aria-expanded="false"
+				aria-controls="add-element-modal"
+				aria-label="Create Ueber Group"
+				data-overlay="#add-ueber-group-modal"
+			>
 				<span class="icon-[material-symbols--edit-outline-rounded]"></span> Create Ueber Group
 			</button>
+
+			<div
+				id="add-ueber-group-modal"
+				class="overlay modal modal-middle overlay-open:opacity-100 hidden"
+				role="dialog"
+				tabindex="-1"
+			>
+				<div class="modal-dialog overlay-open:opacity-100">
+					<div class="modal-content bg-base-300 shadow-outline shadow">
+						<div class="modal-header">
+							<h3 class="modal-title">Add new Ueber Group</h3>
+							<button
+								type="button"
+								class="btn btn-circle btn-text btn-sm absolute end-3 top-3"
+								aria-label="Close"
+								data-overlay="#add-ueber-group-modal"
+							>
+								<span class="icon-[tabler--x] size-4"></span>
+							</button>
+						</div>
+						<div class="modal-body">
+							<div class="w-full overflow-x-auto">
+								<div class="input-filled input-base-content mb-2 w-fit grow">
+									<input
+										type="text"
+										placeholder="Name the demo resource"
+										class="input input-sm md:input-md shadow-shadow shadow-inner"
+										id="name_id_new_element"
+										name="name"
+									/>
+									<label class="input-filled-label" for="name_id_new_element">Name</label>
+								</div>
+								<div class="textarea-filled textarea-base-content w-full">
+									<textarea
+										class="textarea shadow-shadow shadow-inner"
+										placeholder="Describe the demo resource here."
+										id="description_id_new_element"
+										name="description"
+									>
+									</textarea>
+									<label class="textarea-filled-label" for="description_id_new_element">
+										Description
+									</label>
+								</div>
+							</div>
+						</div>
+						<div class="modal-footer">
+							<button
+								class="btn-warning-container btn btn-circle btn-gradient"
+								aria-label="Send Icon Button"
+							>
+								<span class="icon-[tabler--send-2]"></span>
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
 		{/if}
 	</div>
 
@@ -107,14 +171,14 @@
 					<p class="body">{uberGroup.description}</p>
 					<div class="flex gap-2">
 						<a href="./identities/ueber-group/{uberGroup.id}"
-							><button class="btn btn-info-container"
+							><button class="btn btn-info-container shadow-outline shadow-md"
 								><span class="icon-[tabler--info-triangle]"></span>More information</button
 							></a
 						>
-						<button class="btn btn-accent-container"
+						<button class="btn btn-accent-container shadow-outline shadow-md"
 							><span class="icon-[fa6-solid--plus]"></span> Add Group</button
 						>
-						<button class="btn btn-accent-container"
+						<button class="btn btn-accent-container shadow-outline shadow-md"
 							><span class="icon-[fa6-solid--plus]"></span> Add User</button
 						>
 					</div>
