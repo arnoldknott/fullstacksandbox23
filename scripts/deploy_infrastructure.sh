d2303b81b8fee9e8269ed730341608fcfc2f44f0
arnoldknott <knott@dtu.dk>
2025-07-17 13:53:33 +0200

    ci: accidential deployment to prod identified: untagged workflow overwrites the latest tagged image with main commit sha, and then prod loads that image next time the server scales from 0 to 1

--- scripts/deploy_infrastructure.sh
+++ scripts/deploy_infrastructure.sh
@@ -51,8 +51,6 @@
 #         -var "azure_subscription_id=${AZURE_SUBSCRIPTION_ID}" \
 #         -reconfigure
 
-# TBD: check if this script triggers the unwanted deployment to prod?
-
 echo "=== Running: deploy_infrastructure ==="
 
 echo ""
