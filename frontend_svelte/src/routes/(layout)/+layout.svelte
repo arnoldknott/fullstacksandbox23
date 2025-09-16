d286ed0f6b363b56349135e278674fbd1c576145
arnoldknott <knott@dtu.dk>
2025-09-16 10:34:36 +0200

    chore: adds another dropdown select for debugging chrome issue, that select closes immedeatly

--- frontend_svelte/src/routes/(layout)/+layout.svelte
+++ frontend_svelte/src/routes/(layout)/+layout.svelte
@@ -189,7 +189,7 @@
 
 <div bind:this={mainContent} class="mx-5 mt-5 h-full" use:applyTheming>
 	<nav
-		class="navbar rounded-box bg-base-100 sticky start-0 top-0 z-1 justify-between shadow-sm md:flex md:items-stretch"
+		class="navbar rounded-box bg-base-100 z-1 sticky start-0 top-0 justify-between shadow-sm md:flex md:items-stretch"
 	>
 		<div class="dropdown navbar-start inline-flex md:hidden rtl:[--placement:bottom-end]">
 			<button
@@ -246,6 +246,38 @@
 			<div class="heading-large navbar-center text-accent ml-1 flex items-center">23</div>
 		</div>
 		<div class="navbar-end flex items-center">
+			<div class="dropdown relative inline-flex">
+				<button
+					id="dropdown-default"
+					type="button"
+					class="dropdown-toggle btn btn-primary"
+					aria-haspopup="menu"
+					aria-expanded="false"
+					aria-label="Dropdown"
+				>
+					Dropdown
+					<span class="icon-[tabler--chevron-down] dropdown-open:rotate-180 size-4"></span>
+				</button>
+				<ul
+					class="dropdown-menu dropdown-open:opacity-100 hidden min-w-60"
+					role="menu"
+					aria-orientation="vertical"
+					aria-labelledby="dropdown-default"
+				>
+					<li>Dropdown item 1</li>
+					<li>Dropdown item 2</li>
+					<li>
+						<select class="select max-w-sm appearance-none" aria-label="select">
+							<option disabled selected>Pick your favorite Movie</option>
+							<option>The Godfather</option>
+							<option>The Shawshank Redemption</option>
+							<option>Pulp Fiction</option>
+							<option>The Dark Knight</option>
+							<option>Schindler's List</option>
+						</select>
+					</li>
+				</ul>
+			</div>
 			<div class="dropdown flex items-center [--auto-close:inside] rtl:[--placement:bottom-end]">
 				<span
 					id="dropdown-menu-icon-user"
