77887f31012c6c9ef21e54b3564d524aab3b214b
arnoldknott <knott@dtu.dk>
2025-10-07 15:38:53 +0200

    ci: retrigger pipeline by irrelevant change in entrypoint.

--- infrastructure/containerApps.tf
+++ infrastructure/containerApps.tf
@@ -362,6 +362,16 @@ resource "azurerm_container_app" "redisContainer" {
         name        = "REDIS_WORKER_PASSWORD"
         secret_name = "redis-worker-password"
       }
+      # # Bump config version to trigger new revision when any mounted file changes
+      # env {
+      #   name  = "REDIS_CONFIG_VERSION"
+      #   value = sha256(join(",", [
+      #     # filesha256("${path.module}/../cacheRedis/entrypoint.sh"),
+      #     filesha256("${path.module}/../cacheRedis/redis.conf"),
+      #     filesha256("${path.module}/../cacheRedis/redis-full.conf"),
+      #     filesha256("${path.module}/../cacheRedis/users_template.acl"),
+      #   ]))
+      # }
       volume_mounts {
         name = "${terraform.workspace}-redis-data"
         path = "/data"
