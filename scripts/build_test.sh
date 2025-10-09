77adde4da4a2ea9ee5f5f94c7a62003c5cc898b6
arnoldknott <knott@dtu.dk>
2025-10-09 13:44:52 +0200

    ci: renames folder backendAPI into backend, to prepare for split between two containers based on same code base: backend_api and backend_worker

--- scripts/build_test.sh
+++ scripts/build_test.sh
@@ -3,4 +3,4 @@
 
 REPO_ROOT_DIR=$(git rev-parse --show-toplevel)
 cd $REPO_ROOT_DIR
-COMPOSE_PROJECT_NAME=test-fssb23 docker compose -f compose.yml -f compose.override.test.yml --env-file backendAPI/src/tests/.env build
+COMPOSE_PROJECT_NAME=test-fssb23 docker compose -f compose.yml -f compose.override.test.yml --env-file backend/src/tests/.env build
