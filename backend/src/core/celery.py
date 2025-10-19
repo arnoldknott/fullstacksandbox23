eed93f9171324002cf7e7c35f895f0695d5bc5c3
arnoldknott <knott@dtu.dk>
2025-10-19 23:04:53 +0200

    doc: adds a comment about celery configuration

--- backend/src/core/celery.py
+++ backend/src/core/celery.py
@@ -6,4 +6,6 @@ print("👍 ⛏️ Celery started")
 
 broker = f"redis://worker:{config.REDIS_WORKER_PASSWORD}@{config.REDIS_HOST}:{config.REDIS_PORT}/{config.REDIS_WORKER_DB}"
 
+# TBD: add backend
+# TBD: move configuration in module
 celery_app = Celery("tasks", broker=broker)
