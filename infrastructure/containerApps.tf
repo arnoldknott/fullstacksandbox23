82cc518a406c0455ebd96b14f274e3c963408dca
arnoldknott <knott@dtu.dk>
2025-10-19 23:56:39 +0200

    ci: limits the ignore_changes to the image, that is managed through github actions

--- infrastructure/containerApps.tf
+++ infrastructure/containerApps.tf
@@ -66,9 +66,8 @@ resource "azurerm_container_app" "FrontendContainer" {
   revision_mode = "Single"
 
   # Never change the imaage of the container, as this is done in github actions!
-  # template[0].container[0], 
   lifecycle {
-    ignore_changes = [secret, revision_mode] #  ingress
+    ignore_changes = [template[0].container[0].image, secret, revision_mode] #  ingress
   }
 
   template {
@@ -152,10 +151,9 @@ resource "azurerm_container_app" "BackendContainer" {
   # Don't change back to Multiple - which github actions changes to single - see bug below!
   # The change to single also changes the ingress -> so don't change that back either.
 
-  # TBD: get back in, when environment variables are set azure:
-  # template[0].container[0], 
+  # TBD: get back in, when environment variables are set azure: 
   lifecycle {
-    ignore_changes = [secret, revision_mode, ingress] # TBD: get this back in once run on prod - to add volume mounts!
+    ignore_changes = [template[0].container[0].image, secret, revision_mode, ingress] # TBD: get this back in once run on prod - to add volume mounts!
   }
 
   # TBD: needs to change to single for now - but multiple is required for this script to run, otherwise time-out!
