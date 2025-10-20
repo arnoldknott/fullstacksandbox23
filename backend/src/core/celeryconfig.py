7e97f8ea6fecbf0067d56a96b590cdc93cf26c20
arnoldknott <knott@dtu.dk>
2025-10-20 15:51:50 +0200

    chore: includes the jobs in demo tasks in celery app

--- backend/src/core/celeryconfig.py
+++ backend/src/core/celeryconfig.py
@@ -1,7 +1,8 @@
 from core.config import config
 
 celeryconfig = {
-    "broker": f"redis://worker:{config.REDIS_WORKER_PASSWORD}@{config.REDIS_HOST}:{config.REDIS_PORT}/{config.REDIS_WORKER_DB}"
-    # TBD: add backend
+    "broker": f"redis://worker:{config.REDIS_WORKER_PASSWORD}@{config.REDIS_HOST}:{config.REDIS_PORT}/{config.REDIS_WORKER_DB}",
+    # Add other modules manually here:
+    "include": ["jobs.demo.tasks"],
 }
 
