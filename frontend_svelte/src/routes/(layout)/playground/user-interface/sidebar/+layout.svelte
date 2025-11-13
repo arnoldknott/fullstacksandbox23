c439d349e7198086911a614dedb2c95032e281e8
arnoldknott <knott@dtu.dk>
2025-11-13 13:22:07 +0100

    chore: moves sidebar inside layout for demonstration

--- frontend_svelte/src/routes/(layout)/playground/user-interface/sidebar/+layout.svelte
+++ frontend_svelte/src/routes/(layout)/playground/user-interface/sidebar/+layout.svelte
@@ -4,83 +4,89 @@
 	let { children }: { children: Snippet } = $props();
 </script>
 
-<dialog
-	id="collapsible-sidebar"
-	class="overlay border-base-content/20 overlay-open:translate-x-0 drawer drawer-start sm:overlay-layout-open:translate-x-0 hidden w-64 border-e [--auto-close:sm] [--body-scroll:true] [--is-layout-affect:true] [--opened:lg] sm:absolute sm:z-0 sm:flex sm:shadow-none lg:[--overlay-backdrop:false]"
-	tabindex="-1"
-	{@attach initOverlay}
->
-	<div class="drawer-body px-2 pt-4">
-		<ul class="menu p-0">
-			<li>
-				<a href="./sidebar#loreum1">
-					<span class="icon-[tabler--home] size-5"></span>
-					Home
-				</a>
-			</li>
-			<li>
-				<a href="./sidebar#loreum2">
-					<span class="icon-[tabler--user] size-5"></span>
-					Account
-				</a>
-			</li>
-			<li>
-				<a href="./sidebar#loreum3">
-					<span class="icon-[tabler--message] size-5"></span>
-					Notifications
-				</a>
-			</li>
-			<li>
-				<a href="./sidebar#loreum4">
-					<span class="icon-[tabler--mail] size-5"></span>
-					Email
-				</a>
-			</li>
-			<li>
-				<a href="./sidebar#loreum5">
-					<span class="icon-[tabler--calendar] size-5"></span>
-					Calendar
-				</a>
-			</li>
-			<li>
-				<a href="./sidebar#loreum6">
-					<span class="icon-[tabler--shopping-bag] size-5"></span>
-					Product
-				</a>
-			</li>
-			<li>
-				<a href="./sidebar#loreum7">
-					<span class="icon-[tabler--login] size-5"></span>
-					Sign In
-				</a>
-			</li>
-			<li>
-				<a href="./sidebar#loreum8">
-					<span class="icon-[tabler--logout-2] size-5"></span>
-					Sign Out
-				</a>
-			</li>
-		</ul>
-	</div>
-</dialog>
+<div class="relative">
+	<dialog
+		id="collapsible-sidebar"
+		class="overlay border-base-content/20 overlay-open:translate-x-0 drawer drawer-start sm:overlay-layout-open:translate-x-0 hidden w-64 border-e [--auto-close:sm] [--body-scroll:true] [--is-layout-affect:true] [--opened:lg] sm:absolute sm:z-0 sm:flex sm:shadow-none lg:[--overlay-backdrop:false]"
+		tabindex="-1"
+		{@attach initOverlay}
+	>
+		<div class="drawer-body px-2 pt-4">
+			<ul class="menu p-0">
+				<li>
+					<a href="./sidebar#loreum1">
+						<span class="icon-[tabler--home] size-5"></span>
+						Home
+					</a>
+				</li>
+				<li>
+					<a href="./sidebar#loreum2">
+						<span class="icon-[tabler--user] size-5"></span>
+						Account
+					</a>
+				</li>
+				<li>
+					<a href="./sidebar#loreum3">
+						<span class="icon-[tabler--message] size-5"></span>
+						Notifications
+					</a>
+				</li>
+				<li>
+					<a href="./sidebar#loreum4">
+						<span class="icon-[tabler--mail] size-5"></span>
+						Email
+					</a>
+				</li>
+				<li>
+					<a href="./sidebar#loreum5">
+						<span class="icon-[tabler--calendar] size-5"></span>
+						Calendar
+					</a>
+				</li>
+				<li>
+					<a href="./sidebar#loreum6">
+						<span class="icon-[tabler--shopping-bag] size-5"></span>
+						Product
+					</a>
+				</li>
+				<li>
+					<a href="./sidebar#loreum7">
+						<span class="icon-[tabler--login] size-5"></span>
+						Sign In
+					</a>
+				</li>
+				<li>
+					<a href="./sidebar#loreum8">
+						<span class="icon-[tabler--logout-2] size-5"></span>
+						Sign Out
+					</a>
+				</li>
+			</ul>
+		</div>
+	</dialog>
 
-<div class="sm:overlay-layout-open:ps-64 bg-base-100 min-h-full transition-all duration-300">
-	<!-- Navigation Toggle -->
-	<div class="px-2">
-		<button
-			type="button"
-			class="btn btn-text btn-square"
-			aria-haspopup="dialog"
-			aria-expanded="false"
-			aria-controls="collapsible-sidebar"
-			data-overlay="#collapsible-sidebar"
-			aria-label="Toggle Sidebar"
-		>
-			<span class="icon-[tabler--menu-2] size-5"></span>
-		</button>
+	<div class="sm:overlay-layout-open:ps-64 bg-base-100 min-h-full transition-all duration-300">
+		<!-- Navigation Toggle -->
+		<div class="px-2">
+			<button
+				type="button"
+				class="btn btn-text btn-square"
+				aria-haspopup="dialog"
+				aria-expanded="false"
+				aria-controls="collapsible-sidebar"
+				data-overlay="#collapsible-sidebar"
+				aria-label="Toggle Sidebar"
+			>
+				<span class="icon-[tabler--menu-2] size-5"></span>
+			</button>
+		</div>
 	</div>
-</div>
 
-<div class="overlay-layout-open:ps-64 transition-all duration-300">
-	{@render children?.()}
+	<div class="overlay-layout-open:ps-64 transition-all duration-300">
+		<p class="text-base-content/50 label label-large px-2 py-1">
+			Note, for demonstration of the sidebar, it is not all the way left in the window, but relative
+			to the content of this page.
+		</p>
+		{@render children?.()}
+	</div>
 </div>
