a3cff6feb7cb15f5047aedc38b5c99444d6ef6bc
arnoldknott <knott@dtu.dk>
2025-11-22 14:52:05 +0100

    chore: update on segmentation

--- frontend_svelte/src/routes/(layout)/playground/user-interface/sidebar/segmenting/+page.svelte
+++ frontend_svelte/src/routes/(layout)/playground/user-interface/sidebar/segmenting/+page.svelte
@@ -2,7 +2,13 @@
 	import Title from '$components/Title.svelte';
 	import Heading from '$components/Heading.svelte';
 	import Loreum from '../Loreum.svelte';
+	import Display from '$components/Display.svelte';
 </script>
+
+<Display
+	>Changes elements for Sidebar from hard coded to Svelte components and / or Snippets</Display
+>
+<p>Probably drop this, as the way from hierarchy to iteration includes this anyways.</p>
 
 <Heading id="loreum1"><span class="italic">First</span> Lorem ipsum dolor sit amet...</Heading>
 <Loreum />
