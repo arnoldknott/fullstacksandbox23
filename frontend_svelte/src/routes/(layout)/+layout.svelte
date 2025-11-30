4bc3d5b8246368cd641d7af4e44e9235a1d37ee7
arnoldknott <knott@dtu.dk>
2025-11-30 15:53:28 +0100

    chore: applies recursive sidebar to main app and replaces old layout

--- frontend_svelte/src/routes/(layout)/+layout.svelte
+++ frontend_svelte/src/routes/(layout)/+layout.svelte
@@ -8,7 +8,7 @@
 	import { onMount, type Snippet } from 'svelte';
 	import { page } from '$app/state';
 	import Guard from '$components/Guard.svelte';
-	import { initDropdown } from '$lib/userInterface';
+	import { initDropdown, initOverlay } from '$lib/userInterface';
 	import ThemePicker from './playground/components/ThemePicker.svelte';
 	import ArtificialIntelligencePicker from './playground/components/ArtificialIntelligencePicker.svelte';
 	import { themeStore } from '$lib/stores';
@@ -16,6 +16,8 @@
 	// import { enhance } from '$app/forms';
 	import { resolve } from '$app/paths';
 	import WelcomeModal from './WelcomeModal.svelte';
+	import type { SidebarItemContent } from '$lib/types';
+	import SidebarItem from './SidebarItem.svelte';
 
 	let { data, children }: { data: LayoutData; children: Snippet } = $props();
 
@@ -96,6 +98,19 @@
 		// Prevents page from updating/reloading:
 		return () => {};
 	};
+
+	// Sidebar:
+	let sidebarLinks: SidebarItemContent[] = $state([
+		{
+			name: 'Page 1',
+			pathname: resolve('/(layout)/playground/user-interface/sidebar/recursion/page1'),
+			icon: 'icon-[tabler--user]',
+			id: 'page1',
+			items: []
+		}
+	]);
+
+	let scrollspyParent: HTMLDivElement | null = $state(null);
 </script>
 
 <!-- style="--p: 0.45 .2 125" -->
@@ -111,8 +126,189 @@
 
 <!-- <JsonData data={theme.configuration}></JsonData> -->
 
+{#snippet sidebarToggleButton(classes: string, overlayModifier: object)}
+	<button
+		type="button"
+		class="btn btn-square btn-sm btn-outline btn-primary {classes}"
+		aria-haspopup="dialog"
+		aria-expanded="false"
+		aria-controls="collapsible-mini-sidebar"
+		aria-label="Toggle Sidebar"
+		{...overlayModifier}
+	>
+		<span
+			class="icon-[material-symbols--menu-open-rounded] overlay-minified:hidden flex size-6 max-sm:hidden"
+		></span>
+		<span class="icon-[material-symbols--menu] overlay-minified:flex hidden size-6 max-sm:flex"
+		></span>
+	</button>
+{/snippet}
+
+{#snippet navbarPartItem(href: string, icon: string, text: string, textClasses?: string)}
+	<li class="text-primary hidden items-center md:flex">
+		<a {href} aria-label={text} class="flex items-center gap-1"
+			><span class="{icon} size-6"></span>
+			<span class={textClasses}>{text}</span>
+		</a>
+	</li>
+{/snippet}
+
+{#snippet sidebarPartItem(href: string, icon: string, text: string, listItemClasses?: string)}
+	<li class="text-primary {listItemClasses}">
+		<a {href}>
+			<span class="{icon} size-5"></span>
+			<span class="overlay-minified:hidden">{text}</span>
+		</a>
+	</li>
+{/snippet}
+
 <div bind:this={mainContent} class="mx-5 mt-5 h-full" use:applyTheming>
 	<!-- TBD: put navbar into component -->
+	<nav
+		class="navbar rounded-box bg-base-100 shadow-shadow border-outline-variant relative sticky start-0 top-0 z-1 justify-between border-b shadow-sm md:flex md:items-center"
+	>
+		<div class="navbar-start rtl:[--placement:bottom-end]">
+			<ul class="menu menu-horizontal flex flex-nowrap items-center">
+				{@render sidebarToggleButton('hidden sm:flex', {
+					'data-overlay-minifier': '#collapsible-mini-sidebar'
+				})}
+				{@render sidebarToggleButton('sm:hidden', {
+					'data-overlay': '#collapsible-mini-sidebar'
+				})}
+				{@render navbarPartItem('/docs', 'icon-[oui--documentation]', 'Docs')}
+				{@render navbarPartItem(
+					'/playground',
+					'icon-[mdi--playground-seesaw]',
+					'Playground',
+					'hidden lg:block'
+				)}
+				<Guard>
+					<hr class="border-outline -mx-2 my-3" />
+					{@render navbarPartItem(
+						'/dashboard',
+						'icon-[material-symbols--dashboard-outline-rounded]',
+						'Dashboard',
+						'hidden lg:block'
+					)}
+				</Guard>
+				<!-- {@render navbarPartItem('/features', 'icon-[mdi--feature-highlight]', 'Features')}
+				{@render navbarPartItem('/apps', 'icon-[tabler--apps]', 'Apps')}
+				{@render navbarPartItem(
+					'/construction',
+					'icon-[maki--construction]',
+					'Construction',
+					'hidden lg:block'
+				)} -->
+				<!-- {@render navbarPartItem(
+					'/playground/user-interface/sidebar/hierarchy',
+					'icon-[streamline--hierarchy-2]',
+					'Hierarchy'
+				)} -->
+			</ul>
+		</div>
+		<div class="navbar-center flex flex-row max-sm:scale-50">
+			<div class="flex flex-col justify-center">
+				<div class="title-small text-primary italic" style="line-height: 1;">Fullstack</div>
+				<div class="title-small text-secondary font-bold tracking-wide" style="line-height: 1">
+					Platform
+				</div>
+			</div>
+			<div class="heading-large navbar-center text-accent ml-1 flex items-center">23</div>
+		</div>
+		<div class="navbar-end">
+			<button
+				class="btn btn-sm btn-text btn-circle text-primary size-8.5 md:hidden"
+				aria-label="Search"
+			>
+				<span class="icon-[tabler--search] size-5.5"></span>
+			</button>
+			<div class="input mx-2 max-w-56 rounded-full max-md:hidden">
+				<span class="icon-[tabler--search] text-base-content/80 my-auto me-3 size-5 shrink-0"
+				></span>
+				<label class="sr-only" for="searchInput">Search</label>
+				<input type="search" class="grow" placeholder="Search" id="searchInput" />
+			</div>
+			<div
+				class="dropdown flex items-center [--auto-close:inside] rtl:[--placement:bottom-end]"
+				{@attach initDropdown}
+			>
+				<span
+					id="dropdown-menu-icon-user"
+					class="dropdown-toggle {!loggedIn ? 'icon-[fa6-solid--user] bg-secondary size-6' : ''}"
+					role="button"
+					aria-haspopup="menu"
+					aria-expanded="false"
+					aria-label="User Menu"
+				>
+					{#if loggedIn}
+						<img
+							class="not-hover:mask-radial-t-0% h-10 min-w-10 rounded-full not-hover:mask-radial-from-40%"
+							src={resolve('/apiproxies/msgraph') + '?endpoint=/me/photo/$value'}
+							alt="you"
+						/>
+					{/if}
+				</span>
+				<ul
+					class="dropdown-menu bg-base-200 text-secondary shadow-outline dropdown-open:opacity-100 hidden shadow-md"
+					role="menu"
+					aria-orientation="vertical"
+					aria-labelledby="dropdown-menu-icon-user"
+				>
+					<ArtificialIntelligencePicker
+						{updateProfileAccount}
+						{saveProfileAccount}
+						bind:artificialIntelligenceForm
+						bind:artificialIntelligenceConfiguration
+					/>
+					<li>
+						<hr class="border-outline -mx-2 my-5" />
+					</li>
+					<ThemePicker
+						{updateProfileAccount}
+						{saveProfileAccount}
+						bind:themeForm
+						bind:mode
+						bind:themeConfiguration
+					/>
+					<li>
+						<hr class="border-outline -mx-2 my-5" />
+					</li>
+					<li class="flex items-center gap-2">
+						<button
+							aria-label="show Modal"
+							type="button"
+							class="dropdown-item dropdown-close"
+							aria-haspopup="dialog"
+							aria-expanded="false"
+							aria-controls="welcome-modal"
+							data-overlay="#welcome-modal"
+						>
+							<span class="icon-[tabler--eye] bg-secondary size-6"></span>
+							<span class="text-secondary grow">Show welcome modal</span>
+						</button>
+					</li>
+				</ul>
+			</div>
+			<div class="hidden items-center sm:flex md:ml-2">
+				{#if !loggedIn}
+					<button
+						class="btn btn-neutral shadow-neutral ml-2 rounded-full shadow-sm"
+						aria-label="Log In"
+					>
+						<a href="/login">Log in</a>
+					</button>
+				{:else}
+					<button
+						class="btn btn-neutral btn-outline shadow-neutral ml-2 rounded-full shadow-sm"
+						aria-label="Log Out"
+					>
+						<a href="/logout">Log out</a>
+					</button>
+				{/if}
+			</div>
+		</div>
+	</nav>
+	<!--
 	<nav
 		class="navbar rounded-box bg-base-100 border-outline-variant sticky start-0 top-0 z-1 justify-between border-b shadow-sm md:flex md:items-stretch"
 	>
@@ -229,7 +425,6 @@
 							aria-controls="welcome-modal"
 							data-overlay="#welcome-modal"
 						>
-							<!-- works via JavaScript: onclick={() => window.HSOverlay.open(welcomeModal)}  -->
 							<span class="icon-[tabler--eye] bg-secondary size-6"></span>
 							<span class="text-secondary grow">Show welcome modal</span>
 						</button>
@@ -255,8 +450,8 @@
 			</div>
 		</div>
 	</nav>
+	-->
 
-	<!-- TBD: put welcome modal into component -->
 	<WelcomeModal
 		bind:session={data.session}
 		bind:artificialIntelligenceConfiguration
@@ -265,122 +460,75 @@
 		{updateProfileAccount}
 		{saveProfileAccount}
 	/>
-	<!-- <div
-		id="welcome-modal"
-		class="overlay modal overlay-open:opacity-100 overlay-open:duration-300 modal-middle hidden [--body-scroll:true] [--overlay-backdrop:static]"
-		data-overlay-keyboard="false"
-		role="dialog"
-		tabindex="-1"
-		bind:this={welcomeModal}
+
+	<div
+		id="scrollspy-scrollable-parent"
+		class="grid h-screen overflow-y-auto"
+		bind:this={scrollspyParent}
 	>
-		<div class="modal-dialog modal-dialog-md">
-			<div class="modal-content bg-base-300 shadow-outline ring-outline-variant shadow-lg ring">
-				<div class="modal-header">
-					<span class="icon-[ph--smiley] size-6"></span>
-					<h3 class="modal-title grow pl-2">Welcome</h3>
-					<div class="align-center flex grow flex-row justify-center">
-						{page.data.session?.microsoftProfile.displayName}
-					</div>
-
-					<button
-						type="button"
-						class="btn btn-text btn-circle btn-sm absolute end-3 top-3"
-						aria-label="Close"
-						data-overlay="#welcome-modal"
-					>
-						<span class="icon-[tabler--x] size-4"></span>
-					</button>
-				</div>
-				<div class="modal-body flex flex-wrap justify-between">
-					<div class="align-center flex grow flex-row justify-center">
-						<div class="flex flex-col justify-center">
-							<div class="title-small text-primary italic" style="line-height: 1;">Fullstack</div>
-							<div
-								class="title-small text-secondary font-bold tracking-wide"
-								style="line-height: 1"
+		<aside
+			id="collapsible-mini-sidebar"
+			class="overlay overlay-minified:w-19 overlay-open:translate-x-0 drawer drawer-start border-base-content/20 hidden w-66 border-e pt-50 [--auto-close:sm] sm:absolute sm:z-0 sm:flex sm:translate-x-0 sm:shadow-none"
+			tabindex="-1"
+			{@attach initOverlay}
+		>
+			<div class="drawer-body px-2 pt-4">
+				<ul class="menu p-0">
+					{@render sidebarPartItem('/', 'icon-[material-symbols--home-outline-rounded]', 'Home')}
+					<!-- {@render sidebarPartItem('/features', 'icon-[mdi--feature-highlight]', 'Features', 'md:hidden')}
+				{@render sidebarPartItem('/apps', 'icon-[tabler--apps]', 'Apps', 'md:hidden')}
+				{@render sidebarPartItem(
+					'/construction',
+					'icon-[maki--construction]',
+					'Construction',
+					'md:hidden'
+				)} -->
+					<!-- {@render sidebarPartItem(
+					'/playground/user-interface/sidebar/hierarchy',
+					'icon-[streamline--hierarchy-2]',
+					'Hierarchy',
+					'md:hidden'
+				)} -->
+					{@render sidebarPartItem(
+						'/playground/user-interface/sidebar/recursion',
+						'icon-[stash--arrows-switch]',
+						'Recursion',
+						'md:hidden'
+					)}
+				</ul>
+				<div class="divider"></div>
+				<ul class="menu p-0">
+					{#each sidebarLinks as mainItem (mainItem.id)}
+						<SidebarItem
+							content={{ ...mainItem, pathname: mainItem.pathname || page.url.pathname }}
+							topLevel={true}
+						/>
+					{/each}
+					<li>
+						<div class="items-center sm:hidden md:ml-2">
+							<button
+								class="btn btn-primary btn-outline shadow-primary ml-2 rounded-full shadow-sm"
+								aria-label="LogInOut"
 							>
-								Platform
-							</div>
+								<a href="#top">LogInOut</a>
+							</button>
 						</div>
-						<div class="heading-large navbar-center text-accent ml-1 flex items-center">23</div>
-					</div>
-					<img
-						src="/starnberger-see-unset-20230807.jpg"
-						class="w-full rounded-lg mask-y-from-75% mask-y-to-100% mask-x-from-95% mask-x-to-100% opacity-70"
-						alt="Bavarian lake Starnberger See in sunset"
-					/>
-					<div class="m-1 h-full w-full content-center p-4 text-justify font-semibold">
-						Adjust your settings for Artificial Intelligence and Theme configuration now or later by
-						clicking at your user icon in the top right corner.
-					</div>
-					<div class="grid grid-cols-1 gap-2 max-sm:w-full sm:grid-cols-2">
-						<ul
-							class="shadow-outline bg-base-250 m-1 h-[257px] w-full rounded rounded-xl p-4 shadow-inner"
-							role="menu"
-							aria-orientation="vertical"
-							aria-labelledby="dropdown-menu-icon-user"
-						>
-							<ArtificialIntelligencePicker
-								{updateProfileAccount}
-								{saveProfileAccount}
-								bind:artificialIntelligenceForm
-								bind:artificialIntelligenceConfiguration
-							/>
-						</ul>
-						<ul
-							class="shadow-outline bg-base-250 m-1 h-[257px] w-full rounded rounded-xl p-4 shadow-inner"
-							role="menu"
-							aria-orientation="vertical"
-							aria-labelledby="dropdown-menu-icon-user"
-						>
-							<ThemePicker
-								{updateProfileAccount}
-								{saveProfileAccount}
-								bind:themeForm
-								bind:mode
-								bind:themeConfiguration
-							/>
-						</ul>
-					</div>
-				</div>
-				<div class="modal-footer">
-					<form
-						method="POST"
-						action="/?/putme"
-						use:enhance={async (input) => {
-							input.formData.append(
-								'ai-enabled',
-								artificialIntelligenceConfiguration.enabled.toString()
-							);
-							input.formData.append('model-picker', artificialIntelligenceConfiguration.model);
-							input.formData.append(
-								'ai-temperature',
-								artificialIntelligenceConfiguration.temperature.toString()
-							);
-							input.formData.append('color-picker', themeConfiguration.sourceColor);
-							input.formData.append('variant-picker', themeConfiguration.variant);
-							input.formData.append('contrast', themeConfiguration.contrast.toString());
-							updateProfileAccount(input);
-						}}
-					>
-						<button
-							type="submit"
-							onclick={() => (userUnregistered = false)}
-							data-overlay="#welcome-modal"
-							aria-label="Save profile"
-							class="btn btn-primary-container btn-gradient shadow-outline rounded-full shadow-sm"
-						>
-							<span class="icon-[tabler--send-2]"></span>Save profile
-						</button>
-					</form>
-				</div>
+					</li>
+				</ul>
 			</div>
+		</aside>
+		<!-- TBD: how many div's inside each other are necessary here? Consider cleaning up! -->
+		<div
+			id="scrollspy"
+			class="sm:overlay-minified:ps-19 bg-base-100 space-y-4 ps-64 pe-1 transition-all duration-300 max-sm:ps-0"
+		>
+			{@render children?.()}
 		</div>
-	</div> -->
-
-	<div class="mt-5">
-		{@render children?.()}
 	</div>
+
+	<!-- <div class="mt-5">
+		{@render children?.()}
+	</div> -->
 
 	<!-- <JsonData data={theme}></JsonData> -->
 </div>
