89e0f382a224763cfe328ebab0da91e4e2b28f90
arnoldknott <knott@dtu.dk>
2025-12-04 00:17:20 +0100

    chore: splits navbar demonstration into two and recreates the disappearing navbar on scrolling, as workd on small screens in 6b33032

---
+++ frontend_svelte/src/routes/(layout)/playground/user-interface/navbar/scrolling/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	// Recreates scrolling from 6b33032
+	import Loreum from './Loreum.svelte';
+</script>
+
+<main class="h-screen overflow-scroll">
+	<div id="mainContent" class="mx-5 mt-5 h-full">
+		<nav
+			class="navbar rounded-box bg-base-100 shadow-shadow border-outline-variant sticky start-0 top-0 z-1 justify-between border-b shadow-sm md:flex md:items-center"
+		>
+			<div class="navbar-start">LEFT</div>
+			<div class="navbar-center">NAVBAR</div>
+			<div class="navbar-end">RIGHT</div>
+		</nav>
+	
+
+		<div class="mt-5">
+			<Loreum />
+		</div>
+	</div>
+</main>
