b3b036e9df3a19ccd4e941a6a669329c6649b081
arnoldknott <knott@dtu.dk>
2026-01-09 15:55:22 +0100

    chore: changes creossfade to use Sveltes crossfade

--- frontend_svelte/src/routes/(layout)/playground/user-interface/navbar/custom-scrolltracking/SidebarLink.svelte
+++ frontend_svelte/src/routes/(layout)/playground/user-interface/navbar/custom-scrolltracking/SidebarLink.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { getContext, onMount, type Snippet } from 'svelte';
 	import type { Writable } from 'svelte/store';
+	import { crossfade } from 'svelte/transition';
+	import { cubicOut } from 'svelte/easing';
 	// import { page } from '$app/state';
 	// import { initScrollspy } from '$lib/userInterface';
 	import {
@@ -13,6 +15,11 @@
 	} from '$app/navigation';
 	import type { Attachment } from 'svelte/attachments';
 	// import { SvelteURL } from 'svelte/reactivity';
+
+	const [send, receive] = crossfade({
+		duration: 600,
+		easing: cubicOut
+	});
 
 	let {
 		href,
@@ -113,18 +120,19 @@
 			{:else}
 				<!-- Icon crossfade container -->
 				<span class="relative inline-block size-6">
-					<!-- Regular icon - fades out when active -->
-					<span
-						class="{icon} absolute inset-0 size-5 transition-opacity duration-600 {isActive
-							? 'opacity-0'
-							: 'opacity-100'}"
-					></span>
-					<!-- Active finger-pointing icon - fades in when active -->
-					<span
-						class="icon-[tabler--hand-finger-right] text-base-content/100 absolute inset-0 size-6 transition-opacity duration-600 {isActive
-							? 'opacity-100'
-							: 'opacity-0'}"
-					></span>
+					{#if isActive}
+						<span
+							in:receive={{ key: 'icon-' + elementId }}
+							out:send={{ key: 'icon-' + elementId }}
+							class="icon-[tabler--hand-finger-right] text-base-content/100 absolute inset-0 size-6"
+						></span>
+					{:else}
+						<span
+							in:receive={{ key: 'icon-' + elementId }}
+							out:send={{ key: 'icon-' + elementId }}
+							class="{icon} absolute inset-0 size-5"
+						></span>
+					{/if}
 				</span>
 			{/if}
 			<span class="overlay-minified:hidden {isActive ? 'text-base-content/100' : ''}"
