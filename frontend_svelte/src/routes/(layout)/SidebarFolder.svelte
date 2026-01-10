14f2ac77e7fa6a34d22295fbadd7bf76b8ff71eb
arnoldknott <knott@dtu.dk>
2026-01-10 21:48:27 +0100

    chore: stylistic change

--- frontend_svelte/src/routes/(layout)/SidebarFolder.svelte
+++ frontend_svelte/src/routes/(layout)/SidebarFolder.svelte
@@ -23,9 +23,9 @@
 	// Aggregate children's active states and propagate up
 	$effect(() => {
 		hasActiveChild = childActiveStates.some((active) => active);
-		if (hasActiveChild) {
-			// toggleCollapse();
-		}
+		// if (hasActiveChild) {
+		// toggleCollapse();
+		// }
 	});
 
 	const thisPage = $derived.by(() => (pathname: string) => pathname === page.url.pathname);
