1c31ef15856a04d5d92d795561039032a58fc74f
arnoldknott <knott@dtu.dk>
2026-01-10 15:59:46 +0100

    chore: adopts page 2 to Section

--- frontend_svelte/src/routes/(layout)/playground/user-interface/navbar/custom-scrolltracking/page2/+page.svelte
+++ frontend_svelte/src/routes/(layout)/playground/user-interface/navbar/custom-scrolltracking/page2/+page.svelte
@@ -3,6 +3,7 @@
 	import Heading from '$components/Heading.svelte';
 	import Loreum from '../../Loreum.svelte';
 	import Display from '$components/Display.svelte';
+	import Section from '$components/Section.svelte';
 </script>
 
 <div class="bg-warning-container/70 rounded-xl p-4">
@@ -31,47 +32,61 @@
 	<Heading id="pg2loreum6"><span class="italic">Sixth</span> Lorem ipsum dolor sit amet...</Heading>
 	<Loreum repetition={20} /> -->
 
-	<div id="pg2loreum1">
-		<Heading id="pg2loreum1title"
-			><span class="italic">First</span> Lorem ipsum dolor sit amet...</Heading
-		>
+	<Section id="pg2loreum1">
+		{#snippet header()}
+			<Heading>
+				<span class="italic">First</span> Lorem ipsum dolor sit amet...
+			</Heading>
+		{/snippet}
 		<Loreum />
-	</div>
+	</Section>
 
-	<div id="pg2loreum2">
-		<Heading id="pg2loreum2title"
-			><span class="italic">Second</span> Lorem ipsum dolor sit amet...</Heading
-		>
+	<Section id="pg2loreum2">
+		{#snippet header()}
+			<Heading>
+				<span class="italic">Second</span> Lorem ipsum dolor sit amet...
+			</Heading>
+		{/snippet}
 		<Loreum repetition={4} />
-	</div>
+	</Section>
 
-	<Heading id="pg2sub-category">Sub Category</Heading>
+	<Section id="pg2sub-category">
+		<Heading>Sub Category</Heading>
+	</Section>
 
-	<div id="pg2loreum3">
-		<Title id="pg2loreum3title"
-			><span class="italic">Third</span> Lorem ipsum dolor sit amet...</Title
-		>
+	<Section id="pg2loreum3">
+		{#snippet header()}
+			<Title id="pg2loreum3title">
+				<span class="italic">Third</span> Lorem ipsum dolor sit amet...
+			</Title>
+		{/snippet}
 		<Loreum />
-	</div>
+	</Section>
 
-	<div id="pg2loreum4">
-		<Title id="pg2loreum4title"
-			><span class="italic">Fourth</span> Lorem ipsum dolor sit amet...</Title
-		>
+	<Section id="pg2loreum4">
+		{#snippet header()}
+			<Title id="pg2loreum4title">
+				<span class="italic">Fourth</span> Lorem ipsum dolor sit amet...
+			</Title>
+		{/snippet}
 		<Loreum />
-	</div>
+	</Section>
 
-	<div id="pg2loreum5">
-		<Heading id="pg2loreum5title"
-			><span class="italic">Fifth</span> Lorem ipsum dolor sit amet...</Heading
-		>
+	<Section id="pg2loreum5">
+		{#snippet header()}
+			<Heading id="pg2loreum5title">
+				<span class="italic">Fifth</span> Lorem ipsum dolor sit amet...
+			</Heading>
+		{/snippet}
 		<Loreum />
-	</div>
+	</Section>
 
-	<div id="pg2loreum6">
-		<Heading id="pg2loreum6title"
-			><span class="italic">Sixth</span> Lorem ipsum dolor sit amet...</Heading
-		>
+	<Section id="pg2loreum6">
+		{#snippet header()}
+			<Heading id="pg2loreum6title">
+				<span class="italic">Sixth</span> Lorem ipsum dolor sit amet...
+			</Heading>
+		{/snippet}
 		<Loreum repetition={20} />
-	</div>
+	</Section>
 </div>
