9cb23d4c51cda0de817c8251dd357091b0635f01
arnoldknott <knott@dtu.dk>
2026-01-10 15:54:50 +0100

    chore: adds a component Section to provide the encapsulating div with the id for the intersection observer

--- frontend_svelte/src/routes/(layout)/playground/user-interface/navbar/custom-scrolltracking/page4/page4-1/+page.svelte
+++ frontend_svelte/src/routes/(layout)/playground/user-interface/navbar/custom-scrolltracking/page4/page4-1/+page.svelte
@@ -2,6 +2,7 @@
 	import Heading from '$components/Heading.svelte';
 	import Loreum from '../../../Loreum.svelte';
 	import Display from '$components/Display.svelte';
+	import Section from '$components/Section.svelte';
 </script>
 
 <div class="bg-primary-container/70 rounded-xl p-4">
@@ -9,9 +10,23 @@
 
 	<p>Page 4.1 is a subpage to page 4</p>
 
+	<Section id="loreum1">
+		{#snippet header()}<Heading
+				><span class="italic">First</span> Lorem ipsum dolor sit amet...</Heading
+			>{/snippet}
+		<Loreum repetition={30} />
+	</Section>
+	<Section id="loreum2">
+		{#snippet header()}<Heading
+				><span class="italic">Second</span> Lorem ipsum dolor sit amet...</Heading
+			>{/snippet}
+		<Loreum repetition={30} />
+	</Section>
+
+	<!-- 
 	<Heading id="loreum1"><span class="italic">First</span> Lorem ipsum dolor sit amet...</Heading>
 	<Loreum repetition={30} />
 
 	<Heading id="loreum2"><span class="italic">Second</span> Lorem ipsum dolor sit amet...</Heading>
-	<Loreum repetition={30} />
+	<Loreum repetition={30} /> -->
 </div>
