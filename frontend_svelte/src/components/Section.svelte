5275c3abe2be067e3a702ef737ad12fe57c2b05f
arnoldknott <knott@dtu.dk>
2026-01-10 15:56:24 +0100

    chore: adds missing file for Section implmentation

---
+++ frontend_svelte/src/components/Section.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { type Snippet } from 'svelte';
+	let { id, header, children }: { id: string; header?: Snippet; children: Snippet } = $props();
+</script>
+
+<div {id}>
+	{@render header?.()}
+	{@render children?.()}
+</div>
