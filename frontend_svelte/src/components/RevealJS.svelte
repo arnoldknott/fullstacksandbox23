ff80ae234d1eec4ea600bc6401533abcc589d89d
arnoldknott <knott@dtu.dk>
2026-01-27 20:17:36 +0100

    chore: reveal to accept all options as argument and make the javascript instance of Reveal bindable

--- frontend_svelte/src/components/RevealJS.svelte
+++ frontend_svelte/src/components/RevealJS.svelte
@@ -4,20 +4,30 @@
 	import type { Snippet } from 'svelte';
 	import { onMount } from 'svelte';
 	import Reveal from 'reveal.js';
+	import type {Options } from 'reveal.js';
+	type RevealType = typeof Reveal;
 
 	export const ssr = false;
 	// let { children, keyboard=true }: {  children: Snippet, keyboard: boolean} = $props();
-	let { children, keyboard = true }: { children: Snippet; keyboard?: boolean } = $props();
+	let { children, options = {}, reveal = $bindable() }: { children: Snippet; options?: Options; reveal?: RevealType } = $props();
+
+	
 
 	onMount(() => {
 		const reveal = new Reveal({});
 		reveal.initialize({
+			// Default options
 			embedded: true,
-			keyboard: keyboard,
 			slideNumber: 'c/t',
 			width: 1600,
 			height: 900,
-			margin: 0.01
+			margin: 0.01,
+			// Override with external options
+			...options
+		});
+		reveal.on('fragmentshown', (event) => {
+			console.log('=== fragment shown ===');
+			console.log(event);
 		});
 	});
 </script>
