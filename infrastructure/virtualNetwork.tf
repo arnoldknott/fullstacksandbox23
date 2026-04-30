564e8c32a3b509d91aef1cd08f558c5804c9e318
arnoldknott <knott@dtu.dk>
2026-04-30 15:35:43 +0200

    chore: prevent failure from virtual network distruction, coming from virtual machine

--- infrastructure/virtualNetwork.tf
+++ infrastructure/virtualNetwork.tf
@@ -77,16 +77,16 @@ resource "azurerm_subnet" "subnetPostgres" {
   }
 }
 
-# resource "azurerm_subnet" "subnetAdminVirtualMachine" {
-#   name                 = "${var.project_name}-adminVirtualMachine-${terraform.workspace}"
-#   resource_group_name  = azurerm_resource_group.resourceGroup.name
-#   virtual_network_name = azurerm_virtual_network.virtualNetwork.name
-#   address_prefixes     = ["10.0.10.0/24"]
+resource "azurerm_subnet" "subnetAdminVirtualMachine" {
+  name                 = "${var.project_name}-adminVirtualMachine-${terraform.workspace}"
+  resource_group_name  = azurerm_resource_group.resourceGroup.name
+  virtual_network_name = azurerm_virtual_network.virtualNetwork.name
+  address_prefixes     = ["10.0.10.0/24"]
 
-#   lifecycle {
-#     ignore_changes = [private_endpoint_network_policies]
-#   }
-# }
+  lifecycle {
+    ignore_changes = [private_endpoint_network_policies]
+  }
+}
 
 # resource "azurerm_subnet" "subnetFileStorage" {
 #   name                 = "${var.project_name}-fileStorage-${terraform.workspace}"
@@ -162,45 +162,45 @@ resource "azurerm_private_dns_zone_virtual_network_link" "privateDNSZoneLink" {
 # }
 
 
-# resource "azurerm_network_security_group" "adminVirtualMachineNetworkSecurityGroup" {
-#   count               = terraform.workspace == "dev" || terraform.workspace == "stage" ? 1 : 0
-#   name                = "${var.project_name}-virtualMachineNetworkSecurityGroup-${terraform.workspace}"
-#   location            = azurerm_resource_group.resourceGroup.location
-#   resource_group_name = azurerm_resource_group.resourceGroup.name
+resource "azurerm_network_security_group" "adminVirtualMachineNetworkSecurityGroup" {
+  count               = terraform.workspace == "dev" || terraform.workspace == "stage" ? 1 : 0
+  name                = "${var.project_name}-virtualMachineNetworkSecurityGroup-${terraform.workspace}"
+  location            = azurerm_resource_group.resourceGroup.location
+  resource_group_name = azurerm_resource_group.resourceGroup.name
 
-#   security_rule {
-#     name                       = "${var.project_name}-virtualMachineNetworkSecurityGroup-${terraform.workspace}-rule-ssh"
-#     priority                   = 100
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_range          = "*"
-#     destination_port_range     = "22"
-#     source_address_prefix      = "*"
-#     destination_address_prefix = "*"
-#   }
+  # security_rule {
+  #   name                       = "${var.project_name}-virtualMachineNetworkSecurityGroup-${terraform.workspace}-rule-ssh"
+  #   priority                   = 100
+  #   direction                  = "Inbound"
+  #   access                     = "Allow"
+  #   protocol                   = "Tcp"
+  #   source_port_range          = "*"
+  #   destination_port_range     = "22"
+  #   source_address_prefix      = "*"
+  #   destination_address_prefix = "*"
+  # }
 
-#   security_rule {
-#     name                       = "${var.project_name}-virtualMachineNetworkSecurityGroup-${terraform.workspace}-rule-ping"
-#     priority                   = 200
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Icmp"
-#     source_port_range          = "*"
-#     destination_port_range     = "*"
-#     source_address_prefix      = "*"
-#     destination_address_prefix = "*"
-#   }
+  # security_rule {
+  #   name                       = "${var.project_name}-virtualMachineNetworkSecurityGroup-${terraform.workspace}-rule-ping"
+  #   priority                   = 200
+  #   direction                  = "Inbound"
+  #   access                     = "Allow"
+  #   protocol                   = "Icmp"
+  #   source_port_range          = "*"
+  #   destination_port_range     = "*"
+  #   source_address_prefix      = "*"
+  #   destination_address_prefix = "*"
+  # }
 
-#   tags = {
-#     Costcenter  = var.costcenter
-#     Owner       = var.owner_name
-#     Environment = terraform.workspace
-#   }
-# }
+  tags = {
+    Costcenter  = var.costcenter
+    Owner       = var.owner_name
+    Environment = terraform.workspace
+  }
+}
 
-# resource "azurerm_subnet_network_security_group_association" "adminVirtualMachineNetworkSecurityGroupAssociation" {
-#   count                     = terraform.workspace == "dev" || terraform.workspace == "stage" ? 1 : 0
-#   subnet_id                 = azurerm_subnet.subnetAdminVirtualMachine.id
-#   network_security_group_id = azurerm_network_security_group.adminVirtualMachineNetworkSecurityGroup[0].id
-# }
+resource "azurerm_subnet_network_security_group_association" "adminVirtualMachineNetworkSecurityGroupAssociation" {
+  count                     = terraform.workspace == "dev" || terraform.workspace == "stage" ? 1 : 0
+  subnet_id                 = azurerm_subnet.subnetAdminVirtualMachine.id
+  network_security_group_id = azurerm_network_security_group.adminVirtualMachineNetworkSecurityGroup[0].id
+}
