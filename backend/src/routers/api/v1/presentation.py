34ba8ed3de3838696df4fd964809b96f84ec7815
arnoldknott <knott@dtu.dk>
2026-05-12 22:52:27 +0000

    doc: describes the redesign of the presentation endpoint

--- backend/src/routers/api/v1/presentation.py
+++ backend/src/routers/api/v1/presentation.py
@@ -54,6 +54,11 @@ async def get_presentation_by_id(
     return await presentation_view.get_by_id(resource_id, token_payload, guards)
 
 
+# TBD: redesign to remove the public endpooint and
+# make authentication optional in the get_by_id method
+# First try with authentication if provided
+# if not straight go to fetching the presentation without authentication and return it if it is public
+# Also allow filtering for path instead / before UUID from resource_id
 @router.get("/public/{resource_id}", status_code=200)
 async def get_public_presentation_by_id(
     resource_id: UUID,
