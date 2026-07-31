892fe628cfc7b9c694a17c0acc75069b7268b9b5
arnoldknott <knott@dtu.dk>
2026-07-31 13:38:56 +0000

    chore: update ingress on pgadmin

--- infrastructure/containerApps.tf
+++ infrastructure/containerApps.tf
@@ -605,7 +605,7 @@ resource "azurerm_container_app" "postgresAdmin" {
   resource_group_name          = azurerm_resource_group.resourceGroup.name
 
   lifecycle {
-    ignore_changes = [ingress]
+    # ignore_changes = [ingress]
   }
   revision_mode = "Single"
 
