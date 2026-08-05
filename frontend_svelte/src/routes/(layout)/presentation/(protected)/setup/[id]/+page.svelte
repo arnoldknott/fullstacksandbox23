8ad2c243ee8029afeb2eb480f99a913612bbbeeb
arnoldknott <knott@dtu.dk>
2026-08-05 19:19:55 +0000

    chore: turns presentation-setup from div's into cards and loads data from backend

--- frontend_svelte/src/routes/(layout)/presentation/(protected)/setup/[id]/+page.svelte
+++ frontend_svelte/src/routes/(layout)/presentation/(protected)/setup/[id]/+page.svelte
@@ -1,86 +1,92 @@
 <script lang="ts">
 	import { page } from '$app/state';
+	import Card from '$components/Card.svelte';
 	import Display from '$components/Display.svelte';
 	import Drawer from '$components/Drawer.svelte';
 	import Heading from '$components/Heading.svelte';
+	import JsonData from '$components/JsonData.svelte';
+
+	import type { PageData } from './$types';
+
+	let { data }: { data: PageData } = $props();
 </script>
+
+<JsonData data={data.payload} />
 
 <Display id="presentation">Presentation</Display>
 <Heading id="presentation-title">{page.params.id}</Heading>
 
-<div
-	class="title-large alert alert-warning bg-warning-container/20 text-warning-container-content/80 label-large text-center"
-	role="alert"
+<Card
+	id="source"
+	title="Source"
+	extraClasses="bg-warning-container/30 text-warning-container-content/80 label-large border-warning-container"
 >
-	Source
-	<p class="title text-center">
+	<p>
 		A card to set the source of the code for the presentation, like "intern", "github", which
 		branch, and or commit, and so on
 	</p>
-</div>
-<div
-	class="title-large alert alert-warning bg-warning-container/20 text-warning-container-content/80 label-large text-center"
-	role="alert"
+</Card>
+
+<Card
+	id="access"
+	title="Access"
+	extraClasses="bg-warning-container/30 text-warning-container-content/80 label-large border-warning-container"
 >
-	Access
-	<p class="title text-center">
+	<p>
 		Setting the access for the presentation, like "public", "private", "shared with specific users
 		or groups", that is access policies.
 	</p>
-</div>
-<div
-	class="title-large alert alert-warning bg-warning-container/20 text-warning-container-content/80 label-large text-center"
-	role="alert"
->
-	Questions
-	<p class="title text-center">
+</Card>
+<Card id="questions" title="Questions" extraClasses="label-large">
+	<p>
 		Linked questions. Adding a question and opening sidebar to select existing questions - for mode
 		copy (don't keep the original answers and don't keep in sync) or link (keeps answers in sync)
 	</p>
-</div>
-<Drawer id="copy-questions" title="Copy Questions">
-	<p class="title text-center">
-		Opening a side drawer to select existing questions - for mode copy (don't keep the original
-		answers and don't keep in sync)
-	</p>
-</Drawer>
-<Drawer id="link-questions" title="Link Questions">
-	<p class="title text-center">
-		Opening a side drawer to select existing questions - for mode link (keeps answers in sync)
-	</p>
-</Drawer>
-<div
-	class="title-large alert alert-warning bg-warning-container/20 text-warning-container-content/80 label-large text-center"
-	role="alert"
+	<Drawer id="copy-questions" title="Copy Questions">
+		<p class="title text-center">
+			Opening a side drawer to select existing questions - for mode copy (don't keep the original
+			answers and don't keep in sync)
+		</p>
+	</Drawer>
+	<Drawer id="link-questions" title="Link Questions">
+		<p class="title text-center">
+			Opening a side drawer to select existing questions - for mode link (keeps answers in sync)
+		</p>
+	</Drawer>
+</Card>
+<Card
+	id="links"
+	title="Links"
+	extraClasses="bg-warning-container/30 text-warning-container-content/80 label-large border-warning-container"
 >
-	Links
-	<p class="title text-center">
+	<p>
 		Links, that are under continuous check for 200 responses that are used inside the presentation.
 	</p>
-</div>
-<div
-	class="title-large alert alert-warning bg-warning-container/20 text-warning-container-content/80 label-large text-center"
-	role="alert"
+</Card>
+<Card
+	id="drawings"
+	title="(Drawings)"
+	extraClasses="bg-warning-container/30 text-warning-container-content/80 label-large border-warning-container"
 >
-	(Drawings)
 	<p class="title text-center">
 		Draw.io and Excalibur, that are linked and used inside the presentation. Add anew, modify linked
-		or iopen sidedrawer for linking / copying existing. Maybe work with templates?
+		or open side drawer for linking / copying existing. Maybe work with templates?
 	</p>
-</div>
-<div
-	class="title-large alert alert-warning bg-warning-container/20 text-warning-container-content/80 label-large text-center"
-	role="alert"
+</Card>
+<Card
+	id="files"
+	title="Files"
+	extraClasses="bg-warning-container/30 text-warning-container-content/80 label-large border-warning-container"
 >
-	Files
 	<p class="title text-center">
 		Uploading, updating, deleting files, that are referenced in the presentation. Can also open a
 		side drawer for managing existing files.
 	</p>
-</div>
-<div
-	class="title-large alert alert-warning bg-warning-container/20 text-warning-container-content/80 label-large text-center"
-	role="alert"
+</Card>
+<Card
+	id="misc"
+	title="Miscellaneous"
+	extraClasses="bg-warning-container/30 text-warning-container-content/80 label-large border-warning-container"
 >
 	(Maybe languages?, AI activation?, ...)
-</div>
+</Card>
