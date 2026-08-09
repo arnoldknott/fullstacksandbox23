65a9b71eef9c90a24044db33cf00f0f92a40b2dd
arnoldknott <knott@dtu.dk>
2026-08-09 14:16:47 +0000

    chore: moves count store into its own file inside a newly created stores folder

--- frontend_svelte/src/lib/stores.ts
+++ frontend_svelte/src/lib/stores.ts
@@ -3,5 +3,3 @@ import { writable } from 'svelte/store';
 import type { AppTheme } from './theming';
 
 export const themeStore = writable<AppTheme>({} as AppTheme);
-
-export const count = writable<number>(0);
