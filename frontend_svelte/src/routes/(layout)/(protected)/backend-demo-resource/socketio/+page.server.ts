ff8d082bc1abf583a5853a1c23f3770ff9d21ed3
arnoldknott <knott@dtu.dk>
2026-08-17 20:01:36 +0000

    chore: adds statistics about results from no exams to previous student-seminar presentation (commented)

--- frontend_svelte/src/routes/(layout)/(protected)/backend-demo-resource/socketio/+page.server.ts
+++ frontend_svelte/src/routes/(layout)/(protected)/backend-demo-resource/socketio/+page.server.ts
@@ -1,3 +1,4 @@
+import { IdentityType } from '$lib/accessHandler';
 import { backendAPI } from '$lib/server/apis/backendApi';
 import { microsoftGraph } from '$lib/server/apis/msgraph';
 import type { Identity } from '$lib/types';
@@ -12,9 +13,17 @@ export const load: PageServerLoad = async ({ locals }) => {
 		sessionId,
 		locals.sessionData.currentUser?.azure_token_groups
 	);
+	// all linked teams identities:
 	payload.identities.push(...myTeamsIdentities);
+	// all app internal identities (users, ueber-groups, groups, sub-groups, sub-sub-groups):
 	const allIdentities = await backendAPI.getAllIdentities(sessionId);
 	payload.identities.push(...allIdentities);
+	// one public identity:
+	payload.identities.push({
+		id: undefined,
+		name: 'Public',
+		type: IdentityType.PUBLIC
+	});
 
 	return { payload };
 };
