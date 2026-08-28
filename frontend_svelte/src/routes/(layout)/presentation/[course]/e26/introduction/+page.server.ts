1db4273881b20476e623301f3a9645e35df58c99
arnoldknott <knott@dtu.dk>
2026-08-28 17:37:27 +0000

    chore: removes hard coded debugging id from server side code for introduction presentation

--- frontend_svelte/src/routes/(layout)/presentation/[course]/e26/introduction/+page.server.ts
+++ frontend_svelte/src/routes/(layout)/presentation/[course]/e26/introduction/+page.server.ts
@@ -6,7 +6,6 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async ({ url }) => {
 	const presentationPath = url.pathname.split('/presentation/')[1];
 	const presentationResponse = await backendAPI.get(null, '/presentation/path/' + presentationPath);
-	// const presentationResponse = await backendAPI.get(null, '/presentation/' + '2713ce81-c6da-4c64-b63b-66abca6c1c1e');
 	const payload = {
 		presentation: {} as Presentation,
 		questions: [] as Question[]
