25bd66d94ba2830dd7a6592ec6e5793d341c84cf
arnoldknott <knott@dtu.dk>
2026-09-05 15:03:44 +0000

    chore: adopts the table aldo for numerical answers

--- frontend_svelte/src/routes/(layout)/question/(protected)/setup/[id]/+page.svelte
+++ frontend_svelte/src/routes/(layout)/question/(protected)/setup/[id]/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
 	import { onDestroy, onMount } from 'svelte';
-	import { flip } from 'svelte/animate';
+	// import { flip } from 'svelte/animate';
 	import { fade, slide } from 'svelte/transition';
 
 	import Card from '$components/Card.svelte';
@@ -54,9 +54,9 @@
 	// let messageAnswersSorted: MessageExtended[] = $derived(
 	// 	messageSocketio?.getSelectedEntities('sortedMessageAnswers') ?? []
 	// );
-	let numericalAnswersSorted: NumericalExtended[] = $derived(
-		numericalSocketio?.getSelectedEntities('sortedNumericalAnswers') ?? []
-	);
+	// let numericalAnswersSorted: NumericalExtended[] = $derived(
+	// 	numericalSocketio?.getSelectedEntities('sortedNumericalAnswers') ?? []
+	// );
 
 	const messageConnection: SocketioConnection = $derived({
 		namespace: '/message',
@@ -159,9 +159,12 @@
 				: language === 'de-DE'
 					? 'flagpack:de'
 					: 'flagpack:gb-ukm';
+
+	let hideNewNumericalAnswerCard = $state(true);
+	let numericalViewMode = $state<'grid' | 'list' | 'table'>('table');
 </script>
 
-{#snippet answerBubble(text: string | number, id: string, index: number)}
+<!-- {#snippet answerBubble(text: string | number, id: string, index: number)}
 	<div class="chat chat-receiver">
 		<div class="chat-bubble text-left {index % 2 ? 'chat-bubble-accent' : 'chat-bubble-primary'}">
 			<div class="flex flex-col">
@@ -187,7 +190,7 @@
 			</div>
 		</div>
 	</div>
-{/snippet}
+{/snippet} -->
 
 <Display>{data.questionsData?.questions.question || 'No question selected.'}</Display>
 {#if !data.questionsData?.questions.question}
@@ -243,8 +246,8 @@
 
 {#if messageSocketio?.pendingEntities[0]}
 	<Card
-		id="add-questions"
-		title="Add Questions"
+		id="add-messages"
+		title="Add Message Answer"
 		closeButton
 		extraClasses="label-large"
 		bind:hidden={hideNewMessageAnswerCard}
@@ -582,13 +585,341 @@
 
 <Heading id="numerical-answers">Numerical Answers:</Heading>
 
-<div class="mx-2 grid w-full grid-cols-5 gap-2">
+{#if numericalSocketio?.pendingEntities[0]}
+	<Card
+		id="add-numericals"
+		title="Add Numerical Answers"
+		closeButton
+		extraClasses="label-large"
+		bind:hidden={hideNewNumericalAnswerCard}
+	>
+		<!-- <p class="label">
+		{@render warning()} Add questions with tabs for new and existing, where the existing questions get
+		the button to copy and link. Also a copy all selected and link all selected.
+	</p> -->
+		<div class="bg-base-150 shadow-base-shadow overflow-y-auto rounded-lg shadow-inner">
+			<div
+				class="tabs tabs-lifted tabs-secondary m-1"
+				// class="tabs tabs-lifted to-secondary m-1 rounded-lg bg-linear-to-b from-transparent"
+				// class="tabs tabs-lifted to-secondary m-1 bg-linear-to-b from-transparent"
+				aria-label="Tabs"
+				role="tablist"
+				aria-orientation="horizontal"
+				{@attach initTabs}
+			>
+				<button
+					type="button"
+					// class="tab tab-secondary active w-full"
+					class="tab active w-full"
+					id="new-question-head"
+					data-tab="#new-question-tab"
+					aria-controls="new-question-tab"
+					role="tab"
+					aria-selected="true"
+				>
+					<span class="icon-[fa6-solid--plus] mr-2 size-4"></span>
+					<span class="hidden md:inline">Add</span>
+					<span class="hidden sm:inline">&nbsp;new</span>
+				</button>
+				<button
+					type="button"
+					// class="tab tab-secondary w-full"
+					class="tab w-full"
+					id="existing-question-head"
+					data-tab="#existing-question-tab"
+					aria-controls="existing-question-tab"
+					role="tab"
+					aria-selected="false"
+				>
+					<span class="icon-[tabler--copy] mr-2 size-4"></span>
+					<span class="hidden md:inline">Copy </span>
+					&nbsp;or&nbsp;
+					<span class="icon-[tabler--link] mx-2 size-4"></span>
+					<span class="hidden md:inline">link </span>
+					<span class="hidden sm:inline">&nbsp;existing</span>
+				</button>
+			</div>
+			<div class="h-fit p-2">
+				<!--
+				<div
+					id="new-question-tab"
+					class="h-full max-h-120 overflow-scroll"
+					role="tabpanel"
+					aria-labelledby="new-question-head"
+				>
+					<div class="label">
+						{@render warning()} Forms to add new questions, with the ability to add multiple questions
+						at once and inherit access rights - default: true - from presentation.
+					</div>
+					 {#snippet labelDescription()}
+						This label is the way to identify this particular quesion in the code of the
+						presentation. It is mandatory.
+					{/snippet}
+					<FormElement
+						title="Label"
+						mandatory
+						description={labelDescription}
+						extraClasses="max-w-300"
+					>
+						<div class="input-filled input-primary w-full">
+							<input
+								type="text"
+								placeholder=""
+								class="input"
+								id="slugInput"
+								bind:value={socketioQuestions.pendingEntities[0].question}
+							/>
+							<label class="input-filled-label" for="slugInput"
+								>[use this label to identify the question in your presentation]{newSuffix}</label
+							>
+						</div>
+					</FormElement>
+					{#snippet languageDescription()}
+						Giving the language of the question text in the code. To allow automatic translation the
+						algorithm needs to know from what language to translate into all the others, so this is
+						the language of the source.
+					{/snippet}
+					<FormElement title="Language" description={languageDescription} extraClasses="max-w-300">
+						<div class="label">
+							{@render warning()} A drop-down field to select the language for the question, with a default
+							value of the presentation's language.
+						</div>
+					</FormElement>
+					{#snippet accessDescription()}
+						Who has which access to this presentation?
+						<br />
+						Default is public <span class="icon-[gis--globe-earth-alt] size-4"></span>
+						and have read
+						<span
+							class={`${AccessHandler.rightsIcon(Action.READ)} ${AccessHandler.rightsIconColor(Action.READ)} size-4`}
+						></span>
+						access.
+					{/snippet}
+					<FormElement title="Access" description={accessDescription} extraClasses="max-w-130">
+						<div class="label mb-2 flex flex-1 items-center gap-1">
+							<label class="label label-text text-base-content" for="new-question-inherit">
+								<input
+									id="new-question-inherit"
+									type="checkbox"
+									class="checkbox checkbox-primary"
+									bind:checked={inheritPending}
+								/>
+							</label>
+							inherit from {parentId}
+						</div>
+						<ul class="bg-base-150 shadow-base-shadow overflow-y-auto rounded-lg p-2 shadow-inner">
+							{#each shareOptionsForNewQuestion, i (i)}
+								<ShareItem
+									// TBD: adopt for multiple new!
+									resourceId={socketioQuestions.pendingEntities[0].id}
+									// No need to bind, as it is anyways handled through the entityContainer's methods and the socketio's submitEntity method.
+									shareOption={shareOptionsForNewQuestion[i]}
+									socketio={socketioQuestions}
+									actions={[Action.OWN, Action.WRITE, Action.CONNECT, Action.READ, undefined]}
+									// share={socketioQuestions?.shareEntity.bind(socketioQuestions)}
+									wide
+								/>
+							{/each}
+						</ul>
+					</FormElement>
+					<FormElement title="Add multiple questions">
+						<div class="mb-2 flex flex-1 flex-row items-center gap-1">
+							<label class="label label-text text-base-content" for="new-question-multiple">
+								<input
+									id="new-question-multiple"
+									type="checkbox"
+									disabled
+									// TBD: remove disabled, when functionality is implemented!
+									class="checkbox checkbox-primary"
+									bind:checked={multiplePending}
+								/>
+							</label>
+							with suffixes
+							<input
+								id="multiple-groups-start"
+								type="number"
+								placeholder={multipleSuffixes.start.toString()}
+								class="input shadow-shadow flex-2 shadow-inner"
+								name="multiple-groups-suffix-start"
+								disabled={!multiplePending}
+								bind:value={multipleSuffixes.start}
+							/>
+							<span class="label flex-1"> to</span>
+							<input
+								id="multiple-groups-end"
+								type="number"
+								placeholder={multipleSuffixes.end.toString()}
+								class="input shadow-shadow flex-2 shadow-inner"
+								name="multiple-groups-suffix-end"
+								disabled={!multiplePending}
+								bind:value={multipleSuffixes.end}
+							/>
+						</div>
+						<div class="label">
+							{@render warning()} Creation of multiple not functional yet
+						</div>
+					</FormElement> 
+				</div> -->
+				<!-- <div
+					id="existing-question-tab"
+					class="hidden h-full max-h-100 overflow-scroll"
+					role="tabpanel"
+					aria-labelledby="existing-question-head"
+				>
+					<div class="label">
+						{@render warning()} Table of existing questions and functionality to link or copy them into
+						this presentation.
+					</div>
+					{#each notLinkedQuestions as linkedQuestion, idx (idx)}
+						<div class="linked-question">
+							<p>Q: {linkedQuestion.question}</p>
+						</div>
+					{/each}
+				</div> -->
+			</div>
+		</div>
+		{#snippet footer()}
+			<div class="ml-5 flex flex-row justify-end gap-4">
+				<button
+					class="btn btn-secondary-container btn-gradient shadow-outline rounded-full shadow"
+					aria-label="Cancel"
+					onclick={() => {
+						hideNewNumericalAnswerCard = true;
+					}}><span class="icon-[tabler--x] size-5"></span>Cancel</button
+				>
+				<!-- <button
+					class="btn btn-primary-container btn-gradient shadow-outline rounded-full shadow"
+					aria-label="Save new presentation"
+					onclick={() => {
+						numericalSocketio.submitBulk(parentId, inheritPending);
+						hideNewNumericalAnswerCard = true;
+						// For buld submit the new pending needs manual creation:
+						numericalSocketio.createPending();
+					}}><span class="icon-[tabler--send-2] size-5"></span>
+					Save
+				</button> -->
+			</div>
+		{/snippet}
+	</Card>
+{:else if !numericalSocketio?.pendingEntities[0] && !hideNewNumericalAnswerCard}
+	<div class="label text-error" transition:slide={{ duration: 600 }}>
+		<span class="icon-[svg-spinners--12-dots-scale-rotate] size-6"></span>connecting ...
+	</div>
+{/if}
+
+<Card id="linked-numerical" title="Linked numerical answers" extraClasses="label-large">
+	{#snippet header()}
+		<div class="flex justify-between">
+			<Title id="existingQuestions" class="grow">Overview</Title>
+			<div class="flex flex-row items-center justify-center">
+				{#if hideNewNumericalAnswerCard || !numericalSocketio?.pendingEntities[0]}
+					<button
+						transition:fade={{ duration: 600 }}
+						class="btn btn-primary-container btn-gradient label btn shadow-outline mx-4 rounded-full shadow-sm"
+						aria-label="Add new question"
+						onclick={() => (hideNewNumericalAnswerCard = false)}
+					>
+						<!-- onclick={() => goto(resolve('/(layout)/presentation/(protected)/setup/new'))} -->
+						<span class="icon-[fa6-solid--plus] size-5"></span>
+						<!-- <span class="hidden ">Add</span> -->
+						<span class="hidden sm:inline">Add new</span>
+						<span class="hidden md:inline">number</span>
+					</button>
+				{/if}
+				<div class="join shadow-outline rounded-full shadow-sm">
+					<button
+						aria-label="Grid"
+						class="btn join-item btn-secondary btn-gradient btn-sm shadow-outline rounded-l-full py-4 shadow {numericalViewMode !==
+						'grid'
+							? 'opacity-60'
+							: ''}"
+						onclick={() => (numericalViewMode = 'grid')}
+					>
+						<span class="icon-[gridicons--grid] size-5"></span>
+					</button>
+					<button
+						aria-label="List"
+						class="btn join-item btn-secondary btn-gradient btn-sm shadow-outline py-4 shadow {numericalViewMode !==
+						'list'
+							? 'opacity-60'
+							: ''}"
+						onclick={() => (numericalViewMode = 'list')}
+					>
+						<span class="icon-[material-symbols--view-list-outline] size-5"></span>
+					</button>
+					<button
+						aria-label="Table"
+						class="btn join-item btn-secondary btn-gradient btn-sm shadow-outline rounded-r-full py-4 shadow {numericalViewMode !==
+						'table'
+							? 'opacity-60'
+							: ''}"
+						onclick={() => (numericalViewMode = 'table')}
+					>
+						<span class="icon-[material-symbols--table-outline] size-5"></span>
+					</button>
+				</div>
+			</div>
+		</div>
+	{/snippet}
+	<div class="w-full overflow-x-auto {numericalViewMode !== 'grid' ? 'hidden' : ''}">
+		<p class="bg-warning text-warning-content rounded-lg p-4">
+			Grid view mode is not developed yet
+		</p>
+	</div>
+	<div class="w-full overflow-x-auto {numericalViewMode !== 'list' ? 'hidden' : ''}">
+		<p class="bg-warning text-warning-content rounded-lg p-4">
+			List view mode is not developed yet
+		</p>
+	</div>
+	<div class="w-full overflow-x-auto {numericalViewMode !== 'table' ? 'hidden' : ''}">
+		<!-- TBD: move ActionButtons to components -->
+		{#snippet numericalActionsCell(numerical: NumericalExtended)}
+			<ActionButtons
+				resourceId={numerical.id}
+				accessRight={numericalSocketio?.accessRights[numerical.id]}
+				socketio={numericalSocketio}
+			/>
+		{/snippet}
+		<Table
+			columns={[
+				{
+					header: text('Value'),
+					cell: field<NumericalExtended>('value'),
+					headerClass: 'w-3/5',
+					cellClass: 'max-w-0'
+				},
+				{
+					header: text('Tolerance'),
+					cell: field<NumericalExtended>('tolerance'),
+					headerClass: 'text-center',
+					cellClass: 'text-center'
+				},
+				{
+					header: text('Access'),
+					cell: value(() => '[Access]'),
+					headerClass: 'text-center',
+					cellClass: 'text-center'
+				},
+				{
+					header: text('Actions'),
+					cell: snippet(numericalActionsCell),
+					headerClass: 'w-px text-center whitespace-nowrap',
+					cellClass: 'w-px py-1 text-center align-middle whitespace-nowrap'
+				}
+			] satisfies TableColumn<NumericalExtended>[]}
+			entityContainer={numericalSocketio}
+			displaySelection="sortedNumericalAnswers"
+		/>
+	</div>
+</Card>
+
+<!-- <div class="mx-2 grid w-full grid-cols-5 gap-2">
 	{#each numericalAnswersSorted as answer, index (answer.id)}
 		<div animate:flip={{ duration: 300 }}>
 			{@render answerBubble(answer.value, answer.id, index)}
 		</div>
 	{/each}
-</div>
+</div> -->
 
 <Heading id="json-data">JSONdata:</Heading>
 
